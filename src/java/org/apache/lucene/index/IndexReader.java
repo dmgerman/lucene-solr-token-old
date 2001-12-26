b4d129de1da71093394626d36a4ef2e327af45cd
Otis Gospodnetic <otis@apache.org>
2001-12-26 17:23:05 +0000

    - Modified unlock method to have it delete 'commit.lock' with as well. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149635 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/index/IndexReader.java
+++ src/java/org/apache/lucene/index/IndexReader.java
@@ -700,6 +700,13 @@ argument_list|(
 literal|"write.lock"
 argument_list|)
 expr_stmt|;
+name|directory
+operator|.
+name|deleteFile
+argument_list|(
+literal|"commit.lock"
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 end_class
