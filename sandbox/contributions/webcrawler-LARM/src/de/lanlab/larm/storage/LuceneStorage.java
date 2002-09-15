bd24349dfe988d5bad43300aa09c4492ba40e17c
Otis Gospodnetic <otis@apache.org>
2002-09-15 00:22:05 +0000

    - Added APL. - Moved all attributes up. - Fixed weird class javadoc. - Made a few small mods and added FIXMEs.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150824 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/storage/LuceneStorage.java
+++ sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/storage/LuceneStorage.java
@@ -1,4 +1,7 @@
 begin_unit
+begin_comment
+comment|/* ====================================================================  * The Apache Software License, Version 1.1  *  * Copyright (c) 2001 The Apache Software Foundation.  All rights  * reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  *  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in  *    the documentation and/or other materials provided with the  *    distribution.  *  * 3. The end-user documentation included with the redistribution,  *    if any, must include the following acknowledgment:  *       "This product includes software developed by the  *        Apache Software Foundation (http://www.apache.org/)."  *    Alternately, this acknowledgment may appear in the software itself,  *    if and wherever such third-party acknowledgments normally appear.  *  * 4. The names "Apache" and "Apache Software Foundation" and  *    "Apache Lucene" must not be used to endorse or promote products  *    derived from this software without prior written permission. For  *    written permission, please contact apache@apache.org.  *  * 5. Products derived from this software may not be called "Apache",  *    "Apache Lucene", nor may "Apache" appear in their name, without  *    prior written permission of the Apache Software Foundation.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR  * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  * ====================================================================  *  * This software consists of voluntary contributions made by many  * individuals on behalf of the Apache Software Foundation.  For more  * information on the Apache Software Foundation, please see  *<http://www.apache.org/>.  */
+end_comment
 begin_package
 DECL|package|de.lanlab.larm.storage
 package|package
@@ -24,9 +27,6 @@ operator|.
 name|WebDocument
 import|;
 end_import
-begin_comment
-comment|/**  * Title: LARM Lanlab Retrieval Machine Description: Copyright: Copyright (c)  * Company:  *  * @author  * @version   1.0  */
-end_comment
 begin_import
 import|import
 name|org
@@ -85,7 +85,7 @@ name|*
 import|;
 end_import
 begin_comment
-comment|/**  * Description of the Class  *  * @author    Administrator  * @created   14. Juni 2002  */
+comment|/**  * FIXME document this class  * Title: LARM Lanlab Retrieval Machine Description: Copyright: Copyright (c)  * Company:  *  * @author    Administrator  * @created   14. Juni 2002  * @version $Id$  */
 end_comment
 begin_class
 DECL|class|LuceneStorage
@@ -95,7 +95,35 @@ name|LuceneStorage
 implements|implements
 name|DocumentStorage
 block|{
+DECL|field|INDEX
+specifier|public
+specifier|final
+specifier|static
+name|int
+name|INDEX
+init|=
+literal|1
+decl_stmt|;
+DECL|field|STORE
+specifier|public
+specifier|final
+specifier|static
+name|int
+name|STORE
+init|=
+literal|2
+decl_stmt|;
+DECL|field|TOKEN
+specifier|public
+specifier|final
+specifier|static
+name|int
+name|TOKEN
+init|=
+literal|4
+decl_stmt|;
 DECL|field|fieldInfos
+specifier|private
 name|HashMap
 name|fieldInfos
 init|=
@@ -104,16 +132,24 @@ name|HashMap
 argument_list|()
 decl_stmt|;
 DECL|field|writer
+specifier|private
 name|IndexWriter
 name|writer
 decl_stmt|;
 DECL|field|analyzer
+specifier|private
 name|Analyzer
 name|analyzer
 decl_stmt|;
 DECL|field|indexName
+specifier|private
 name|String
 name|indexName
+decl_stmt|;
+DECL|field|create
+specifier|private
+name|boolean
+name|create
 decl_stmt|;
 comment|/**      * Constructor for the LuceneStorage object      */
 DECL|method|LuceneStorage
@@ -199,10 +235,6 @@ operator|=
 name|create
 expr_stmt|;
 block|}
-DECL|field|create
-name|boolean
-name|create
-decl_stmt|;
 comment|/**      * Description of the Method      */
 DECL|method|open
 specifier|public
@@ -210,6 +242,7 @@ name|void
 name|open
 parameter_list|()
 block|{
+comment|// FIXME: replace with logging
 name|System
 operator|.
 name|out
@@ -244,6 +277,7 @@ name|IOException
 name|e
 parameter_list|)
 block|{
+comment|// FIXME: replace with logging
 name|System
 operator|.
 name|err
@@ -270,6 +304,7 @@ operator|!=
 literal|null
 condition|)
 block|{
+comment|// FIXME: replace with logging
 name|System
 operator|.
 name|out
@@ -281,34 +316,7 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-DECL|field|INDEX
-specifier|public
-specifier|final
-specifier|static
-name|int
-name|INDEX
-init|=
-literal|1
-decl_stmt|;
-DECL|field|STORE
-specifier|public
-specifier|final
-specifier|static
-name|int
-name|STORE
-init|=
-literal|2
-decl_stmt|;
-DECL|field|TOKEN
-specifier|public
-specifier|final
-specifier|static
-name|int
-name|TOKEN
-init|=
-literal|4
-decl_stmt|;
-comment|/**      * Gets the fieldInfo attribute of the LuceneStorage object      *      * @param fieldName  Description of the Parameter      * @param def        Description of the Parameter      * @return           The fieldInfo value      */
+comment|/**      * Gets the fieldInfo attribute of the LuceneStorage object      *      * @param fieldName  Description of the Parameter      * @param defaultValue Description of the Parameter      * @return           The fieldInfo value      */
 DECL|method|getFieldInfo
 specifier|protected
 name|int
@@ -318,7 +326,7 @@ name|String
 name|fieldName
 parameter_list|,
 name|int
-name|def
+name|defaultValue
 parameter_list|)
 block|{
 name|Integer
@@ -351,7 +359,7 @@ block|}
 else|else
 block|{
 return|return
-name|def
+name|defaultValue
 return|;
 block|}
 block|}
@@ -442,21 +450,19 @@ block|{
 comment|//System.out.println("storing " + webDoc.getUrl());
 name|boolean
 name|store
-decl_stmt|;
-name|boolean
-name|index
-decl_stmt|;
-name|boolean
-name|token
-decl_stmt|;
-name|store
-operator|=
-name|index
-operator|=
-name|token
-operator|=
+init|=
 literal|false
-expr_stmt|;
+decl_stmt|;
+name|boolean
+name|index
+init|=
+literal|false
+decl_stmt|;
+name|boolean
+name|token
+init|=
+literal|false
+decl_stmt|;
 name|Document
 name|doc
 init|=
@@ -623,6 +629,7 @@ name|IOException
 name|e
 parameter_list|)
 block|{
+comment|// FIXME: replace with logging
 name|System
 operator|.
 name|err
