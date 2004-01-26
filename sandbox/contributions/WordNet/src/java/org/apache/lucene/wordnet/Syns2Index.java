6486ef9c5a4b6f12c68a366769ee263961a8cebc
Erik Hatcher <ehatcher@apache.org>
2004-01-26 17:29:09 +0000

    modify to build syn index in compound format
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150947 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contributions/WordNet/src/java/org/apache/lucene/wordnet/Syns2Index.java
+++ sandbox/contributions/WordNet/src/java/org/apache/lucene/wordnet/Syns2Index.java
@@ -822,6 +822,13 @@ argument_list|,
 literal|true
 argument_list|)
 decl_stmt|;
+name|writer
+operator|.
+name|setUseCompoundFile
+argument_list|(
+literal|true
+argument_list|)
+expr_stmt|;
 name|Iterator
 name|i1
 init|=
