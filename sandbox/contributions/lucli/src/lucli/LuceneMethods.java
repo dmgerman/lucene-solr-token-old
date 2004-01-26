d896684bf9943a324955a7eb7ce59d55f881f557
Erik Hatcher <ehatcher@apache.org>
2004-01-26 01:36:15 +0000

    lucli code cleanup and a couple of minor enhancements/fixes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150945 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contributions/lucli/src/lucli/LuceneMethods.java
+++ sandbox/contributions/lucli/src/lucli/LuceneMethods.java
@@ -590,9 +590,6 @@ operator|+
 literal|" total matching documents"
 argument_list|)
 expr_stmt|;
-name|Query
-name|explainQuery
-decl_stmt|;
 if|if
 condition|(
 name|explain
@@ -682,7 +679,11 @@ name|message
 argument_list|(
 literal|"---------------- "
 operator|+
+operator|(
 name|ii
+operator|+
+literal|1
+operator|)
 operator|+
 literal|" score:"
 operator|+
@@ -804,6 +805,7 @@ name|close
 argument_list|()
 expr_stmt|;
 block|}
+comment|/**    * @todo Allow user to specify what field(s) to display    */
 DECL|method|printHit
 specifier|private
 name|void
@@ -839,15 +841,33 @@ name|ii
 index|]
 decl_stmt|;
 name|String
+index|[]
 name|result
 init|=
 name|doc
 operator|.
-name|get
+name|getValues
 argument_list|(
 name|currField
 argument_list|)
 decl_stmt|;
+for|for
+control|(
+name|int
+name|i
+init|=
+literal|0
+init|;
+name|i
+operator|<
+name|result
+operator|.
+name|length
+condition|;
+name|i
+operator|++
+control|)
+block|{
 name|message
 argument_list|(
 name|currField
@@ -855,8 +875,12 @@ operator|+
 literal|":"
 operator|+
 name|result
+index|[
+name|i
+index|]
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 comment|//another option is to just do message(doc);
 block|}
@@ -1063,6 +1087,7 @@ name|query
 operator|)
 return|;
 block|}
+comment|/**    * @todo Allow user to specify analyzer    */
 DECL|method|initSearch
 specifier|private
 name|Hits
@@ -1761,7 +1786,7 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-comment|/** Provides a list of the top terms of the index. 	 * 	 * @param field  - the name of the command or null for all of them. 	 */
+comment|/** Provides a list of the top terms of the index.    *    * @param field  - the name of the command or null for all of them.    */
 DECL|method|terms
 specifier|public
 name|void
@@ -1838,19 +1863,6 @@ name|termMap
 operator|.
 name|put
 argument_list|(
-operator|new
-name|Integer
-argument_list|(
-operator|(
-literal|0
-operator|-
-name|terms
-operator|.
-name|docFreq
-argument_list|()
-operator|)
-argument_list|)
-argument_list|,
 name|term
 operator|.
 name|field
@@ -1862,6 +1874,17 @@ name|term
 operator|.
 name|text
 argument_list|()
+argument_list|,
+operator|new
+name|Integer
+argument_list|(
+operator|(
+name|terms
+operator|.
+name|docFreq
+argument_list|()
+operator|)
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
@@ -1896,28 +1919,28 @@ name|ii
 operator|++
 control|)
 block|{
-name|Integer
-name|termFreq
-init|=
-operator|(
-name|Integer
-operator|)
-name|termIterator
-operator|.
-name|next
-argument_list|()
-decl_stmt|;
 name|String
 name|termDetails
 init|=
 operator|(
 name|String
 operator|)
+name|termIterator
+operator|.
+name|next
+argument_list|()
+decl_stmt|;
+name|Integer
+name|termFreq
+init|=
+operator|(
+name|Integer
+operator|)
 name|termMap
 operator|.
 name|get
 argument_list|(
-name|termFreq
+name|termDetails
 argument_list|)
 decl_stmt|;
 name|message
@@ -1936,7 +1959,7 @@ name|close
 argument_list|()
 expr_stmt|;
 block|}
-comment|/** Sort Hashtable values 	 * @param h the hashtable we're sorting 	 * from http://developer.java.sun.com/developer/qow/archive/170/index.jsp 	 */
+comment|/** Sort Hashtable values    * @param h the hashtable we're sorting    * from http://developer.java.sun.com/developer/qow/archive/170/index.jsp    */
 specifier|public
 specifier|static
 name|Entry
--- sandbox/contributions/lucli/src/lucli/Lucli.java
+++ sandbox/contributions/lucli/src/lucli/Lucli.java
@@ -94,7 +94,7 @@ name|HISTORYFILE
 init|=
 literal|".lucli"
 decl_stmt|;
-comment|//directory "index" under the current directory
+comment|//history file in user's home directory
 DECL|field|MAX_TERMS
 specifier|public
 specifier|final
@@ -1454,7 +1454,7 @@ parameter_list|()
 block|{
 name|message
 argument_list|(
-literal|"Usage: lucli [-j]"
+literal|"Usage: lucli [-r]"
 argument_list|)
 expr_stmt|;
 name|message
