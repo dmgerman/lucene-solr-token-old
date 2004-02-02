31c773cd442fb7b3c4fed347c349b891b51dd060
Otis Gospodnetic <otis@apache.org>
2004-02-02 13:27:52 +0000

    - Added ASL 2.0
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150186 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/spans/Spans.java
+++ src/java/org/apache/lucene/search/spans/Spans.java
@@ -13,6 +13,9 @@ operator|.
 name|spans
 package|;
 end_package
+begin_comment
+comment|/**  * Copyright 2004 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 begin_import
 import|import
 name|java
@@ -39,7 +42,7 @@ parameter_list|()
 throws|throws
 name|IOException
 function_decl|;
-comment|/** Skips to the first match beyond the current whose document number is    * greater than or equal to<i>target</i>.<p>Returns true iff there is such    * a match.<p>Behaves as if written:<pre>    *   boolean skipTo(int target) {    *     do {    *       if (!next())    * 	     return false;    *     } while (target> doc());    *     return true;    *   }    *</pre>    * Most implementations are considerably more efficient than that.    */
+comment|/** Skips to the first match beyond the current, whose document number is    * greater than or equal to<i>target</i>.<p>Returns true iff there is such    * a match.<p>Behaves as if written:<pre>    *   boolean skipTo(int target) {    *     do {    *       if (!next())    * 	     return false;    *     } while (target> doc());    *     return true;    *   }    *</pre>    * Most implementations are considerably more efficient than that.    */
 DECL|method|skipTo
 name|boolean
 name|skipTo
