23bba424c23266f62bd3e3a375b4d6abaeb09551
Otis Gospodnetic <otis@apache.org>
2004-08-17 20:53:16 +0000

    - Small fixes from Bernhard Messer (http://issues.apache.org/bugzilla/show_bug.cgi?id=30522) PR: 30522 Obtained from: Submitted by:	Bernhard Messer Reviewed by:	Otis Gospodnetic
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150443 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/index/TermVectorsWriter.java
+++ src/java/org/apache/lucene/index/TermVectorsWriter.java
@@ -11,6 +11,9 @@ operator|.
 name|index
 package|;
 end_package
+begin_comment
+comment|/**  * Copyright 2004 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 begin_import
 import|import
 name|org
@@ -69,7 +72,7 @@ name|Vector
 import|;
 end_import
 begin_comment
-comment|/**  * Writer works by opening a document and then opening the fields within the document and then  * writing out the vectors for each field.  *   * Rough usage:  *<CODE>  for each document  {  writer.openDocument();  for each field on the document  {  writer.openField(field);  for all of the terms  {  writer.addTerm(...)  }  writer.closeField  }  writer.closeDocument()      }</CODE>  */
+comment|/**  * Writer works by opening a document and then opening the fields within the document and then  * writing out the vectors for each field.  *   * Rough usage:  *<CODE>  for each document  {  writer.openDocument();  for each field on the document  {  writer.openField(field);  for all of the terms  {  writer.addTerm(...)  }  writer.closeField  }  writer.closeDocument()      }</CODE>  *  * @version $Id$  *   */
 end_comment
 begin_class
 DECL|class|TermVectorsWriter
