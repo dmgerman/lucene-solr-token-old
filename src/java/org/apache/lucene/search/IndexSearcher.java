aeedeb0012bb3030033535daf14e6b14b36b5d73
Daniel Naber <dnaber@apache.org>
2004-09-30 20:53:07 +0000

    Adding a small comment that says people should use only one IndexSearches for all searches.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150554 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/IndexSearcher.java
+++ src/java/org/apache/lucene/search/IndexSearcher.java
@@ -85,7 +85,7 @@ name|Term
 import|;
 end_import
 begin_comment
-comment|/** Implements search over a single IndexReader.  *  *<p>Applications usually need only call the inherited {@link #search(Query)}  * or {@link #search(Query,Filter)} methods.  */
+comment|/** Implements search over a single IndexReader.  *  *<p>Applications usually need only call the inherited {@link #search(Query)}  * or {@link #search(Query,Filter)} methods. For performance reasons it is   * recommended to open only one IndexSearcher and use it for all of your searches.  */
 end_comment
 begin_class
 DECL|class|IndexSearcher
