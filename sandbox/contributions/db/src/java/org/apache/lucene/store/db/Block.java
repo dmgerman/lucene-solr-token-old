2300af6e098bbce8c3e8e47c66c12020050fa991
Erik Hatcher <ehatcher@apache.org>
2005-01-18 14:11:09 +0000

    Update of DBDirectory from Andi.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@151034 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contributions/db/src/java/org/apache/lucene/store/db/Block.java
+++ sandbox/contributions/db/src/java/org/apache/lucene/store/db/Block.java
@@ -14,7 +14,7 @@ name|db
 package|;
 end_package
 begin_comment
-comment|/* ====================================================================  * The Apache Software License, Version 1.1  *  * Copyright (c) 2004 The Apache Software Foundation.  All rights  * reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  *  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in  *    the documentation and/or other materials provided with the  *    distribution.  *  * 3. The end-user documentation included with the redistribution,  *    if any, must include the following acknowledgment:  *       "This product includes software developed by the  *        Apache Software Foundation (http://www.apache.org/)."  *    Alternately, this acknowledgment may appear in the software itself,  *    if and wherever such third-party acknowledgments normally appear.  *  * 4. The names "Apache" and "Apache Software Foundation" and  *    "Apache Lucene" must not be used to endorse or promote products  *    derived from this software without prior written permission. For  *    written permission, please contact apache@apache.org.  *  * 5. Products derived from this software may not be called "Apache",  *    "Apache Lucene", nor may "Apache" appear in their name, without  *    prior written permission of the Apache Software Foundation.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR  * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  * ====================================================================  *  * This software consists of voluntary contributions made by the Open Source  * Applications Foundation on behalf of the Apache Software Foundation.  * For more information on the Open Source Applications Foundation, please see  *<http://www.osafoundation.org>.  * For more information on the Apache Software Foundation, please see  *<http://www.apache.org/>.  */
+comment|/**  * Copyright 2002-2004 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_import
 import|import
@@ -33,7 +33,7 @@ name|sleepycat
 operator|.
 name|db
 operator|.
-name|Dbt
+name|DatabaseEntry
 import|;
 end_import
 begin_import
@@ -43,6 +43,8 @@ operator|.
 name|sleepycat
 operator|.
 name|db
+operator|.
+name|internal
 operator|.
 name|Db
 import|;
@@ -55,6 +57,8 @@ name|sleepycat
 operator|.
 name|db
 operator|.
+name|internal
+operator|.
 name|DbTxn
 import|;
 end_import
@@ -66,7 +70,7 @@ name|sleepycat
 operator|.
 name|db
 operator|.
-name|DbException
+name|DatabaseException
 import|;
 end_import
 begin_comment
@@ -83,7 +87,7 @@ block|{
 DECL|field|key
 DECL|field|data
 specifier|protected
-name|Dbt
+name|DatabaseEntry
 name|key
 decl_stmt|,
 name|data
@@ -110,7 +114,7 @@ decl_stmt|;
 name|key
 operator|=
 operator|new
-name|Dbt
+name|DatabaseEntry
 argument_list|(
 operator|new
 name|byte
@@ -125,17 +129,21 @@ argument_list|)
 expr_stmt|;
 name|key
 operator|.
-name|setFlags
+name|setUserBuffer
 argument_list|(
-name|Db
+name|fileKey
 operator|.
-name|DB_DBT_USERMEM
+name|length
+operator|+
+literal|8
+argument_list|,
+literal|true
 argument_list|)
 expr_stmt|;
 name|data
 operator|=
 operator|new
-name|Dbt
+name|DatabaseEntry
 argument_list|(
 operator|new
 name|byte
@@ -148,21 +156,14 @@ argument_list|)
 expr_stmt|;
 name|data
 operator|.
-name|setUserBufferLength
+name|setUserBuffer
 argument_list|(
 name|data
 operator|.
 name|getSize
 argument_list|()
-argument_list|)
-expr_stmt|;
-name|data
-operator|.
-name|setFlags
-argument_list|(
-name|Db
-operator|.
-name|DB_DBT_USERMEM
+argument_list|,
+literal|true
 argument_list|)
 expr_stmt|;
 name|System
@@ -450,7 +451,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|DbException
+name|DatabaseException
 name|e
 parameter_list|)
 block|{
@@ -501,7 +502,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|DbException
+name|DatabaseException
 name|e
 parameter_list|)
 block|{
