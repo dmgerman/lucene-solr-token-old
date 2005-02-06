92f97677a2bc3a1a9b7790e544e997c29c9b71a6
Mark Harwood <mharwood@apache.org>
2005-02-06 21:31:54 +0000

    Added Nicko Cadell's Encoder contribution
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@151622 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/highlighter/src/java/org/apache/lucene/search/highlight/Encoder.java
@@ -0,0 +1,39 @@
+begin_unit
+begin_package
+DECL|package|org.apache.lucene.search.highlight
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|search
+operator|.
+name|highlight
+package|;
+end_package
+begin_comment
+comment|/**  * Copyright 2005 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+begin_comment
+comment|/**  * Encodes original text. The Encoder works with the Formatter to generate the output.  *  * @author Nicko Cadell  */
+end_comment
+begin_interface
+DECL|interface|Encoder
+specifier|public
+interface|interface
+name|Encoder
+block|{
+comment|/** 	 * @param originalText The section of text being output 	 * @return 	 */
+DECL|method|encodeText
+name|String
+name|encodeText
+parameter_list|(
+name|String
+name|originalText
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+end_unit
