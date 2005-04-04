8704f9462f3aa0f4294eac7395a236ab5da3d03a
Erik Hatcher <ehatcher@apache.org>
2005-04-04 09:10:05 +0000

    Add accent character normalizer filter contributed by Sven Duzont.  Also created simple test case.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@160011 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/analyzers/src/java/org/apache/lucene/analysis/ISOLatin1AccentFilter.java
@@ -0,0 +1,637 @@
+begin_unit
+begin_package
+DECL|package|org.apache.lucene.analysis
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|analysis
+package|;
+end_package
+begin_comment
+comment|/**  * Copyright 2005 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+begin_comment
+comment|/**  * A filter that replace accented characters in the ISO Latin 1 character set by  * their unaccented equivalent. The case will not be altered.  *<p>  * For instance, 'à' will be replaced by 'a'.  *<p>  */
+end_comment
+begin_class
+DECL|class|ISOLatin1AccentFilter
+specifier|public
+class|class
+name|ISOLatin1AccentFilter
+extends|extends
+name|TokenFilter
+block|{
+DECL|method|ISOLatin1AccentFilter
+specifier|public
+name|ISOLatin1AccentFilter
+parameter_list|(
+name|TokenStream
+name|input
+parameter_list|)
+block|{
+name|super
+argument_list|(
+name|input
+argument_list|)
+expr_stmt|;
+block|}
+comment|/** 	 * To replace accented characters by unaccented equivalents. 	 */
+DECL|method|next
+specifier|public
+specifier|final
+name|Token
+name|next
+parameter_list|()
+throws|throws
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+block|{
+specifier|final
+name|Token
+name|t
+init|=
+name|input
+operator|.
+name|next
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|t
+operator|==
+literal|null
+condition|)
+return|return
+literal|null
+return|;
+comment|// Return a token with filtered characters.
+return|return
+operator|new
+name|Token
+argument_list|(
+name|RemoveAccents
+argument_list|(
+name|t
+operator|.
+name|termText
+argument_list|()
+argument_list|)
+argument_list|,
+name|t
+operator|.
+name|startOffset
+argument_list|()
+argument_list|,
+name|t
+operator|.
+name|endOffset
+argument_list|()
+argument_list|,
+name|t
+operator|.
+name|type
+argument_list|()
+argument_list|)
+return|;
+block|}
+comment|/** 	 * To replace accented characters in a String by unaccented equivalents. 	 */
+DECL|method|RemoveAccents
+specifier|public
+specifier|final
+specifier|static
+name|String
+name|RemoveAccents
+parameter_list|(
+name|String
+name|input
+parameter_list|)
+block|{
+specifier|final
+name|StringBuffer
+name|output
+init|=
+operator|new
+name|StringBuffer
+argument_list|()
+decl_stmt|;
+for|for
+control|(
+name|int
+name|i
+init|=
+literal|0
+init|;
+name|i
+operator|<
+name|input
+operator|.
+name|length
+argument_list|()
+condition|;
+name|i
+operator|++
+control|)
+block|{
+switch|switch
+condition|(
+name|input
+operator|.
+name|charAt
+argument_list|(
+name|i
+argument_list|)
+condition|)
+block|{
+case|case
+literal|'\u00C0'
+case|:
+comment|// À
+case|case
+literal|'\u00C1'
+case|:
+comment|// Á
+case|case
+literal|'\u00C2'
+case|:
+comment|// Â
+case|case
+literal|'\u00C3'
+case|:
+comment|// Ã
+case|case
+literal|'\u00C4'
+case|:
+comment|// Ä
+case|case
+literal|'\u00C5'
+case|:
+comment|// Å
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"A"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00C6'
+case|:
+comment|// Æ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"AE"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00C7'
+case|:
+comment|// Ç
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"C"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00C8'
+case|:
+comment|// È
+case|case
+literal|'\u00C9'
+case|:
+comment|// É
+case|case
+literal|'\u00CA'
+case|:
+comment|// Ê
+case|case
+literal|'\u00CB'
+case|:
+comment|// Ë
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"E"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00CC'
+case|:
+comment|// Ì
+case|case
+literal|'\u00CD'
+case|:
+comment|// Í
+case|case
+literal|'\u00CE'
+case|:
+comment|// Î
+case|case
+literal|'\u00CF'
+case|:
+comment|// Ï
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"I"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00D0'
+case|:
+comment|// Ð
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"D"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00D1'
+case|:
+comment|// Ñ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"N"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00D2'
+case|:
+comment|// Ò
+case|case
+literal|'\u00D3'
+case|:
+comment|// Ó
+case|case
+literal|'\u00D4'
+case|:
+comment|// Ô
+case|case
+literal|'\u00D5'
+case|:
+comment|// Õ
+case|case
+literal|'\u00D6'
+case|:
+comment|// Ö
+case|case
+literal|'\u00D8'
+case|:
+comment|// Ø
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"O"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u0152'
+case|:
+comment|// 
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"OE"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00DE'
+case|:
+comment|// Þ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"TH"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00D9'
+case|:
+comment|// Ù
+case|case
+literal|'\u00DA'
+case|:
+comment|// Ú
+case|case
+literal|'\u00DB'
+case|:
+comment|// Û
+case|case
+literal|'\u00DC'
+case|:
+comment|// Ü
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"U"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00DD'
+case|:
+comment|// Ý
+case|case
+literal|'\u0178'
+case|:
+comment|// 
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"Y"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00E0'
+case|:
+comment|// à
+case|case
+literal|'\u00E1'
+case|:
+comment|// á
+case|case
+literal|'\u00E2'
+case|:
+comment|// â
+case|case
+literal|'\u00E3'
+case|:
+comment|// ã
+case|case
+literal|'\u00E4'
+case|:
+comment|// ä
+case|case
+literal|'\u00E5'
+case|:
+comment|// å
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"a"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00E6'
+case|:
+comment|// æ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"ae"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00E7'
+case|:
+comment|// ç
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"c"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00E8'
+case|:
+comment|// è
+case|case
+literal|'\u00E9'
+case|:
+comment|// é
+case|case
+literal|'\u00EA'
+case|:
+comment|// ê
+case|case
+literal|'\u00EB'
+case|:
+comment|// ë
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"e"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00EC'
+case|:
+comment|// ì
+case|case
+literal|'\u00ED'
+case|:
+comment|// í
+case|case
+literal|'\u00EE'
+case|:
+comment|// î
+case|case
+literal|'\u00EF'
+case|:
+comment|// ï
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"i"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00F0'
+case|:
+comment|// ð
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"d"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00F1'
+case|:
+comment|// ñ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"n"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00F2'
+case|:
+comment|// ò
+case|case
+literal|'\u00F3'
+case|:
+comment|// ó
+case|case
+literal|'\u00F4'
+case|:
+comment|// ô
+case|case
+literal|'\u00F5'
+case|:
+comment|// õ
+case|case
+literal|'\u00F6'
+case|:
+comment|// ö
+case|case
+literal|'\u00F8'
+case|:
+comment|// ø
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"o"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u0153'
+case|:
+comment|// 
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"oe"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00DF'
+case|:
+comment|// ß
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"ss"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00FE'
+case|:
+comment|// þ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"th"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00F9'
+case|:
+comment|// ù
+case|case
+literal|'\u00FA'
+case|:
+comment|// ú
+case|case
+literal|'\u00FB'
+case|:
+comment|// û
+case|case
+literal|'\u00FC'
+case|:
+comment|// 
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"u"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'\u00FD'
+case|:
+comment|// ý
+case|case
+literal|'\u00FF'
+case|:
+comment|// ÿ
+name|output
+operator|.
+name|append
+argument_list|(
+literal|"y"
+argument_list|)
+expr_stmt|;
+break|break;
+default|default :
+name|output
+operator|.
+name|append
+argument_list|(
+name|input
+operator|.
+name|charAt
+argument_list|(
+name|i
+argument_list|)
+argument_list|)
+expr_stmt|;
+break|break;
+block|}
+block|}
+return|return
+name|output
+operator|.
+name|toString
+argument_list|()
+return|;
+block|}
+block|}
+end_class
+end_unit
