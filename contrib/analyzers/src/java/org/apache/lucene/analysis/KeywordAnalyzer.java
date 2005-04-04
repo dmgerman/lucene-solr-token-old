c2cc621177a5c57da5b431e063efefc22c6f5af7
Daniel Naber <dnaber@apache.org>
2005-04-04 17:45:36 +0000

    import cleanup
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@160065 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/analyzers/src/java/org/apache/lucene/analysis/KeywordAnalyzer.java
+++ contrib/analyzers/src/java/org/apache/lucene/analysis/KeywordAnalyzer.java
@@ -20,15 +20,6 @@ name|java
 operator|.
 name|io
 operator|.
-name|IOException
-import|;
-end_import
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|Reader
 import|;
 end_import
