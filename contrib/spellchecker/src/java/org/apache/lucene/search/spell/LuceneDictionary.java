7c21768168efa427d6387ba65d96bec8ece78f9d
Erik Hatcher <ehatcher@apache.org>
2005-04-11 23:48:02 +0000

    Fixed deprecation issues, adjusted test cases to use assertEquals better, reformatted style
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@160987 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/spellchecker/src/java/org/apache/lucene/search/spell/LuceneDictionary.java
+++ contrib/spellchecker/src/java/org/apache/lucene/search/spell/LuceneDictionary.java
@@ -14,7 +14,7 @@ name|spell
 package|;
 end_package
 begin_comment
-comment|/**  * Copyright 2002-2004 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Copyright 2002-2005 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_import
 import|import
@@ -74,7 +74,7 @@ name|*
 import|;
 end_import
 begin_comment
-comment|/**  *  Lucene Dictionnary  * @author Nicolas Maisonneuve  */
+comment|/**  * Lucene Dictionnary  *  * @author Nicolas Maisonneuve  */
 end_comment
 begin_class
 DECL|class|LuceneDictionary
@@ -157,7 +157,7 @@ parameter_list|()
 block|{
 try|try
 block|{
-enum_decl|enum=
+enum_decl|enum =
 name|reader
 operator|.
 name|terms
@@ -306,7 +306,7 @@ specifier|public
 name|void
 name|remove
 parameter_list|()
-block|{}
+block|{     }
 empty_stmt|;
 block|}
 block|}
