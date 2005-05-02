88649ea5ac388bd77d43ab747e482f72abb6c6ad
Erik Hatcher <ehatcher@apache.org>
2005-05-02 00:08:04 +0000

    adjust license headers to be ASL 2.0
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@165565 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/analyzers/src/java/org/apache/lucene/analysis/KeywordTokenizer.java
+++ contrib/analyzers/src/java/org/apache/lucene/analysis/KeywordTokenizer.java
@@ -11,6 +11,9 @@ operator|.
 name|analysis
 package|;
 end_package
+begin_comment
+comment|/**  * Copyright 2004-2005 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 begin_import
 import|import
 name|java
@@ -30,7 +33,7 @@ name|Reader
 import|;
 end_import
 begin_comment
-comment|/**  * Copyright 2005 The Apache Software Foundation  *<p/>  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *<p/>  * http://www.apache.org/licenses/LICENSE-2.0  *<p/>  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Emits the entire input as a single token.  */
 end_comment
 begin_class
 DECL|class|KeywordTokenizer
