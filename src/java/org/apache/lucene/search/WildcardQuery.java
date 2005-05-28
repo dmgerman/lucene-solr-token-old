296cf8614c7f2737368cf1ce814318aa50a05b6e
Daniel Naber <dnaber@apache.org>
2005-05-28 23:21:00 +0000

    small javadoc fix
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@178902 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/WildcardQuery.java
+++ src/java/org/apache/lucene/search/WildcardQuery.java
@@ -50,7 +50,7 @@ name|IOException
 import|;
 end_import
 begin_comment
-comment|/** Implements the wildcard search query. Supported wildcards are<code>*</code>, which  * matches any character sequence (including the empty one), and<code>?</code>,  * which matches any single character. Note this query can be slow, as it  * needs to iterate over all terms. In order to prevent extremely slow WildcardQueries,  * a Wildcard term must not start with one of the wildcards<code>*</code> or  *<code>?</code>.  *   * @see WildcardTermEnum  */
+comment|/** Implements the wildcard search query. Supported wildcards are<code>*</code>, which  * matches any character sequence (including the empty one), and<code>?</code>,  * which matches any single character. Note this query can be slow, as it  * needs to iterate over many terms. In order to prevent extremely slow WildcardQueries,  * a Wildcard term should not start with one of the wildcards<code>*</code> or  *<code>?</code>.  *   * @see WildcardTermEnum  */
 end_comment
 begin_class
 DECL|class|WildcardQuery
