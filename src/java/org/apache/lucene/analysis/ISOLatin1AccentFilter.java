b23def396f97cb0a9972f190d87f027acea3d52c
Daniel Naber <dnaber@apache.org>
2005-11-21 21:41:43 +0000

    tiny whitespace and javadoc fixes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@347992 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/analysis/ISOLatin1AccentFilter.java
+++ src/java/org/apache/lucene/analysis/ISOLatin1AccentFilter.java
@@ -15,7 +15,7 @@ begin_comment
 comment|/**  * Copyright 2005 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/**  * A filter that replaces accented characters in the ISO Latin 1 character set by  * their unaccented equivalent. The case will not be altered.  *<p>  * For instance, '&agrave;' will be replaced by 'a'.  *<p>  */
+comment|/**  * A filter that replaces accented characters in the ISO Latin 1 character set   * (ISO-8859-1) by their unaccented equivalent. The case will not be altered.  *<p>  * For instance, '&agrave;' will be replaced by 'a'.  *<p>  */
 end_comment
 begin_class
 DECL|class|ISOLatin1AccentFilter
