2636d1580e57cf53ad94f9f822268c3679fb214a
Erik Hatcher <ehatcher@apache.org>
2006-04-21 17:10:12 +0000

    add escaping of attribute values
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@395950 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/request/XMLWriter.java
+++ src/java/org/apache/solr/request/XMLWriter.java
@@ -867,11 +867,13 @@ argument_list|(
 literal|"=\""
 argument_list|)
 expr_stmt|;
-name|writer
+name|XML
 operator|.
-name|write
+name|escapeAttributeValue
 argument_list|(
 name|val
+argument_list|,
+name|writer
 argument_list|)
 expr_stmt|;
 name|writer
@@ -928,17 +930,10 @@ operator|!=
 literal|null
 condition|)
 block|{
-name|writer
-operator|.
-name|write
+name|writeAttr
 argument_list|(
-literal|" name=\""
-argument_list|)
-expr_stmt|;
-name|writer
-operator|.
-name|write
-argument_list|(
+literal|"name"
+argument_list|,
 name|name
 argument_list|)
 expr_stmt|;
@@ -951,7 +946,7 @@ name|writer
 operator|.
 name|write
 argument_list|(
-literal|"\"/>"
+literal|"/>"
 argument_list|)
 expr_stmt|;
 block|}
@@ -961,7 +956,7 @@ name|writer
 operator|.
 name|write
 argument_list|(
-literal|"\">"
+literal|">"
 argument_list|)
 expr_stmt|;
 block|}
