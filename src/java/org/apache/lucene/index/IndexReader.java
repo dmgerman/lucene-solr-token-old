fbeb40fc006a89d39fa36f5f636fee2fff4afc6e
Daniel Naber <dnaber@apache.org>
2006-10-15 14:22:51 +0000

    LUCENE-684: improve javadoc of deleDocuments(Term), thanks to Paul Elschot
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@464201 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/index/IndexReader.java
+++ src/java/org/apache/lucene/index/IndexReader.java
@@ -1611,7 +1611,7 @@ parameter_list|)
 throws|throws
 name|IOException
 function_decl|;
-comment|/** Deletes all documents containing<code>term</code>.    * This is useful if one uses a document field to hold a unique ID string for    * the document.  Then to delete such a document, one merely constructs a    * term with the appropriate field and the unique ID string as its text and    * passes it to this method.    * See {@link #deleteDocument(int)} for information about when this deletion will     * become effective.    * @return the number of documents deleted    */
+comment|/** Deletes all documents that have a given<code>term</code> indexed.    * This is useful if one uses a document field to hold a unique ID string for    * the document.  Then to delete such a document, one merely constructs a    * term with the appropriate field and the unique ID string as its text and    * passes it to this method.    * See {@link #deleteDocument(int)} for information about when this deletion will     * become effective.    * @return the number of documents deleted    */
 DECL|method|deleteDocuments
 specifier|public
 specifier|final
