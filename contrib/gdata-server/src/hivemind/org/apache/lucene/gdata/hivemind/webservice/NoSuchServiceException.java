ef2ba4fdca99f07aed9d839b979a113c9a1545de
Simon Willnauer <simonw@apache.org>
2006-11-13 10:51:04 +0000

    Webarchive packageing prepared for including IoC metadata descriptors 	- including configured IoC descriptors in war distribution 	- improved dependency fetching (on demand) 	- fixed some spelling errors
    Hessian Webservice support
    	- created hessian servlet as a generic http interface for provided services
    	- Interface def., Impl. and Tests
    	- services can be exposed via HessianSkeletonProvider by defining a mapping key, Interface and an implementation instance.
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@474244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/gdata-server/src/hivemind/org/apache/lucene/gdata/hivemind/webservice/NoSuchServiceException.java
@@ -0,0 +1,45 @@
+begin_unit
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+begin_package
+DECL|package|org.apache.lucene.gdata.hivemind.webservice
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|gdata
+operator|.
+name|hivemind
+operator|.
+name|webservice
+package|;
+end_package
+begin_comment
+comment|/**  * Service Exception thrown if the requested service is not registered or not available.  * @author Simon Willnauer  *  */
+end_comment
+begin_class
+DECL|class|NoSuchServiceException
+specifier|public
+class|class
+name|NoSuchServiceException
+extends|extends
+name|RuntimeException
+block|{
+comment|/**      *       */
+DECL|field|serialVersionUID
+specifier|private
+specifier|static
+specifier|final
+name|long
+name|serialVersionUID
+init|=
+operator|-
+literal|3485216993407631494L
+decl_stmt|;
+block|}
+end_class
+end_unit
