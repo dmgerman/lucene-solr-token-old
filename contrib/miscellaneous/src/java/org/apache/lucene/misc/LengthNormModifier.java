5a27c8aee471e062319aaf6852a13b6d46b54bc6
Otis Gospodnetic <otis@apache.org>
2006-12-07 18:07:09 +0000

    - Reindented
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@483579 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/miscellaneous/src/java/org/apache/lucene/misc/LengthNormModifier.java
+++ contrib/miscellaneous/src/java/org/apache/lucene/misc/LengthNormModifier.java
@@ -12,7 +12,7 @@ name|misc
 package|;
 end_package
 begin_comment
-comment|/**   * Copyright 2006 The Apache Software Foundation   *   * Licensed under the Apache License, Version 2.0 (the "License");   * you may not use this file except in compliance with the License.   * You may obtain a copy of the License at   *   *     http://www.apache.org/licenses/LICENSE-2.0   *   * Unless required by applicable law or agreed to in writing, software   * distributed under the License is distributed on an "AS IS" BASIS,   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   * See the License for the specific language governing permissions and   * limitations under the License.   */
+comment|/**  * Copyright 2006 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_import
 import|import
@@ -133,7 +133,7 @@ name|Date
 import|;
 end_import
 begin_comment
-comment|/**  * Given a directory, a Similarity, and a list of fields, updates the  * fieldNorms in place for every document using the Similarity.lengthNorm.  *  *<p>  * NOTE: This only works if you do<b>not</b> use field/document boosts in your  * index.  *</p>  *  * @version $Id:$  */
+comment|/**  * Given a directory, a Similarity, and a list of fields, updates the  * fieldNorms in place for every document using the Similarity.lengthNorm.  *  *<p>  * NOTE: This only works if you do<b>not</b> use field/document boosts in your  * index.  *</p>  *  * @version $Id$  */
 end_comment
 begin_class
 DECL|class|LengthNormModifier
@@ -141,7 +141,7 @@ specifier|public
 class|class
 name|LengthNormModifier
 block|{
-comment|/**      * Command Line Execution method      *      *<pre>      * Usage: LengthNormModifier /path/index package.SimilarityClassName field1 field2 ...      *</pre>      */
+comment|/**    * Command Line Execution method    *    *<pre>    * Usage: LengthNormModifier /path/index package.SimilarityClassName field1 field2 ...    *</pre>    */
 DECL|method|main
 specifier|public
 specifier|static
@@ -362,7 +362,7 @@ specifier|private
 name|Similarity
 name|sim
 decl_stmt|;
-comment|/**      * Constructor for code that wishes to use this class progromaticaly      *      * @param d The Directory to modify      * @param s The Similarity to use in<code>reSetNorms</code>      */
+comment|/**    * Constructor for code that wishes to use this class progromaticaly    *    * @param d The Directory to modify    * @param s The Similarity to use in<code>reSetNorms</code>    */
 DECL|method|LengthNormModifier
 specifier|public
 name|LengthNormModifier
@@ -383,7 +383,7 @@ operator|=
 name|s
 expr_stmt|;
 block|}
-comment|/**      * Re-Set the norms for the specified field.      *      *<p>      * Opens a new IndexReader on the Directory given to this instance,      * modifies the norms using the Similarity given to this instance,      * and closes the IndexReader.      *</p>      *      * @param field the field whose norms      */
+comment|/**    * Re-Set the norms for the specified field.    *    *<p>    * Opens a new IndexReader on the Directory given to this instance,    * modifies the norms using the Similarity given to this instance,    * and closes the IndexReader.    *</p>    *    * @param field the field whose norms    */
 DECL|method|reSetNorms
 specifier|public
 name|void
