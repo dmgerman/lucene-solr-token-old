042c8651f1dff28398547e82ead2fc72a2ce16e3
Mark Harwood <mharwood@apache.org>
2007-01-26 23:51:38 +0000

    Added missing Apache license comments - thanks for the reminder, Otis.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@500417 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/xml-query-parser/src/java/org/apache/lucene/xmlparser/QueryTemplateManager.java
+++ contrib/xml-query-parser/src/java/org/apache/lucene/xmlparser/QueryTemplateManager.java
@@ -217,6 +217,9 @@ name|SAXException
 import|;
 end_import
 begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+begin_comment
 comment|/**  * Provides utilities for turning query form input (such as from a web page or Swing gui) into   * Lucene XML queries by using XSL templates.  This approach offers a convenient way of externalizing   * and changing how user input is turned into Lucene queries.   * Database applications often adopt similar practices by externalizing SQL in template files that can  * be easily changed/optimized by a DBA.    * @author Mark Harwood  */
 end_comment
 begin_class
