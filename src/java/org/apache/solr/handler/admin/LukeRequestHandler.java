4292f6dce74f87616a5c19b31794c6f24d5da429
Ryan McKinley <ryan@apache.org>
2007-04-28 06:05:57 +0000

    changing docID to docId
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@533304 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/handler/admin/LukeRequestHandler.java
+++ src/java/org/apache/solr/handler/admin/LukeRequestHandler.java
@@ -494,7 +494,7 @@ specifier|final
 name|String
 name|DOC_ID
 init|=
-literal|"docID"
+literal|"docId"
 decl_stmt|;
 DECL|field|ID
 specifier|public
@@ -583,7 +583,7 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 name|Integer
-name|docID
+name|docId
 init|=
 name|params
 operator|.
@@ -594,7 +594,7 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
-name|docID
+name|docId
 operator|==
 literal|null
 operator|&&
@@ -649,7 +649,7 @@ argument_list|,
 name|v
 argument_list|)
 decl_stmt|;
-name|docID
+name|docId
 operator|=
 name|searcher
 operator|.
@@ -660,7 +660,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|docID
+name|docId
 operator|<
 literal|0
 condition|)
@@ -686,7 +686,7 @@ block|}
 comment|// Read the document from the index
 if|if
 condition|(
-name|docID
+name|docId
 operator|!=
 literal|null
 condition|)
@@ -704,7 +704,7 @@ name|reader
 operator|.
 name|document
 argument_list|(
-name|docID
+name|docId
 argument_list|)
 expr_stmt|;
 block|}
@@ -729,7 +729,7 @@ literal|404
 argument_list|,
 literal|"Can't find document: "
 operator|+
-name|docID
+name|docId
 argument_list|)
 throw|;
 block|}
@@ -743,7 +743,7 @@ name|getDocumentFieldsInfo
 argument_list|(
 name|doc
 argument_list|,
-name|docID
+name|docId
 argument_list|,
 name|reader
 argument_list|,
@@ -767,9 +767,9 @@ name|docinfo
 operator|.
 name|add
 argument_list|(
-literal|"docID"
+literal|"docId"
 argument_list|,
-name|docID
+name|docId
 argument_list|)
 expr_stmt|;
 name|docinfo
@@ -1623,7 +1623,7 @@ name|Document
 name|doc
 parameter_list|,
 name|int
-name|docID
+name|docId
 parameter_list|,
 name|IndexReader
 name|reader
@@ -1879,7 +1879,7 @@ name|reader
 operator|.
 name|getTermFreqVector
 argument_list|(
-name|docID
+name|docId
 argument_list|,
 name|fieldable
 operator|.
