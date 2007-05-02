3997b3dec97cfd7cdacdcde30439e52a15b237a1
Otis Gospodnetic <otis@apache.org>
2007-05-02 17:48:47 +0000

    - Added the missing ASL v2.0
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@534568 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/SortComparatorSource.java
+++ src/java/org/apache/lucene/search/SortComparatorSource.java
@@ -11,6 +11,9 @@ operator|.
 name|search
 package|;
 end_package
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 begin_import
 import|import
 name|org
@@ -53,7 +56,7 @@ name|SortComparatorSource
 extends|extends
 name|Serializable
 block|{
-comment|/**    * Creates a comparator for the field in the given index.    * @param reader Index to create comparator for.    * @param fieldname  Fieldable to create comparator for.    * @return Comparator of ScoreDoc objects.    * @throws IOException If an error occurs reading the index.    */
+comment|/**    * Creates a comparator for the field in the given index.    * @param reader Index to create comparator for.    * @param fieldname  Name of the field to create comparator for.    * @return Comparator of ScoreDoc objects.    * @throws IOException If an error occurs reading the index.    */
 DECL|method|newComparator
 name|ScoreDocComparator
 name|newComparator
