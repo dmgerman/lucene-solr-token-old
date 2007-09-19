a7506833e58875f1a71c786bb91b8220326f2758
Ryan McKinley <ryan@apache.org>
2007-09-19 18:03:03 +0000

    SOLR-360 -- create a new XMLInputFactory for each request
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@577405 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/handler/XmlUpdateRequestHandler.java
+++ src/java/org/apache/solr/handler/XmlUpdateRequestHandler.java
@@ -532,11 +532,6 @@ name|ALLOW_DUPS
 init|=
 literal|"allowDups"
 decl_stmt|;
-DECL|field|inputFactory
-specifier|private
-name|XMLInputFactory
-name|inputFactory
-decl_stmt|;
 annotation|@
 name|SuppressWarnings
 argument_list|(
@@ -559,13 +554,6 @@ name|init
 argument_list|(
 name|args
 argument_list|)
-expr_stmt|;
-name|inputFactory
-operator|=
-name|BaseXMLInputFactory
-operator|.
-name|newInstance
-argument_list|()
 expr_stmt|;
 block|}
 annotation|@
@@ -705,6 +693,14 @@ argument_list|()
 decl_stmt|;
 try|try
 block|{
+name|XMLInputFactory
+name|inputFactory
+init|=
+name|BaseXMLInputFactory
+operator|.
+name|newInstance
+argument_list|()
+decl_stmt|;
 name|XMLStreamReader
 name|parser
 init|=
@@ -2129,6 +2125,14 @@ name|SolrQueryResponse
 argument_list|()
 decl_stmt|;
 comment|// ignored
+name|XMLInputFactory
+name|inputFactory
+init|=
+name|BaseXMLInputFactory
+operator|.
+name|newInstance
+argument_list|()
+decl_stmt|;
 name|XMLStreamReader
 name|parser
 init|=
