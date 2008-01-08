bd1c772e0558023852972903d5ce00c48abb155e
Ryan McKinley <ryan@apache.org>
2008-01-08 05:52:51 +0000

    make SolrResponse Serializable
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@609879 13f79535-47bb-0310-9956-ffa450edef68

--- client/java/solrj/src/org/apache/solr/client/solrj/SolrResponse.java
+++ client/java/solrj/src/org/apache/solr/client/solrj/SolrResponse.java
@@ -18,6 +18,15 @@ package|;
 end_package
 begin_import
 import|import
+name|java
+operator|.
+name|io
+operator|.
+name|Serializable
+import|;
+end_import
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -39,6 +48,8 @@ DECL|interface|SolrResponse
 specifier|public
 interface|interface
 name|SolrResponse
+extends|extends
+name|Serializable
 block|{
 DECL|method|getElapsedTime
 name|long
