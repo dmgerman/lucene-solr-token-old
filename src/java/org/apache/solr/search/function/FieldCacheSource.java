e52192b9a34f94705adbd714c583ef181b85cfd8
Grant Ingersoll <gsingers@apache.org>
2008-02-20 03:36:49 +0000

    SOLR-324: fixed support for longs/doubles, still need to add short and byte, per next Lucene update from trunk.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@629334 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/search/function/FieldCacheSource.java
+++ src/java/org/apache/solr/search/function/FieldCacheSource.java
@@ -29,6 +29,19 @@ operator|.
 name|FieldCache
 import|;
 end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|search
+operator|.
+name|ExtendedFieldCache
+import|;
+end_import
 begin_comment
 comment|/**  * A base class for ValueSource implementations that retrieve values for  * a single field from the {@link org.apache.lucene.search.FieldCache}.  *  * @version $Id$  */
 end_comment
@@ -51,9 +64,9 @@ specifier|protected
 name|FieldCache
 name|cache
 init|=
-name|FieldCache
+name|ExtendedFieldCache
 operator|.
-name|DEFAULT
+name|EXT_DEFAULT
 decl_stmt|;
 DECL|method|FieldCacheSource
 specifier|public
@@ -70,6 +83,7 @@ operator|=
 name|field
 expr_stmt|;
 block|}
+comment|/**    * If you are using longs or doubles, this needs to be a {@link org.apache.lucene.search.ExtendedFieldCache}.    *    * @param cache The {@link org.apache.lucene.search.FieldCache}    */
 DECL|method|setFieldCache
 specifier|public
 name|void
