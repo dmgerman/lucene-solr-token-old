9372b1467e88f9161113531e103faaf43b5b5b35
Ryan McKinley <ryan@apache.org>
2008-03-25 20:10:12 +0000

    make SolrServer serializable
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@640964 13f79535-47bb-0310-9956-ffa450edef68

--- client/java/solrj/src/org/apache/solr/client/solrj/SolrServer.java
+++ client/java/solrj/src/org/apache/solr/client/solrj/SolrServer.java
@@ -29,6 +29,15 @@ begin_import
 import|import
 name|java
 operator|.
+name|io
+operator|.
+name|Serializable
+import|;
+end_import
+begin_import
+import|import
+name|java
+operator|.
 name|util
 operator|.
 name|Collection
@@ -188,6 +197,8 @@ specifier|public
 specifier|abstract
 class|class
 name|SolrServer
+implements|implements
+name|Serializable
 block|{
 DECL|method|add
 specifier|public
