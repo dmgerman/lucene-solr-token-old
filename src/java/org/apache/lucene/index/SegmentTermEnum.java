d68e0da4565b2126babf703dd2444761a2a8c3e5
Michael Busch <buschmi@apache.org>
2008-05-23 17:20:31 +0000

    LUCENE-1195: Improve term lookup performance by adding a LRU cache to the TermInfosReader.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@659602 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/index/SegmentTermEnum.java
+++ src/java/org/apache/lucene/index/SegmentTermEnum.java
@@ -644,10 +644,10 @@ return|return
 literal|true
 return|;
 block|}
-comment|/** Optimized scan, without allocating new terms. */
+comment|/** Optimized scan, without allocating new terms.     *  Return number of invocations to next(). */
 DECL|method|scanTo
 specifier|final
-name|void
+name|int
 name|scanTo
 parameter_list|(
 name|Term
@@ -663,6 +663,11 @@ argument_list|(
 name|term
 argument_list|)
 expr_stmt|;
+name|int
+name|count
+init|=
+literal|0
+decl_stmt|;
 while|while
 condition|(
 name|scanBuffer
@@ -677,7 +682,14 @@ operator|&&
 name|next
 argument_list|()
 condition|)
-block|{}
+block|{
+name|count
+operator|++
+expr_stmt|;
+block|}
+return|return
+name|count
+return|;
 block|}
 comment|/** Returns the current Term in the enumeration.    Initially invalid, valid after next() called for the first time.*/
 DECL|method|term
