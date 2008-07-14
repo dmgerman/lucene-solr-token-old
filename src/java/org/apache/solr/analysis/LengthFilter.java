cfaba95a8f1453e403252fb401cbefd8b94961db
Chris M. Hostetter <hossman@apache.org>
2008-07-14 01:04:38 +0000

    SOLR-520: Deprecated unused LengthFilter since already core in Lucene-Java
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@676460 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/analysis/LengthFilter.java
+++ src/java/org/apache/solr/analysis/LengthFilter.java
@@ -63,9 +63,11 @@ name|IOException
 import|;
 end_import
 begin_comment
-comment|/**  * @version $Id$  */
+comment|/**  * @version $Id$  * @deprecated use {@link org.apache.lucene.analysis.LengthFilter}  */
 end_comment
 begin_class
+annotation|@
+name|Deprecated
 DECL|class|LengthFilter
 specifier|public
 specifier|final
