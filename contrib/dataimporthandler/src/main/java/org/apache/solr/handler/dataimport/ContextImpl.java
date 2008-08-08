b69f4d635a5b870f6aed91e4e44f7999730e6733
Shalin Shekhar Mangar <shalin@apache.org>
2008-08-08 17:50:11 +0000

    Changed mention of interface in javadocs to abstract classes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@684025 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/ContextImpl.java
+++ contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/ContextImpl.java
@@ -63,7 +63,7 @@ name|Map
 import|;
 end_import
 begin_comment
-comment|/**  *<p>  * An implementation for the Context  *</p>  *<p/>  *<b>This API is experimental and subject to change</b>  *  * @version $Id$  * @since solr 1.3  */
+comment|/**  *<p>  * An implementation for the Context  *</p>  *<b>This API is experimental and subject to change</b>  *  * @version $Id$  * @since solr 1.3  */
 end_comment
 begin_class
 DECL|class|ContextImpl
