1604b99dc34ffad601e8744372a8f88865029217
Mark Harwood <mharwood@apache.org>
2008-09-26 21:44:55 +0000

    Fixed bug in FuzzyLikeThisQuery.java. Queries that contain a term with no fuzzy variants caused the query construction logic to exit loop early, producing no fuzzy variants for all subsequent terms in the query string. Junit test added which recreates the problem conditions and added fix to FuzzyLikeThisQuery that solves the issue.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@699512 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/queries/src/java/org/apache/lucene/search/FuzzyLikeThisQuery.java
+++ contrib/queries/src/java/org/apache/lucene/search/FuzzyLikeThisQuery.java
@@ -653,13 +653,10 @@ do|;
 if|if
 condition|(
 name|numVariants
-operator|==
+operator|>
 literal|0
 condition|)
 block|{
-comment|//no variants to rank here
-break|break;
-block|}
 name|int
 name|avgDf
 init|=
@@ -682,7 +679,8 @@ expr_stmt|;
 comment|//use avg df of all variants
 block|}
 comment|// take the top variants (scored by edit distance) and reset the score
-comment|// to include an IDF factor then add to the global queue for ranking overall top query terms
+comment|// to include an IDF factor then add to the global queue for ranking
+comment|// overall top query terms
 name|int
 name|size
 init|=
@@ -747,6 +745,7 @@ argument_list|(
 name|st
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 block|}
 block|}
