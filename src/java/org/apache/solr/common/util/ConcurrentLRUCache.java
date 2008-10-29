e857d56b587a9d1ace692b7b25e4770685ec7db3
Shalin Shekhar Mangar <shalin@apache.org>
2008-10-29 17:21:59 +0000

    Finishing a half-sentence in the Javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@708946 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/common/util/ConcurrentLRUCache.java
+++ src/java/org/apache/solr/common/util/ConcurrentLRUCache.java
@@ -91,7 +91,7 @@ name|ReentrantLock
 import|;
 end_import
 begin_comment
-comment|/**  * A LRU cache implementation based upon ConcurrentHashMap and other techniques to reduce  * contention and synchronization overhead to utilize multiple CPU cores more effectively.  *  * Note that the implementation does not follow a true LRU (least-recently-used) eviction  * strategy. Instead it strives to   *  * @version $Id$  * @since solr 1.4  */
+comment|/**  * A LRU cache implementation based upon ConcurrentHashMap and other techniques to reduce  * contention and synchronization overhead to utilize multiple CPU cores more effectively.  *  * Note that the implementation does not follow a true LRU (least-recently-used) eviction  * strategy. Instead it strives to remove least recently used items but when the initial  * cleanup does not remove enough items to reach the 'acceptableWaterMark' limit, it can  * remove more items forcefully regardless of access order.  *  * @version $Id$  * @since solr 1.4  */
 end_comment
 begin_class
 DECL|class|ConcurrentLRUCache
