7eeee0ed1d7a7df92288f7cedfab9b0033aeffc5
Uwe Schindler <uschindler@apache.org>
2009-01-14 15:09:25 +0000

    Trie encoded fields do not need norms (as they are only used for filtering with TrieRangeFilter or for sorting)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@734414 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/queries/src/java/org/apache/lucene/search/trie/TrieUtils.java
+++ contrib/queries/src/java/org/apache/lucene/search/trie/TrieUtils.java
@@ -1221,7 +1221,7 @@ name|Field
 operator|.
 name|Index
 operator|.
-name|NOT_ANALYZED
+name|NOT_ANALYZED_NO_NORMS
 else|:
 name|Field
 operator|.
@@ -1337,7 +1337,7 @@ name|Field
 operator|.
 name|Index
 operator|.
-name|NOT_ANALYZED
+name|NOT_ANALYZED_NO_NORMS
 argument_list|)
 expr_stmt|;
 name|f
