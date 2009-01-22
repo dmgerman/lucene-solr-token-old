4e1b5cfb4e8363d50bd9254a3c335aaa2163c8f5
Shalin Shekhar Mangar <shalin@apache.org>
2009-01-22 19:08:47 +0000

    Log the file names which could not be deleted
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@736746 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/handler/SnapPuller.java
+++ src/java/org/apache/solr/handler/SnapPuller.java
@@ -3464,6 +3464,11 @@ condition|)
 return|return
 literal|false
 return|;
+name|boolean
+name|isSuccess
+init|=
+literal|true
+decl_stmt|;
 name|File
 name|contents
 index|[]
@@ -3509,9 +3514,21 @@ condition|(
 operator|!
 name|success
 condition|)
-return|return
+block|{
+name|LOG
+operator|.
+name|warn
+argument_list|(
+literal|"Unable to delete directory : "
+operator|+
+name|file
+argument_list|)
+expr_stmt|;
+name|isSuccess
+operator|=
 literal|false
-return|;
+expr_stmt|;
+block|}
 block|}
 else|else
 block|{
@@ -3528,13 +3545,30 @@ condition|(
 operator|!
 name|success
 condition|)
+block|{
+name|LOG
+operator|.
+name|warn
+argument_list|(
+literal|"Unable to delete file : "
+operator|+
+name|file
+argument_list|)
+expr_stmt|;
+name|isSuccess
+operator|=
+literal|false
+expr_stmt|;
 return|return
 literal|false
 return|;
 block|}
 block|}
 block|}
+block|}
 return|return
+name|isSuccess
+operator|&&
 name|dir
 operator|.
 name|delete
@@ -4752,6 +4786,19 @@ name|e
 parameter_list|)
 block|{
 comment|/* noop */
+name|LOG
+operator|.
+name|error
+argument_list|(
+literal|"Error closing the file stream: "
+operator|+
+name|this
+operator|.
+name|saveAs
+argument_list|,
+name|e
+argument_list|)
+expr_stmt|;
 block|}
 try|try
 block|{
