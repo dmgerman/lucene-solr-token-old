7c56415f52ea6be4f27bf420c4d2b25b4f914eba
Michael McCandless <mikemccand@apache.org>
2009-02-25 11:02:57 +0000

    make TopFieldDocs ctor public
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@747745 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/TopFieldDocs.java
+++ src/java/org/apache/lucene/search/TopFieldDocs.java
@@ -34,6 +34,7 @@ name|fields
 decl_stmt|;
 comment|/** Creates one of these objects. 	 * @param totalHits  Total number of hits for the query. 	 * @param scoreDocs  The top hits for the query. 	 * @param fields     The sort criteria used to find the top hits. 	 * @param maxScore   The maximum score encountered. 	 */
 DECL|method|TopFieldDocs
+specifier|public
 name|TopFieldDocs
 parameter_list|(
 name|int
