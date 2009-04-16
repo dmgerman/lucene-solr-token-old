eed406afdbc24bd34bea023a115304b71d3fc69a
Shalin Shekhar Mangar <shalin@apache.org>
2009-04-16 06:29:53 +0000

    SOLR-1096 followup -- Change default connection timeout to 5 seconds and read timeout to 20 seconds
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@765483 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/handler/SnapPuller.java
+++ src/java/org/apache/solr/handler/SnapPuller.java
@@ -635,7 +635,7 @@ name|connTimeout
 operator|==
 literal|null
 condition|?
-literal|5000
+literal|20000
 else|:
 name|Integer
 operator|.
@@ -645,7 +645,7 @@ name|connTimeout
 argument_list|)
 argument_list|)
 expr_stmt|;
-comment|//5 secs
+comment|//20 secs
 name|mgr
 operator|.
 name|getParams
@@ -657,7 +657,7 @@ name|readTimeout
 operator|==
 literal|null
 condition|?
-literal|10000
+literal|5000
 else|:
 name|Integer
 operator|.
@@ -667,7 +667,7 @@ name|readTimeout
 argument_list|)
 argument_list|)
 expr_stmt|;
-comment|//10 secs
+comment|//5 secs
 name|HttpClient
 name|httpClient
 init|=
