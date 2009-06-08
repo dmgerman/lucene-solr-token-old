3ff8f9b6873ef4e2c2351880231afdb613603d45
Mark Robert Miller <markrmiller@apache.org>
2009-06-08 13:31:57 +0000

    cleanup - searcher is set to null and closed if not null, but never set to not null
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@782619 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/spelling/FileBasedSpellChecker.java
+++ src/java/org/apache/solr/spelling/FileBasedSpellChecker.java
@@ -433,11 +433,6 @@ name|SolrResourceLoader
 name|loader
 parameter_list|)
 block|{
-name|IndexSearcher
-name|searcher
-init|=
-literal|null
-decl_stmt|;
 try|try
 block|{
 comment|// Get the field's analyzer
@@ -668,31 +663,6 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
-block|}
-finally|finally
-block|{
-try|try
-block|{
-if|if
-condition|(
-name|searcher
-operator|!=
-literal|null
-condition|)
-name|searcher
-operator|.
-name|close
-argument_list|()
-expr_stmt|;
-block|}
-catch|catch
-parameter_list|(
-name|IOException
-name|e
-parameter_list|)
-block|{
-comment|// Ignore
-block|}
 block|}
 block|}
 DECL|method|getCharacterEncoding
