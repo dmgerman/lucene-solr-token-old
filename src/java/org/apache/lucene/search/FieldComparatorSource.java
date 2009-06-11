b90d1d9a3aeb9e0ce3b8f16c90b1490af4d4f810
Mark Robert Miller <markrmiller@apache.org>
2009-06-11 01:45:00 +0000

    LUCENE-1598: FieldComparatorSource should be Serializable for back compat with SortComparatorSource
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@783598 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/FieldComparatorSource.java
+++ src/java/org/apache/lucene/search/FieldComparatorSource.java
@@ -23,6 +23,15 @@ operator|.
 name|IOException
 import|;
 end_import
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|Serializable
+import|;
+end_import
 begin_comment
 comment|/**  * Provides a {@link FieldComparator} for custom field sorting.  *  *<b>NOTE:</b> This API is experimental and might change in  * incompatible ways in the next release.  *  */
 end_comment
@@ -32,6 +41,8 @@ specifier|public
 specifier|abstract
 class|class
 name|FieldComparatorSource
+implements|implements
+name|Serializable
 block|{
 comment|/**    * Creates a comparator for the field in the given index.    *     * @param fieldname    *          Name of the field to create comparator for.    * @return FieldComparator.    * @throws IOException    *           If an error occurs reading the index.    */
 DECL|method|newComparator
