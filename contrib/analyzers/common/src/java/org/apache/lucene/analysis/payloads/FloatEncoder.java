454665757909ee8321f2dfe2c441d8afe0103119
Grant Ingersoll <gsingers@apache.org>
2009-08-05 13:17:20 +0000

    Javadoc updates
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@801219 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/analyzers/common/src/java/org/apache/lucene/analysis/payloads/FloatEncoder.java
+++ contrib/analyzers/common/src/java/org/apache/lucene/analysis/payloads/FloatEncoder.java
@@ -30,7 +30,7 @@ name|Payload
 import|;
 end_import
 begin_comment
-comment|/**  *  Encode a character array Float as a {@link org.apache.lucene.index.Payload}.  *  * @see {@link org.apache.lucene.analysis.payloads.PayloadHelper#encodeFloat(float, byte[], int)}  *  **/
+comment|/**  *  Encode a character array Float as a {@link org.apache.lucene.index.Payload}.  *<p/>  * @see {@link org.apache.lucene.analysis.payloads.PayloadHelper#encodeFloat(float, byte[], int)}  *  **/
 end_comment
 begin_class
 DECL|class|FloatEncoder
