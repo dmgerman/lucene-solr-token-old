320bab6f70bae5ee1f079b05ddd5df10e69e5419
Grant Ingersoll <gsingers@apache.org>
2009-08-07 23:57:25 +0000

    LUCENE-1790: flip around for back compatibility
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@802272 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/Similarity.java
+++ src/java/org/apache/lucene/search/Similarity.java
@@ -474,6 +474,7 @@ name|maxOverlap
 parameter_list|)
 function_decl|;
 comment|/**    * Calculate a scoring factor based on the data in the payload.  Overriding implementations    * are responsible for interpreting what is in the payload.  Lucene makes no assumptions about    * what is in the byte array.    *<p>    * The default implementation returns 1.    *    * @param fieldName The fieldName of the term this payload belongs to    * @param payload The payload byte array to be scored    * @param offset The offset into the payload array    * @param length The length in the array    * @return An implementation dependent float to be used as a scoring factor    *    * @deprecated See {@link #scorePayload(int, String, byte[], int, int)}    */
+comment|//TODO: When removing this, set the default value below to return 1.
 DECL|method|scorePayload
 specifier|public
 name|float
@@ -495,18 +496,7 @@ parameter_list|)
 block|{
 comment|//Do nothing
 return|return
-name|scorePayload
-argument_list|(
-name|NO_DOC_ID_PROVIDED
-argument_list|,
-name|fieldName
-argument_list|,
-name|payload
-argument_list|,
-name|offset
-argument_list|,
-name|length
-argument_list|)
+literal|1
 return|;
 block|}
 comment|/**    * Calculate a scoring factor based on the data in the payload.  Overriding implementations    * are responsible for interpreting what is in the payload.  Lucene makes no assumptions about    * what is in the byte array.    *<p>    * The default implementation returns 1.    *    * @param docId The docId currently being scored.  If this value is {@link #NO_DOC_ID_PROVIDED}, then it should be assumed that the PayloadQuery implementation does not provide document information    * @param fieldName The fieldName of the term this payload belongs to    * @param payload The payload byte array to be scored    * @param offset The offset into the payload array    * @param length The length in the array    * @return An implementation dependent float to be used as a scoring factor    *    */
@@ -532,9 +522,18 @@ name|int
 name|length
 parameter_list|)
 block|{
-comment|//Do nothing
+comment|//TODO: When removing the deprecated scorePayload above, set this to return 1
 return|return
-literal|1
+name|scorePayload
+argument_list|(
+name|fieldName
+argument_list|,
+name|payload
+argument_list|,
+name|offset
+argument_list|,
+name|length
+argument_list|)
 return|;
 block|}
 block|}
