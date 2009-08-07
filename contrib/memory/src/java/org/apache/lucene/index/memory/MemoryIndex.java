9e0a54cdea0b2c9e8de14cc393580a3d508275f8
Uwe Schindler <uschindler@apache.org>
2009-08-07 17:19:53 +0000

    LUCENE-1607: Change some more String.intern() in contrib
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@802095 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/memory/src/java/org/apache/lucene/index/memory/MemoryIndex.java
+++ contrib/memory/src/java/org/apache/lucene/index/memory/MemoryIndex.java
@@ -875,7 +875,6 @@ name|float
 name|boost
 parameter_list|)
 block|{
-comment|/*      * Note that this method signature avoids having a user call new      * o.a.l.d.Field(...) which would be much too expensive due to the      * String.intern() usage of that class.      *       * More often than not, String.intern() leads to serious performance      * degradations rather than improvements! If you're curious why, check      * out the JDK's native code, see how it oscillates multiple times back      * and forth between Java code and native code on each intern() call,      * only to end up using a plain vanilla java.util.HashMap on the Java      * heap for it's interned strings! String.equals() has a small cost      * compared to String.intern(), trust me. Application level interning      * (e.g. a HashMap per Directory/Index) typically leads to better      * solutions than frequent hidden low-level calls to String.intern().      *       * Perhaps with some luck, Lucene's Field.java (and Term.java) and      * cousins could be fixed to not use String.intern(). Sigh :-(      */
 try|try
 block|{
 if|if
