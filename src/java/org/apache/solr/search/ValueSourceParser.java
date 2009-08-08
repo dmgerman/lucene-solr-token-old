8dd6b155a68e61374e38007841cff6d731f3f4b9
Yonik Seeley <yonik@apache.org>
2009-08-08 21:33:28 +0000

    SOLR-1259: fix scale function
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@802458 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/search/ValueSourceParser.java
+++ src/java/org/apache/solr/search/ValueSourceParser.java
@@ -577,6 +577,9 @@ argument_list|()
 decl_stmt|;
 return|return
 operator|new
+name|TopValueSource
+argument_list|(
+operator|new
 name|ScaleFloatFunction
 argument_list|(
 name|source
@@ -584,6 +587,7 @@ argument_list|,
 name|min
 argument_list|,
 name|max
+argument_list|)
 argument_list|)
 return|;
 block|}
