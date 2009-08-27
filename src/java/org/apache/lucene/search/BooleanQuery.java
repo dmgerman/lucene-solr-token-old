6c109820bdc1490ee9fd3c93b3b1fd1abf99b230
Mark Robert Miller <markrmiller@apache.org>
2009-08-27 01:32:15 +0000

    with LUCENE-1860 there is no real need for this comment - reducing to core info
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@808246 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/BooleanQuery.java
+++ src/java/org/apache/lucene/search/BooleanQuery.java
@@ -131,7 +131,7 @@ return|return
 name|maxClauseCount
 return|;
 block|}
-comment|/** Set the maximum number of clauses permitted per BooleanQuery.    * Default value is 1024.    *<p>TermQuery clauses are generated from for example prefix queries and    * fuzzy queries. Each TermQuery needs some buffer space during search,    * so this parameter indirectly controls the maximum buffer requirements for    * query search.    *<p>When this parameter becomes a bottleneck for a Query one can use a    * Filter. For example instead of a {@link TermRangeQuery} one can use a    * {@link TermRangeFilter}.    *<p>Normally the buffers are allocated by the JVM. When using for example    * {@link org.apache.lucene.store.MMapDirectory} the buffering is left to    * the operating system.    */
+comment|/**     * Set the maximum number of clauses permitted per BooleanQuery.    * Default value is 1024.    */
 DECL|method|setMaxClauseCount
 specifier|public
 specifier|static
