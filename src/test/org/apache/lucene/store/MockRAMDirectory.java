82629298c558cab491b8add95b9a92586da57bad
Mark Robert Miller <markrmiller@apache.org>
2009-08-27 01:43:56 +0000

    javadoc spell fixes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@808251 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lucene/store/MockRAMDirectory.java
+++ src/test/org/apache/lucene/store/MockRAMDirectory.java
@@ -114,7 +114,7 @@ name|Arrays
 import|;
 end_import
 begin_comment
-comment|/**  * This is a subclass of RAMDirectory that adds methods  * intented to be used only by unit tests.  * @version $Id: RAMDirectory.java 437897 2006-08-29 01:13:10Z yonik $  */
+comment|/**  * This is a subclass of RAMDirectory that adds methods  * intended to be used only by unit tests.  * @version $Id: RAMDirectory.java 437897 2006-08-29 01:13:10Z yonik $  */
 end_comment
 begin_class
 DECL|class|MockRAMDirectory
@@ -347,7 +347,7 @@ name|name
 argument_list|)
 expr_stmt|;
 block|}
-comment|/** Simulates a crash of OS or machine by overwriting    *  unsycned files. */
+comment|/** Simulates a crash of OS or machine by overwriting    *  unsynced files. */
 DECL|method|crash
 specifier|public
 specifier|synchronized
