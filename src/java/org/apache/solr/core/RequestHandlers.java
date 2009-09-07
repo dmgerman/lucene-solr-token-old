6e6bb6d8df04907174ebc88b258363908657a248
Noble Paul <noble@apache.org>
2009-09-07 07:45:57 +0000

    SOLR-1198 moved all fields and getter methods to one map and a getter by classname
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@812023 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/core/RequestHandlers.java
+++ src/java/org/apache/solr/core/RequestHandlers.java
@@ -547,8 +547,15 @@ name|info
 range|:
 name|config
 operator|.
-name|getReqHandlerInfo
+name|getPluginInfos
+argument_list|(
+name|SolrRequestHandler
+operator|.
+name|class
+operator|.
+name|getName
 argument_list|()
+argument_list|)
 control|)
 block|{
 try|try
