dfb1a8e685af437bef89565da3d70e8a8f2f210a
Michael McCandless <mikemccand@apache.org>
2009-09-22 00:08:21 +0000

    LUCENE-1781: fix various issues with lat/lng bounding box computation for contrib/spatial
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@817456 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/spatial/src/java/org/apache/lucene/spatial/geometry/shape/DistanceApproximation.java
+++ contrib/spatial/src/java/org/apache/lucene/spatial/geometry/shape/DistanceApproximation.java
@@ -19,7 +19,7 @@ name|shape
 package|;
 end_package
 begin_comment
-comment|/**  * Imported from mq java client.  No changes made.  *  *<p><font color="red"><b>NOTE:</b> This API is still in  * flux and might change in incompatible ways in the next  * release.</font>  */
+comment|/**  * Imported from mq java client.  No changes made.  *  *<p><font color="red"><b>NOTE:</b> This API is still in  * flux and might change in incompatible ways in the next  * release.</font>  *  * @deprecated This has been replaced with more accurate  * math in {@link LLRect}.  */
 end_comment
 begin_class
 DECL|class|DistanceApproximation
