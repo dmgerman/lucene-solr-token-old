2224551b1305acf4e7170e17907146475d78edbf
Noble Paul <noble@apache.org>
2009-10-09 04:06:09 +0000

    SOLR-1500 Jdbc datasource fails with Oracle driver
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@823398 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/JdbcDataSource.java
+++ contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/JdbcDataSource.java
@@ -1207,7 +1207,16 @@ name|CLOSE_CURSORS_AT_COMMIT
 argument_list|)
 expr_stmt|;
 block|}
-else|else
+elseif|else
+if|if
+condition|(
+literal|"HOLD_CURSORS_OVER_COMMIT"
+operator|.
+name|equals
+argument_list|(
+name|holdability
+argument_list|)
+condition|)
 block|{
 name|c
 operator|.
