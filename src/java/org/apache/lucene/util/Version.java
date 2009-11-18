dac99c33a6121906076f5ae792c8b549d05c7b73
Robert Muir <rmuir@apache.org>
2009-11-18 16:06:06 +0000

    LUCENE-2080: Improve Version javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@881819 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/util/Version.java
+++ src/java/org/apache/lucene/util/Version.java
@@ -15,7 +15,7 @@ begin_comment
 comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/**  * Use by certain classes to match version compatibility  * across releases of Lucene.  */
+comment|/**  * Use by certain classes to match version compatibility  * across releases of Lucene.  *   *<p><b>WARNING</b>: When changing the version parameter  * that you supply to components in Lucene, do not simply  * change the version at search-time, but instead also adjust  * your indexing code to match, and re-index.  */
 end_comment
 begin_enum
 DECL|enum|Version
