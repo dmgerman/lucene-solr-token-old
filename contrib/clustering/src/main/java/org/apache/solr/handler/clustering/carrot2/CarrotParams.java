f205551917d35dfb3fdfc9f1ccd127c099e3b446
Grant Ingersoll <gsingers@apache.org>
2010-01-04 14:28:59 +0000

    SOLR-1692: fix produceSummary issue with Carrot2 clustering
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@895640 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/clustering/src/main/java/org/apache/solr/handler/clustering/carrot2/CarrotParams.java
+++ contrib/clustering/src/main/java/org/apache/solr/handler/clustering/carrot2/CarrotParams.java
@@ -37,6 +37,21 @@ operator|.
 name|ImmutableSet
 import|;
 end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
+name|common
+operator|.
+name|params
+operator|.
+name|HighlightParams
+import|;
+end_import
 begin_comment
 comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
@@ -108,6 +123,14 @@ name|CARROT_PREFIX
 operator|+
 literal|"outputSubClusters"
 decl_stmt|;
+DECL|field|SUMMARY_FRAGSIZE
+name|String
+name|SUMMARY_FRAGSIZE
+init|=
+name|CARROT_PREFIX
+operator|+
+literal|"fragzise"
+decl_stmt|;
 DECL|field|CARROT_PARAM_NAMES
 specifier|public
 specifier|static
@@ -135,6 +158,8 @@ argument_list|,
 name|NUM_DESCRIPTIONS
 argument_list|,
 name|OUTPUT_SUB_CLUSTERS
+argument_list|,
+name|SUMMARY_FRAGSIZE
 argument_list|)
 decl_stmt|;
 block|}
