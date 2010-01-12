8ef6f49728e23bc61de80cb168fcdff36b1d3e88
Noble Paul <noble@apache.org>
2010-01-12 07:53:59 +0000

    SOLR-1352 . Multithreaded implementation (documentation fix)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@898211 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/ThreadedEntityProcessorWrapper.java
+++ contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/ThreadedEntityProcessorWrapper.java
@@ -1,4 +1,7 @@
 begin_unit
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 begin_package
 DECL|package|org.apache.solr.handler.dataimport
 package|package
@@ -127,7 +130,7 @@ name|Collections
 import|;
 end_import
 begin_comment
-comment|/**  * Each Entity may have only a single EntityProcessor .  But the same entity can be run by  * multiple EntityProcessorWrapper (1 per thread) . thhis helps running transformations in multiple threads  */
+comment|/**  * Each Entity may have only a single EntityProcessor .  But the same entity can be run by  * multiple EntityProcessorWrapper (1 per thread) . thhis helps running transformations in multiple threads  * @since Solr 1.5  */
 end_comment
 begin_class
 DECL|class|ThreadedEntityProcessorWrapper
