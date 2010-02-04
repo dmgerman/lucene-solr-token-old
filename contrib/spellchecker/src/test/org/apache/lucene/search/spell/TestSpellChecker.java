c38b04fd94e8dd70f8b8592e6abd065cf7fd21a6
Simon Willnauer <simonw@apache.org>
2010-02-04 10:23:42 +0000

    updated test after clover instrumentation run failure. Thread termination timeout set to 60 seconds and made booleans in workers volatile.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@906441 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/spellchecker/src/test/org/apache/lucene/search/spell/TestSpellChecker.java
+++ contrib/spellchecker/src/test/org/apache/lucene/search/spell/TestSpellChecker.java
@@ -1840,11 +1840,12 @@ operator|.
 name|shutdown
 argument_list|()
 expr_stmt|;
+comment|// wait for 60 seconds - usually this is very fast but coverage runs could take quite long
 name|executor
 operator|.
 name|awaitTermination
 argument_list|(
-literal|5
+literal|60L
 argument_list|,
 name|TimeUnit
 operator|.
@@ -1870,6 +1871,15 @@ control|)
 block|{
 name|assertFalse
 argument_list|(
+name|String
+operator|.
+name|format
+argument_list|(
+literal|"worker thread %d failed"
+argument_list|,
+name|i
+argument_list|)
+argument_list|,
 name|workers
 index|[
 name|i
@@ -1880,6 +1890,15 @@ argument_list|)
 expr_stmt|;
 name|assertTrue
 argument_list|(
+name|String
+operator|.
+name|format
+argument_list|(
+literal|"worker thread %d is still running but should be terminated"
+argument_list|,
+name|i
+argument_list|)
+argument_list|,
 name|workers
 index|[
 name|i
@@ -2103,12 +2122,14 @@ name|IndexReader
 name|reader
 decl_stmt|;
 DECL|field|terminated
+specifier|volatile
 name|boolean
 name|terminated
 init|=
 literal|false
 decl_stmt|;
 DECL|field|failed
+specifier|volatile
 name|boolean
 name|failed
 init|=
