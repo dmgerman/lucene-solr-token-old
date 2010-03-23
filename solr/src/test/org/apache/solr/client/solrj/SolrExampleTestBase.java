d7a7d62e93a1b25ebacb31fe33de0e7c34d9fff7
Yonik Seeley <yonik@apache.org>
2010-03-23 21:32:01 +0000

    tests: ignore expected exceptions
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@926793 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/client/solrj/SolrExampleTestBase.java
+++ solr/src/test/org/apache/solr/client/solrj/SolrExampleTestBase.java
@@ -91,6 +91,11 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|ignoreException
+argument_list|(
+literal|"maxWarmingSearchers"
+argument_list|)
+expr_stmt|;
 name|super
 operator|.
 name|setUp
