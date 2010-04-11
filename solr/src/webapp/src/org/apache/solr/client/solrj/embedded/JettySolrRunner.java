75755528db44fa12e0a69e4cc27cc72092cc26c1
Mark Robert Miller <markrmiller@apache.org>
2010-04-11 02:15:32 +0000

    remove troublesome $id
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@932828 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/webapp/src/org/apache/solr/client/solrj/embedded/JettySolrRunner.java
+++ solr/src/webapp/src/org/apache/solr/client/solrj/embedded/JettySolrRunner.java
@@ -162,7 +162,7 @@ name|Logger
 import|;
 end_import
 begin_comment
-comment|/**  * Run solr using jetty  *   * @version $Id$  * @since solr 1.3  */
+comment|/**  * Run solr using jetty  *   * @since solr 1.3  */
 end_comment
 begin_class
 DECL|class|JettySolrRunner
