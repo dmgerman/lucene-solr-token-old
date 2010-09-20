4b351d5dfaae310ce8445ce5cb94dc963ed643b7
Robert Muir <rmuir@apache.org>
2010-09-20 17:27:15 +0000

    LUCENE-2656: if tests fail, don't report about unclosed resources
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@999016 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/src/test/org/apache/lucene/util/LuceneTestCase.java
@@ -1783,6 +1783,10 @@ argument_list|(
 name|timeZone
 argument_list|)
 expr_stmt|;
+name|testsFailed
+operator|=
+literal|false
+expr_stmt|;
 block|}
 annotation|@
 name|AfterClass
@@ -1827,6 +1831,11 @@ literal|"solr.data.dir"
 argument_list|)
 expr_stmt|;
 comment|// now look for unclosed resources
+if|if
+condition|(
+operator|!
+name|testsFailed
+condition|)
 for|for
 control|(
 name|MockDirectoryWrapper
@@ -1889,6 +1898,13 @@ operator|=
 literal|null
 expr_stmt|;
 block|}
+DECL|field|testsFailed
+specifier|private
+specifier|static
+name|boolean
+name|testsFailed
+decl_stmt|;
+comment|/* true if any tests failed */
 comment|// This is how we get control when errors occur.
 comment|// Think of this as start/end/success/failed
 comment|// events.
@@ -1917,6 +1933,10 @@ name|FrameworkMethod
 name|method
 parameter_list|)
 block|{
+name|testsFailed
+operator|=
+literal|true
+expr_stmt|;
 name|reportAdditionalFailureInfo
 argument_list|()
 expr_stmt|;
