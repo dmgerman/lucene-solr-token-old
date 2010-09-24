3c8ff0d9119ed58847333c0b4889278c658baf6a
Yonik Seeley <yonik@apache.org>
2010-09-24 01:17:28 +0000

    remove obsolete scorer methods, add @Override
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1000685 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/search/function/FunctionQuery.java
+++ solr/src/java/org/apache/solr/search/function/FunctionQuery.java
@@ -150,6 +150,8 @@ return|return
 name|func
 return|;
 block|}
+annotation|@
+name|Override
 DECL|method|rewrite
 specifier|public
 name|Query
@@ -165,6 +167,8 @@ return|return
 name|this
 return|;
 block|}
+annotation|@
+name|Override
 DECL|method|extractTerms
 specifier|public
 name|void
@@ -236,6 +240,8 @@ name|searcher
 argument_list|)
 expr_stmt|;
 block|}
+annotation|@
+name|Override
 DECL|method|getQuery
 specifier|public
 name|Query
@@ -248,6 +254,8 @@ operator|.
 name|this
 return|;
 block|}
+annotation|@
+name|Override
 DECL|method|getValue
 specifier|public
 name|float
@@ -580,12 +588,12 @@ return|return
 name|doc
 return|;
 block|}
-annotation|@
-name|Override
 comment|// instead of matching all docs, we could also embed a query.
 comment|// the score could either ignore the subscore, or boost it.
 comment|// Containment:  floatline(foo:myTerm, "myFloatField", 1.0, 0.0f)
 comment|// Boost:        foo:myTerm^floatline("myFloatField",1.0,0.0f)
+annotation|@
+name|Override
 DECL|method|nextDoc
 specifier|public
 name|int
@@ -658,69 +666,8 @@ name|nextDoc
 argument_list|()
 return|;
 block|}
-comment|// instead of matching all docs, we could also embed a query.
-comment|// the score could either ignore the subscore, or boost it.
-comment|// Containment:  floatline(foo:myTerm, "myFloatField", 1.0, 0.0f)
-comment|// Boost:        foo:myTerm^floatline("myFloatField",1.0,0.0f)
-DECL|method|next
-specifier|public
-name|boolean
-name|next
-parameter_list|()
-throws|throws
-name|IOException
-block|{
-for|for
-control|(
-init|;
-condition|;
-control|)
-block|{
-operator|++
-name|doc
-expr_stmt|;
-if|if
-condition|(
-name|doc
-operator|>=
-name|maxDoc
-condition|)
-block|{
-return|return
-literal|false
-return|;
-block|}
-if|if
-condition|(
-name|hasDeletions
-operator|&&
-name|delDocs
-operator|.
-name|get
-argument_list|(
-name|doc
-argument_list|)
-condition|)
-continue|continue;
-comment|// todo: maybe allow score() to throw a specific exception
-comment|// and continue on to the next document if it is thrown...
-comment|// that may be useful, but exceptions aren't really good
-comment|// for flow control.
-return|return
-literal|true
-return|;
-block|}
-block|}
-DECL|method|doc
-specifier|public
-name|int
-name|doc
-parameter_list|()
-block|{
-return|return
-name|doc
-return|;
-block|}
+annotation|@
+name|Override
 DECL|method|score
 specifier|public
 name|float
@@ -757,28 +704,6 @@ operator|-
 name|Float
 operator|.
 name|MAX_VALUE
-return|;
-block|}
-DECL|method|skipTo
-specifier|public
-name|boolean
-name|skipTo
-parameter_list|(
-name|int
-name|target
-parameter_list|)
-throws|throws
-name|IOException
-block|{
-name|doc
-operator|=
-name|target
-operator|-
-literal|1
-expr_stmt|;
-return|return
-name|next
-argument_list|()
 return|;
 block|}
 DECL|method|explain
@@ -867,6 +792,8 @@ name|result
 return|;
 block|}
 block|}
+annotation|@
+name|Override
 DECL|method|createWeight
 specifier|public
 name|Weight
@@ -889,6 +816,8 @@ argument_list|)
 return|;
 block|}
 comment|/** Prints a user-readable version of this query. */
+annotation|@
+name|Override
 DECL|method|toString
 specifier|public
 name|String
@@ -934,6 +863,8 @@ operator|)
 return|;
 block|}
 comment|/** Returns true if<code>o</code> is equal to this. */
+annotation|@
+name|Override
 DECL|method|equals
 specifier|public
 name|boolean
@@ -989,6 +920,8 @@ argument_list|)
 return|;
 block|}
 comment|/** Returns a hash code value for this object. */
+annotation|@
+name|Override
 DECL|method|hashCode
 specifier|public
 name|int
