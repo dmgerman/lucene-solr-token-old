e1db4f774655dd2b11ce70f1f5836a2d50dee29d
Simon Willnauer <simonw@apache.org>
2010-09-29 16:35:48 +0000

    LUCENE-2677: Tests failing when run with tests.iter > 1
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1002724 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestFieldsReader.java
+++ lucene/src/test/org/apache/lucene/index/TestFieldsReader.java
@@ -434,6 +434,12 @@ operator|.
 name|close
 argument_list|()
 expr_stmt|;
+name|FaultyIndexInput
+operator|.
+name|doFail
+operator|=
+literal|false
+expr_stmt|;
 block|}
 annotation|@
 name|Override
