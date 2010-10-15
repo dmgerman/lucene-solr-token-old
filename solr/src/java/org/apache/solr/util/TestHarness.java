08c2d69833d3ba7df8916b6985fb0f053057928a
Yonik Seeley <yonik@apache.org>
2010-10-15 00:42:46 +0000

    tests: close request in a finally block
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1022790 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/util/TestHarness.java
+++ solr/src/java/org/apache/solr/util/TestHarness.java
@@ -1236,7 +1236,7 @@ name|req
 argument_list|)
 return|;
 block|}
-comment|/**    * Processes a "query" using a user constructed SolrQueryRequest    *    * @param handler the name of the request handler to process the request    * @param req the Query to process, will be closed.    * @return The XML response to the query    * @exception Exception any exception in the response.    * @exception IOException if there is a problem writing the XML    * @see LocalSolrQueryRequest    */
+comment|/**    * Processes a "query" using a user constructed SolrQueryRequest, and closes the request at the end.    *    * @param handler the name of the request handler to process the request    * @param req the Query to process, will be closed.    * @return The XML response to the query    * @exception Exception any exception in the response.    * @exception IOException if there is a problem writing the XML    * @see LocalSolrQueryRequest    */
 DECL|method|query
 specifier|public
 name|String
@@ -1252,6 +1252,8 @@ throws|throws
 name|IOException
 throws|,
 name|Exception
+block|{
+try|try
 block|{
 name|SolrQueryResponse
 name|rsp
@@ -1305,6 +1307,16 @@ name|toString
 argument_list|()
 return|;
 block|}
+finally|finally
+block|{
+name|req
+operator|.
+name|close
+argument_list|()
+expr_stmt|;
+block|}
+block|}
+comment|/** It is the users responsibility to close the request object when done with it */
 DECL|method|queryAndResponse
 specifier|public
 name|SolrQueryResponse
