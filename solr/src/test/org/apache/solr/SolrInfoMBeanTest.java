826fa9581d2253138337e07bee9c67a6c023e5e5
Robert Muir <rmuir@apache.org>
2010-10-17 21:30:53 +0000

    disable this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1023587 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/SolrInfoMBeanTest.java
+++ solr/src/test/org/apache/solr/SolrInfoMBeanTest.java
@@ -124,6 +124,15 @@ import|;
 end_import
 begin_import
 import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Ignore
+import|;
+end_import
+begin_import
+import|import
 name|java
 operator|.
 name|io
@@ -179,6 +188,11 @@ extends|extends
 name|LuceneTestCase
 block|{
 comment|/**    * Gets a list of everything we can find in the classpath and makes sure it has    * a name, description, etc...    */
+annotation|@
+name|Ignore
+argument_list|(
+literal|"meddles with unrelated tests"
+argument_list|)
 DECL|method|testCallMBeanInfo
 specifier|public
 name|void
