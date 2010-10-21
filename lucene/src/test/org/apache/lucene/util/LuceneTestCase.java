c41e10c5163783756d7b95a8ec46865e532423c0
Uwe Schindler <uschindler@apache.org>
2010-10-21 22:29:13 +0000

    remove warning
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1026167 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/src/test/org/apache/lucene/util/LuceneTestCase.java
@@ -1824,6 +1824,8 @@ return|;
 block|}
 block|}
 comment|/** @deprecated: until we fix no-fork problems in solr tests */
+annotation|@
+name|Deprecated
 DECL|field|testClassesRun
 specifier|private
 specifier|static
