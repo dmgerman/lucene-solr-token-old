9ed2b5c74e1c276d0fdd0a59b6aca61c253b7a8e
Michael McCandless <mikemccand@apache.org>
2010-11-26 11:25:42 +0000

    LUCENE-2776: don't create term vectors files if no doc uses term vectors
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1039314 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/TermVectorsReader.java
+++ lucene/src/java/org/apache/lucene/index/TermVectorsReader.java
@@ -517,9 +517,10 @@ block|}
 block|}
 else|else
 block|{
-comment|// TODO: understand why FieldInfos.hasVectors() can
-comment|// return true yet the term vectors files don't
-comment|// exist...
+comment|// If all documents flushed in a segment had hit
+comment|// non-aborting exceptions, it's possible that
+comment|// FieldInfos.hasVectors returns true yet the term
+comment|// vector files don't exist.
 name|format
 operator|=
 literal|0
