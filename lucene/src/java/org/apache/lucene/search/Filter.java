910cd57acd77801354defee3734294c92ab47377
Robert Muir <rmuir@apache.org>
2011-01-17 21:25:46 +0000

    clear javadocs warnings
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1060098 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/search/Filter.java
+++ lucene/src/java/org/apache/lucene/search/Filter.java
@@ -34,6 +34,22 @@ operator|.
 name|index
 operator|.
 name|IndexReader
+import|;
+end_import
+begin_comment
+comment|// javadocs
+end_comment
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|index
+operator|.
+name|IndexReader
 operator|.
 name|AtomicReaderContext
 import|;
