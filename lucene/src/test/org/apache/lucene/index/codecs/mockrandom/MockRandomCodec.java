902d4a170a6d230f722f35cb5f5142547dc41ed0
Michael McCandless <mikemccand@apache.org>
2011-01-28 23:12:22 +0000

    LUCENE-2895: MockRandomCodec loads termsIndex even if termsIndexDivisor is set to -1
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1064926 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/codecs/mockrandom/MockRandomCodec.java
+++ lucene/src/test/org/apache/lucene/index/codecs/mockrandom/MockRandomCodec.java
@@ -1869,6 +1869,16 @@ literal|")"
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|state
+operator|.
+name|termsIndexDivisor
+operator|!=
+operator|-
+literal|1
+condition|)
+block|{
 name|state
 operator|.
 name|termsIndexDivisor
@@ -1884,6 +1894,7 @@ argument_list|,
 literal|10
 argument_list|)
 expr_stmt|;
+block|}
 name|indexReader
 operator|=
 operator|new
