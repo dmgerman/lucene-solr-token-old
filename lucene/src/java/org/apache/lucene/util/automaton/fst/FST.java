0841f538befd5d088545d2e310f9d7a5b3ae67d1
Dawid Weiss <dweiss@apache.org>
2011-02-21 14:23:48 +0000

    Alternative depth-based DOT layout ordering in FST's Utils. https://issues.apache.org/jira/browse/LUCENE-2934
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1072978 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/util/automaton/fst/FST.java
+++ lucene/src/java/org/apache/lucene/util/automaton/fst/FST.java
@@ -2273,7 +2273,7 @@ return|return
 name|arc
 return|;
 block|}
-comment|/** Follow the follow arc and read the first arc of its    *  target; this changes the provide arc (2nd arg) in-place    *  and returns it. */
+comment|/**    * Follow the<code>follow</code> arc and read the first arc of its target;    * this changes the provided<code>arc</code> (2nd arg) in-place and returns    * it.    *     * @returns Returns the second argument (<code>arc</code>).    */
 DECL|method|readFirstTargetArc
 specifier|public
 name|Arc
