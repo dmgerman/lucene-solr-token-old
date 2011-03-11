a833a1633ba2c0583044bc1f72e8936e3400a536
Robert Muir <rmuir@apache.org>
2011-03-11 20:12:04 +0000

    try to avoid jre bug in test, take 2
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1080748 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/cloud/ZkSolrClientTest.java
+++ solr/src/test/org/apache/solr/cloud/ZkSolrClientTest.java
@@ -119,6 +119,15 @@ operator|.
 name|Watcher
 import|;
 end_import
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Ignore
+import|;
+end_import
 begin_class
 DECL|class|ZkSolrClientTest
 specifier|public
@@ -658,6 +667,11 @@ expr_stmt|;
 block|}
 block|}
 block|}
+annotation|@
+name|Ignore
+argument_list|(
+literal|"test case crashes modern jres"
+argument_list|)
 DECL|method|testWatchChildren
 specifier|public
 name|void
