252f1f55e0c9511654ea5cc5910524c22f88d6dd
Robert Muir <rmuir@apache.org>
2011-03-13 02:46:01 +0000

    revert hack
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/docvalues@1081034 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/search/SortField.java
+++ lucene/src/java/org/apache/lucene/search/SortField.java
@@ -277,16 +277,12 @@ init|=
 literal|false
 decl_stmt|;
 comment|// defaults to natural order
-comment|// This is not generified because of a javac bug in early JDK 1.5 (e.g. used by FreeBSD).
-comment|// With<?> as type param, getComparator() fails to compile because of cast problems.
 DECL|field|creator
-annotation|@
-name|SuppressWarnings
-argument_list|(
-literal|"unchecked"
-argument_list|)
 specifier|private
 name|CachedArrayCreator
+argument_list|<
+name|?
+argument_list|>
 name|creator
 decl_stmt|;
 DECL|field|missingValue
