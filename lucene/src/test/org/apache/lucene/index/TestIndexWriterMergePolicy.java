3f014ee4bd9259a41c8d4e9090bf26980106f3e6
Michael McCandless <mikemccand@apache.org>
2011-03-25 17:38:16 +0000

    force SMS to workaround overly pedantic test failure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1085485 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestIndexWriterMergePolicy.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriterMergePolicy.java
@@ -635,6 +635,13 @@ operator|new
 name|LogDocMergePolicy
 argument_list|()
 argument_list|)
+operator|.
+name|setMergeScheduler
+argument_list|(
+operator|new
+name|SerialMergeScheduler
+argument_list|()
+argument_list|)
 argument_list|)
 decl_stmt|;
 comment|// leftmost* segment has 1 doc
@@ -777,7 +784,7 @@ operator|.
 name|setMergeScheduler
 argument_list|(
 operator|new
-name|ConcurrentMergeScheduler
+name|SerialMergeScheduler
 argument_list|()
 argument_list|)
 argument_list|)
