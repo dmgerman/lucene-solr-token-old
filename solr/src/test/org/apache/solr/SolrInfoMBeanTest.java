3b8bed4412e08683caa232aa4d4198cffcd86091
Simon Willnauer <simonw@apache.org>
2011-06-09 13:11:28 +0000

    LUCENE-3108: manually merged with trunk
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/docvalues@1133855 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/SolrInfoMBeanTest.java
+++ solr/src/test/org/apache/solr/SolrInfoMBeanTest.java
@@ -120,15 +120,6 @@ import|;
 end_import
 begin_import
 import|import
-name|org
-operator|.
-name|junit
-operator|.
-name|Ignore
-import|;
-end_import
-begin_import
-import|import
 name|java
 operator|.
 name|io
@@ -203,9 +194,6 @@ argument_list|)
 expr_stmt|;
 block|}
 comment|/**    * Gets a list of everything we can find in the classpath and makes sure it has    * a name, description, etc...    */
-annotation|@
-name|Ignore
-comment|// TODO: reenable once SOLR-2160 is fixed
 DECL|method|testCallMBeanInfo
 specifier|public
 name|void
@@ -214,7 +202,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-comment|//    Object[] init = org.apache.solr.search.QParserPlugin.standardPlugins;
 name|List
 argument_list|<
 name|Class
