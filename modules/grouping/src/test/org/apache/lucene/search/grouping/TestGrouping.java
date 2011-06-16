381fe85c0fc789f2503a3fd45439b1de8c5091cd
Uwe Schindler <uschindler@apache.org>
2011-06-16 19:03:35 +0000

    LUCENE-3208: Missed change in grouping module
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1136605 13f79535-47bb-0310-9956-ffa450edef68

--- modules/grouping/src/test/org/apache/lucene/search/grouping/TestGrouping.java
+++ modules/grouping/src/test/org/apache/lucene/search/grouping/TestGrouping.java
@@ -6695,11 +6695,11 @@ specifier|final
 name|Weight
 name|w
 init|=
-name|query
-operator|.
-name|weight
-argument_list|(
 name|topSearcher
+operator|.
+name|createNormalizedWeight
+argument_list|(
+name|query
 argument_list|)
 decl_stmt|;
 specifier|final
