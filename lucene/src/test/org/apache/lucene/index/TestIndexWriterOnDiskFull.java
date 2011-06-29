d29242f560f58b711874039763846d1662feaf26
Simon Willnauer <simonw@apache.org>
2011-06-29 07:11:13 +0000

    LUCENE-3175: optimized slow tests to run faster if not TESTS_NIGHTLY
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1140980 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestIndexWriterOnDiskFull.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriterOnDiskFull.java
@@ -667,7 +667,11 @@ decl_stmt|;
 name|int
 name|NUM_DIR
 init|=
+name|TEST_NIGHTLY
+condition|?
 literal|50
+else|:
+literal|5
 decl_stmt|;
 name|int
 name|END_COUNT
@@ -676,7 +680,13 @@ name|START_COUNT
 operator|+
 name|NUM_DIR
 operator|*
+operator|(
+name|TEST_NIGHTLY
+condition|?
 literal|25
+else|:
+literal|5
+operator|)
 decl_stmt|;
 comment|// Build up a bunch of dirs that have indexes which we
 comment|// will then merge together by calling addIndexes(*):
