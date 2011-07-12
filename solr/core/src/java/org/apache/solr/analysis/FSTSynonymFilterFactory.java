d57febb85fd5a5daf8cad91a3e02c4c41b7528b2
Robert Muir <rmuir@apache.org>
2011-07-12 03:46:37 +0000

    LUCENE-3233: log a warning instead of error, if synonyms rule set is completely empty
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1145442 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/analysis/FSTSynonymFilterFactory.java
+++ solr/core/src/java/org/apache/solr/analysis/FSTSynonymFilterFactory.java
@@ -362,7 +362,17 @@ name|TokenStream
 name|input
 parameter_list|)
 block|{
+comment|// if the fst is null, it means there's actually no synonyms... just return the original stream
+comment|// as there is nothing to do here.
 return|return
+name|map
+operator|.
+name|fst
+operator|==
+literal|null
+condition|?
+name|input
+else|:
 operator|new
 name|SynonymFilter
 argument_list|(
@@ -593,6 +603,27 @@ argument_list|(
 name|e
 argument_list|)
 throw|;
+block|}
+if|if
+condition|(
+name|map
+operator|.
+name|fst
+operator|==
+literal|null
+condition|)
+block|{
+name|log
+operator|.
+name|warn
+argument_list|(
+literal|"Synonyms loaded with "
+operator|+
+name|args
+operator|+
+literal|" has empty rule set!"
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 comment|/**    * Load synonyms from the solr format, "format=solr".    */
