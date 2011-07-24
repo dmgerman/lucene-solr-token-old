0dae61d42f9ba9adb196d8dbfdea7c8b9324be6d
Robert Muir <rmuir@apache.org>
2011-07-24 17:08:40 +0000

    SOLR-2673: fix test dependent on order
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1150437 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/SolrExampleTests.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/SolrExampleTests.java
@@ -5878,6 +5878,12 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|SolrServer
+name|server
+init|=
+name|getSolrServer
+argument_list|()
+decl_stmt|;
 comment|// Empty the database...
 name|server
 operator|.
