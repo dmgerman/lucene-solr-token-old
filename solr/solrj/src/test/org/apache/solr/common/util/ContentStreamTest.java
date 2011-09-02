71228eb03b95e24d150a2b02438b6d0850c6cc88
Chris M. Hostetter <hossman@apache.org>
2011-09-02 00:31:29 +0000

    improve test assumptions: assume content instead of assert; catch IOException so we also assume no UnknownHostException
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1164311 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/common/util/ContentStreamTest.java
+++ solr/solrj/src/test/org/apache/solr/common/util/ContentStreamTest.java
@@ -448,10 +448,21 @@ argument_list|(
 name|in
 argument_list|)
 expr_stmt|;
+name|assumeTrue
+argument_list|(
+literal|"not enough content for test to be useful"
+argument_list|,
+name|content
+operator|.
+name|length
+operator|>
+literal|10
+argument_list|)
+expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|ConnectException
+name|IOException
 name|ex
 parameter_list|)
 block|{
@@ -485,16 +496,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|assertTrue
-argument_list|(
-name|content
-operator|.
-name|length
-operator|>
-literal|10
-argument_list|)
-expr_stmt|;
-comment|// found something...
 name|ContentStreamBase
 name|stream
 init|=
