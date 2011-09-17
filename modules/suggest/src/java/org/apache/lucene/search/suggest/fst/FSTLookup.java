ed8b6ff2a8cc75b45c431eecdca41adda2e4680b
Dawid Weiss <dweiss@apache.org>
2011-09-17 16:12:36 +0000

    SOLR-2762: FSTLookup returns one less suggestion than it should when onlyMorePopular=true
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1172006 13f79535-47bb-0310-9956-ffa450edef68

--- modules/suggest/src/java/org/apache/lucene/search/suggest/fst/FSTLookup.java
+++ modules/suggest/src/java/org/apache/lucene/search/suggest/fst/FSTLookup.java
@@ -994,7 +994,7 @@ name|key
 argument_list|,
 name|num
 argument_list|,
-literal|true
+literal|false
 argument_list|)
 return|;
 block|}
@@ -1046,7 +1046,7 @@ name|key
 argument_list|,
 name|num
 argument_list|,
-literal|false
+literal|true
 argument_list|)
 decl_stmt|;
 comment|// Sort and trim.
@@ -1117,7 +1117,7 @@ return|return
 name|res
 return|;
 block|}
-comment|/**    * Lookup suggestions sorted by weight (descending order).    *     * @param greedy If<code>true</code>, the routine terminates immediately when<code>num</code>    * suggestions have been collected. If<code>false</code>, it will collect suggestions from    * all weight arcs (needed for {@link #lookupSortedAlphabetically}.    */
+comment|/**    * Lookup suggestions sorted by weight (descending order).    *     * @param collectAll If<code>true</code>, the routine terminates immediately when<code>num</code>    * suggestions have been collected. If<code>false</code>, it will collect suggestions from    * all weight arcs (needed for {@link #lookupSortedAlphabetically}.    */
 DECL|method|lookupSortedByWeight
 specifier|private
 name|ArrayList
@@ -1133,11 +1133,14 @@ name|int
 name|num
 parameter_list|,
 name|boolean
-name|greedy
+name|collectAll
 parameter_list|)
 throws|throws
 name|IOException
 block|{
+comment|// Don't overallocate the results buffers. This also serves the purpose of allowing
+comment|// the user of this class to request all matches using Integer.MAX_VALUE as the number
+comment|// of results.
 specifier|final
 name|ArrayList
 argument_list|<
@@ -1286,7 +1289,8 @@ argument_list|,
 name|arc
 argument_list|)
 operator|&&
-name|greedy
+operator|!
+name|collectAll
 condition|)
 block|{
 comment|// We have enough suggestions to return immediately. Keep on looking for an
@@ -1294,6 +1298,17 @@ comment|// exact match, if requested.
 if|if
 condition|(
 name|exactMatchFirst
+condition|)
+block|{
+if|if
+condition|(
+operator|!
+name|checkExistingAndReorder
+argument_list|(
+name|res
+argument_list|,
+name|key
+argument_list|)
 condition|)
 block|{
 name|Float
@@ -1313,28 +1328,14 @@ operator|!=
 literal|null
 condition|)
 block|{
-name|res
-operator|.
-name|add
-argument_list|(
-literal|0
-argument_list|,
-operator|new
-name|LookupResult
-argument_list|(
-name|key
-argument_list|,
-name|exactMatchWeight
-argument_list|)
-argument_list|)
-expr_stmt|;
+comment|// Insert as the first result and truncate at num.
 while|while
 condition|(
 name|res
 operator|.
 name|size
 argument_list|()
-operator|>
+operator|>=
 name|num
 condition|)
 block|{
@@ -1351,6 +1352,22 @@ literal|1
 argument_list|)
 expr_stmt|;
 block|}
+name|res
+operator|.
+name|add
+argument_list|(
+literal|0
+argument_list|,
+operator|new
+name|LookupResult
+argument_list|(
+name|key
+argument_list|,
+name|exactMatchWeight
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 block|}
 break|break;
@@ -1359,6 +1376,82 @@ block|}
 block|}
 return|return
 name|res
+return|;
+block|}
+comment|/**    * Checks if the list of {@link LookupResult}s already has a<code>key</code>. If so,    * reorders that {@link LookupResult} to the first position.    *     * @return Returns<code>true<code> if and only if<code>list</code> contained<code>key</code>.    */
+DECL|method|checkExistingAndReorder
+specifier|private
+name|boolean
+name|checkExistingAndReorder
+parameter_list|(
+name|ArrayList
+argument_list|<
+name|LookupResult
+argument_list|>
+name|list
+parameter_list|,
+name|String
+name|key
+parameter_list|)
+block|{
+comment|// We assume list does not have duplicates (because of how the FST is created).
+for|for
+control|(
+name|int
+name|i
+init|=
+name|list
+operator|.
+name|size
+argument_list|()
+init|;
+operator|--
+name|i
+operator|>=
+literal|0
+condition|;
+control|)
+block|{
+if|if
+condition|(
+name|key
+operator|.
+name|equals
+argument_list|(
+name|list
+operator|.
+name|get
+argument_list|(
+name|i
+argument_list|)
+operator|.
+name|key
+argument_list|)
+condition|)
+block|{
+comment|// Key found. Unless already at i==0, remove it and push up front so that the ordering
+comment|// remains identical with the exception of the exact match.
+name|list
+operator|.
+name|add
+argument_list|(
+literal|0
+argument_list|,
+name|list
+operator|.
+name|remove
+argument_list|(
+name|i
+argument_list|)
+argument_list|)
+expr_stmt|;
+return|return
+literal|true
+return|;
+block|}
+block|}
+return|return
+literal|false
 return|;
 block|}
 comment|/**    * Descend along the path starting at<code>arc</code> and going through    * bytes in<code>utf8</code> argument.    *      * @param arc The starting arc. This argument is modified in-place.    * @param term The term to descend with.    * @return If<code>true</code>,<code>arc</code> will be set to the arc matching    * last byte of<code>utf8</code>.<code>false</code> is returned if no such     * prefix<code>utf8</code> exists.    */
