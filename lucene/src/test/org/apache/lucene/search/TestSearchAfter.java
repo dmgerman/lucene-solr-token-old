a65b8cfe886648d322cba85b00bce4b75dd8458a
Robert Muir <rmuir@apache.org>
2011-09-26 16:22:46 +0000

    use textField not Stringfield in this test (it was intended to analyze the english)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1175936 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/search/TestSearchAfter.java
+++ lucene/src/test/org/apache/lucene/search/TestSearchAfter.java
@@ -37,7 +37,7 @@ name|lucene
 operator|.
 name|document
 operator|.
-name|StringField
+name|TextField
 import|;
 end_import
 begin_import
@@ -233,7 +233,7 @@ argument_list|(
 name|i
 argument_list|)
 argument_list|,
-name|StringField
+name|TextField
 operator|.
 name|TYPE_UNSTORED
 argument_list|)
@@ -259,7 +259,7 @@ literal|"even"
 else|:
 literal|"odd"
 argument_list|,
-name|StringField
+name|TextField
 operator|.
 name|TYPE_UNSTORED
 argument_list|)
