e8180be6ad1573f1c8f31a22c81133a748750503
Michael McCandless <mikemccand@apache.org>
2011-10-10 22:08:52 +0000

    LUCENE-3486: fix javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1181268 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/misc/src/java/org/apache/lucene/search/SearcherLifetimeManager.java
+++ lucene/contrib/misc/src/java/org/apache/lucene/search/SearcherLifetimeManager.java
@@ -105,6 +105,22 @@ name|apache
 operator|.
 name|lucene
 operator|.
+name|index
+operator|.
+name|IndexReader
+import|;
+end_import
+begin_comment
+comment|// javadocs
+end_comment
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
 name|store
 operator|.
 name|AlreadyClosedException
