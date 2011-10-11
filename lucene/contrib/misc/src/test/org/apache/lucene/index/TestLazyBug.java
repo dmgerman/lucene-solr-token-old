030d60ede8c1fa12c78577308b49c7546f7f7a10
Dawid Weiss <dweiss@apache.org>
2011-10-11 08:52:35 +0000

    Typo.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1181664 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/misc/src/test/org/apache/lucene/index/TestLazyBug.java
+++ lucene/contrib/misc/src/test/org/apache/lucene/index/TestLazyBug.java
@@ -164,7 +164,7 @@ name|BeforeClass
 import|;
 end_import
 begin_comment
-comment|/**  * Test demonstrating EOF bug on the last field of the last doc  * if other docs have allready been accessed.  */
+comment|/**  * Test demonstrating EOF bug on the last field of the last doc  * if other docs have already been accessed.  */
 end_comment
 begin_class
 DECL|class|TestLazyBug
