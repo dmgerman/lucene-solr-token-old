941f07a4d475feae8fa57b6c9fbf99599f864ba8
Robert Muir <rmuir@apache.org>
2011-10-26 12:14:27 +0000

    disable test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1189160 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/core/TestJmxIntegration.java
+++ solr/core/src/test/org/apache/solr/core/TestJmxIntegration.java
@@ -66,6 +66,15 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -567,6 +576,11 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"fix me"
+argument_list|)
 DECL|method|testJmxOnCoreReload
 specifier|public
 name|void
