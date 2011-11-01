ff7dbe7c057e4c7afc624d6817a4924c4fc2a45e
Chris M. Hostetter <hossman@apache.org>
2011-11-01 21:31:35 +0000

    SOLR-2860: remove broken code for tracking solrindexwriter open/close pairs -- redundent because of SOLR-2279
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1196292 13f79535-47bb-0310-9956-ffa450edef68

--- solr/test-framework/src/java/org/apache/solr/util/AbstractSolrTestCase.java
+++ solr/test-framework/src/java/org/apache/solr/util/AbstractSolrTestCase.java
@@ -304,11 +304,6 @@ operator|.
 name|startTrackingSearchers
 argument_list|()
 expr_stmt|;
-name|SolrTestCaseJ4
-operator|.
-name|startTrackingWriters
-argument_list|()
-expr_stmt|;
 block|}
 annotation|@
 name|AfterClass
@@ -324,11 +319,6 @@ block|{
 name|SolrTestCaseJ4
 operator|.
 name|endTrackingSearchers
-argument_list|()
-expr_stmt|;
-name|SolrTestCaseJ4
-operator|.
-name|endTrackingWriters
 argument_list|()
 expr_stmt|;
 block|}
