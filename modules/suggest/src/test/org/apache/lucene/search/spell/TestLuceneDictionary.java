28319aca64759051ad02497cdc135bae2858d2ac
Robert Muir <rmuir@apache.org>
2011-11-04 09:54:55 +0000

    LUCENE-3557: SpellChecker should take IWC, remove sneaky silent optimize() methods
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1197469 13f79535-47bb-0310-9956-ffa450edef68

--- modules/suggest/src/test/org/apache/lucene/search/spell/TestLuceneDictionary.java
+++ modules/suggest/src/test/org/apache/lucene/search/spell/TestLuceneDictionary.java
@@ -1114,6 +1114,15 @@ name|indexReader
 argument_list|,
 literal|"contents"
 argument_list|)
+argument_list|,
+name|newIndexWriterConfig
+argument_list|(
+name|TEST_VERSION_CURRENT
+argument_list|,
+literal|null
+argument_list|)
+argument_list|,
+literal|false
 argument_list|)
 expr_stmt|;
 name|String
--- modules/suggest/src/test/org/apache/lucene/search/spell/TestPlainTextDictionary.java
+++ modules/suggest/src/test/org/apache/lucene/search/spell/TestPlainTextDictionary.java
@@ -136,6 +136,15 @@ operator|.
 name|indexDictionary
 argument_list|(
 name|ptd
+argument_list|,
+name|newIndexWriterConfig
+argument_list|(
+name|TEST_VERSION_CURRENT
+argument_list|,
+literal|null
+argument_list|)
+argument_list|,
+literal|false
 argument_list|)
 expr_stmt|;
 name|String
