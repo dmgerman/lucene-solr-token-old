bd0068016c3aa1c71f7b5890d79e7ad90166722d
Yonik Seeley <yonik@apache.org>
2011-11-07 23:12:21 +0000

    SOLR-2861: add additional asserts to catch read past EOF that jenkins sees
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1198999 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/FSUpdateLog.java
+++ solr/core/src/java/org/apache/solr/update/FSUpdateLog.java
@@ -1401,6 +1401,10 @@ name|String
 argument_list|>
 argument_list|()
 decl_stmt|;
+DECL|field|lengthForDebugging
+name|long
+name|lengthForDebugging
+decl_stmt|;
 comment|// write a BytesRef as a byte array
 DECL|field|resolver
 name|JavaBinCodec
@@ -2386,6 +2390,25 @@ name|flushBuffer
 argument_list|()
 expr_stmt|;
 comment|/***          System.out.println("###flushBuffer to " + fos.size() + " raf.length()=" + raf.length() + " pos="+pos);         if (fos.size() != raf.length() || pos>= fos.size() ) {           throw new RuntimeException("ERROR" + "###flushBuffer to " + fos.size() + " raf.length()=" + raf.length() + " pos="+pos);         }         ***/
+assert|assert
+name|pos
+operator|<
+name|fos
+operator|.
+name|size
+argument_list|()
+assert|;
+assert|assert
+name|fos
+operator|.
+name|size
+argument_list|()
+operator|==
+name|channel
+operator|.
+name|size
+argument_list|()
+assert|;
 block|}
 name|ChannelFastInputStream
 name|fis
@@ -2612,6 +2635,14 @@ argument_list|,
 name|len
 argument_list|)
 decl_stmt|;
+assert|assert
+name|chPosition
+operator|<
+name|ch
+operator|.
+name|size
+argument_list|()
+assert|;
 name|int
 name|ret
 init|=
