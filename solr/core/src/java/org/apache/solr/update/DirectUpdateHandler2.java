c1b94dc2166520a8f059192d9ec373eb0564ea79
Simon Willnauer <simonw@apache.org>
2011-11-15 09:28:46 +0000

    SOLR-2895: call CommitTracker#addedDocument after document is added
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1202107 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/DirectUpdateHandler2.java
+++ solr/core/src/java/org/apache/solr/update/DirectUpdateHandler2.java
@@ -884,15 +884,6 @@ expr_stmt|;
 block|}
 try|try
 block|{
-name|commitTracker
-operator|.
-name|addedDocument
-argument_list|(
-name|cmd
-operator|.
-name|commitWithin
-argument_list|)
-expr_stmt|;
 name|softCommitTracker
 operator|.
 name|addedDocument
@@ -1054,6 +1045,15 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
+name|commitTracker
+operator|.
+name|addedDocument
+argument_list|(
+name|cmd
+operator|.
+name|commitWithin
+argument_list|)
+expr_stmt|;
 comment|// Add to the transaction log *after* successfully adding to the index, if there was no error.
 comment|// This ordering ensures that if we log it, it's definitely been added to the the index.
 comment|// This also ensures that if a commit sneaks in-between, that we know everything in a particular
