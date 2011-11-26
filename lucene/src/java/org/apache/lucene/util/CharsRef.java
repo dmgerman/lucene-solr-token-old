6af6e66d976d2e04a1071a7016d3d5e0c3597370
Robert Muir <rmuir@apache.org>
2011-11-26 15:00:38 +0000

    LUCENE-3590: fix javadocs warning
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1206452 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/util/CharsRef.java
+++ lucene/src/java/org/apache/lucene/util/CharsRef.java
@@ -24,7 +24,7 @@ begin_comment
 comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/**  * Represents char[], as a slice (offset + length) into an existing char[].  * The {@link #chars} member should never be null; use  * {@link #EMPTY_ARRAY} if necessary.  * @lucene.internal  */
+comment|/**  * Represents char[], as a slice (offset + length) into an existing char[].  * The {@link #chars} member should never be null; use  * {@link #EMPTY_CHARS} if necessary.  * @lucene.internal  */
 end_comment
 begin_class
 DECL|class|CharsRef
