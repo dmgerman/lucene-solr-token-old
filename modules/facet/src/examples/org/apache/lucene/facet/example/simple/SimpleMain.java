db4a8b5af7d6e6068ac8b05088dd7193e070d1fb
Robert Muir <rmuir@apache.org>
2011-12-05 14:57:44 +0000

    LUCENE-3606: remove indexreader modification tasks from benchmark
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3606@1210486 13f79535-47bb-0310-9956-ffa450edef68

--- modules/facet/src/examples/org/apache/lucene/facet/example/simple/SimpleMain.java
+++ modules/facet/src/examples/org/apache/lucene/facet/example/simple/SimpleMain.java
@@ -251,8 +251,6 @@ operator|.
 name|open
 argument_list|(
 name|indexDir
-argument_list|,
-literal|true
 argument_list|)
 decl_stmt|;
 name|ExampleUtils
@@ -364,8 +362,6 @@ operator|.
 name|open
 argument_list|(
 name|indexDir
-argument_list|,
-literal|true
 argument_list|)
 decl_stmt|;
 name|ExampleUtils
