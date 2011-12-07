ff7ac260893f839ec97009a9fbd6d98fbda9ed1f
Michael McCandless <mikemccand@apache.org>
2011-12-07 17:07:38 +0000

    LUCENE-3622: accept SortedSource impls that don't back the ords with packed ints
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3622@1211541 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/values/IndexDocValues.java
+++ lucene/src/java/org/apache/lucene/index/values/IndexDocValues.java
@@ -563,6 +563,17 @@ name|BytesRef
 name|bytesRef
 parameter_list|)
 function_decl|;
+comment|/** Return true if it's safe to call {@link      *  #getDocToOrd}. */
+DECL|method|hasPackedDocToOrd
+specifier|public
+name|boolean
+name|hasPackedDocToOrd
+parameter_list|()
+block|{
+return|return
+literal|false
+return|;
+block|}
 comment|/**      * Returns the PackedInts.Reader impl that maps document to ord.      */
 DECL|method|getDocToOrd
 specifier|public
@@ -990,6 +1001,17 @@ literal|0
 expr_stmt|;
 return|return
 name|bytesRef
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|hasPackedDocToOrd
+parameter_list|()
+block|{
+return|return
+literal|true
 return|;
 block|}
 annotation|@
