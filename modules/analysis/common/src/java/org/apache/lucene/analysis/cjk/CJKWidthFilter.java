da720b7cd37b166f4828f230b7f5fdbdb18a3502
Steven Rowe <sarowe@apache.org>
2011-12-29 18:53:10 +0000

    - Added license declaration - Removed unused 'length' param to combine()
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1225615 13f79535-47bb-0310-9956-ffa450edef68

--- modules/analysis/common/src/java/org/apache/lucene/analysis/cjk/CJKWidthFilter.java
+++ modules/analysis/common/src/java/org/apache/lucene/analysis/cjk/CJKWidthFilter.java
@@ -13,6 +13,9 @@ operator|.
 name|cjk
 package|;
 end_package
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 begin_import
 import|import
 name|java
@@ -359,8 +362,6 @@ argument_list|(
 name|text
 argument_list|,
 name|i
-argument_list|,
-name|length
 argument_list|,
 name|ch
 argument_list|)
@@ -808,9 +809,6 @@ index|[]
 parameter_list|,
 name|int
 name|pos
-parameter_list|,
-name|int
-name|length
 parameter_list|,
 name|char
 name|ch
