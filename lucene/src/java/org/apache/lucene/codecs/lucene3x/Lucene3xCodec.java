bc619f3d31d636d127b49db8ec00533ce37e191b
Robert Muir <rmuir@apache.org>
2012-01-17 19:57:46 +0000

    LUCENE-3687: remove now-obselete TODO
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1232556 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/codecs/lucene3x/Lucene3xCodec.java
+++ lucene/src/java/org/apache/lucene/codecs/lucene3x/Lucene3xCodec.java
@@ -315,7 +315,6 @@ operator|new
 name|Lucene40TermVectorsFormat
 argument_list|()
 decl_stmt|;
-comment|// TODO: this should really be a different impl
 DECL|field|fieldInfosFormat
 specifier|private
 specifier|final
