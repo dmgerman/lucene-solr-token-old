304679ea966ba9a2bc35892931a7462fd76c9255
Robert Muir <rmuir@apache.org>
2012-01-29 16:51:18 +0000

    LUCENE-2858: fix contrib/sandbox
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene2858@1237340 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/sandbox/src/java/org/apache/lucene/sandbox/queries/DuplicateFilter.java
+++ lucene/contrib/sandbox/src/java/org/apache/lucene/sandbox/queries/DuplicateFilter.java
@@ -39,7 +39,7 @@ name|lucene
 operator|.
 name|index
 operator|.
-name|IndexReader
+name|AtomicIndexReader
 operator|.
 name|AtomicReaderContext
 import|;
@@ -253,6 +253,7 @@ argument_list|(
 name|context
 operator|.
 name|reader
+argument_list|()
 argument_list|,
 name|acceptDocs
 argument_list|)
@@ -266,6 +267,7 @@ argument_list|(
 name|context
 operator|.
 name|reader
+argument_list|()
 argument_list|,
 name|acceptDocs
 argument_list|)
@@ -277,7 +279,7 @@ specifier|private
 name|FixedBitSet
 name|correctBits
 parameter_list|(
-name|IndexReader
+name|AtomicIndexReader
 name|reader
 parameter_list|,
 name|Bits
@@ -464,7 +466,7 @@ specifier|private
 name|FixedBitSet
 name|fastBits
 parameter_list|(
-name|IndexReader
+name|AtomicIndexReader
 name|reader
 parameter_list|,
 name|Bits
--- lucene/contrib/sandbox/src/java/org/apache/lucene/sandbox/queries/SlowCollatedStringComparator.java
+++ lucene/contrib/sandbox/src/java/org/apache/lucene/sandbox/queries/SlowCollatedStringComparator.java
@@ -44,7 +44,7 @@ name|lucene
 operator|.
 name|index
 operator|.
-name|IndexReader
+name|AtomicIndexReader
 operator|.
 name|AtomicReaderContext
 import|;
@@ -424,6 +424,7 @@ argument_list|(
 name|context
 operator|.
 name|reader
+argument_list|()
 argument_list|,
 name|field
 argument_list|)
