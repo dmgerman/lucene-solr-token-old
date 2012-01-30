81da6d8750d34f577d05cb1d1035b20b99b2d441
Robert Muir <rmuir@apache.org>
2012-01-30 14:36:38 +0000

    consistent error messages for size mismatch
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3661@1237682 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/codecs/lucene40/Lucene40StoredFieldsWriter.java
+++ lucene/src/java/org/apache/lucene/codecs/lucene40/Lucene40StoredFieldsWriter.java
@@ -1122,7 +1122,7 @@ throw|throw
 operator|new
 name|RuntimeException
 argument_list|(
-literal|"mergeFields produced an invalid result: docCount is "
+literal|"fdx size mismatch: docCount is "
 operator|+
 name|numDocs
 operator|+
