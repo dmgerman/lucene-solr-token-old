547e4a7918843c365ce3728ce33de545889860aa
Uwe Schindler <uschindler@apache.org>
2012-02-02 08:09:55 +0000

    LUCENE-3733: Remove dead javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1239478 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/SlowCompositeReaderWrapper.java
+++ lucene/src/java/org/apache/lucene/index/SlowCompositeReaderWrapper.java
@@ -103,7 +103,7 @@ begin_comment
 comment|// javadoc
 end_comment
 begin_comment
-comment|/**  * This class forces a composite reader (eg a {@link  * MultiReader} or {@link DirectoryReader} or any other  * IndexReader subclass that returns non-null from {@link  * CompositeReader#getSequentialSubReaders}) to emulate an  * atomic reader.  This requires implementing the postings  * APIs on-the-fly, using the static methods in {@link  * MultiFields}, {@link MultiDocValues},   * by stepping through the sub-readers to merge fields/terms,   * appending docs, etc.  *  *<p>If you ever hit an UnsupportedOperationException saying  * "please use MultiXXX.YYY instead", the simple  * but non-performant workaround is to wrap your reader  * using this class.</p>  *  *<p><b>NOTE</b>: this class almost always results in a  * performance hit.  If this is important to your use case,  * it's better to get the sequential sub readers (see {@link  * ReaderUtil#gatherSubReaders}, instead, and iterate through them  * yourself.</p>  */
+comment|/**  * This class forces a composite reader (eg a {@link  * MultiReader} or {@link DirectoryReader}) to emulate an  * atomic reader.  This requires implementing the postings  * APIs on-the-fly, using the static methods in {@link  * MultiFields}, {@link MultiDocValues},   * by stepping through the sub-readers to merge fields/terms,   * appending docs, etc.  *  *<p><b>NOTE</b>: this class almost always results in a  * performance hit.  If this is important to your use case,  * it's better to get the sequential sub readers (see {@link  * ReaderUtil#gatherSubReaders}, instead, and iterate through them  * yourself.</p>  */
 end_comment
 begin_class
 DECL|class|SlowCompositeReaderWrapper
