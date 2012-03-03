0ddc11075d4b497071ba28e9322749be8ef52249
Mark Robert Miller <markrmiller@apache.org>
2012-03-03 20:31:00 +0000

    boost how long we are willing to wait for config info to show up in cluster state
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1296692 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -4608,7 +4608,7 @@ name|log
 operator|.
 name|info
 argument_list|(
-literal|"Could not find collection configName - pausing for 2 seconds and trying again - try: "
+literal|"Could not find collection configName - pausing for 3 seconds and trying again - try: "
 operator|+
 name|retry
 argument_list|)
@@ -4617,7 +4617,7 @@ name|Thread
 operator|.
 name|sleep
 argument_list|(
-literal|2000
+literal|3000
 argument_list|)
 expr_stmt|;
 block|}
@@ -4625,7 +4625,7 @@ if|if
 condition|(
 name|retry
 operator|==
-literal|6
+literal|10
 condition|)
 block|{
 name|log
