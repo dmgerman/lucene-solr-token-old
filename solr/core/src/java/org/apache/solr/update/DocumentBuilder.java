86229ba54c09c7f591eb618ec5173c69c67506e0
Yonik Seeley <yonik@apache.org>
2012-03-13 23:24:01 +0000

    improve exception handling messages, don't rewrap SolrException
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1300408 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/DocumentBuilder.java
+++ solr/core/src/java/org/apache/solr/update/DocumentBuilder.java
@@ -1420,6 +1420,16 @@ block|}
 block|}
 catch|catch
 parameter_list|(
+name|SolrException
+name|ex
+parameter_list|)
+block|{
+throw|throw
+name|ex
+throw|;
+block|}
+catch|catch
+parameter_list|(
 name|Exception
 name|ex
 parameter_list|)
@@ -1457,7 +1467,12 @@ operator|.
 name|getValue
 argument_list|()
 operator|+
-literal|"'"
+literal|"' msg="
+operator|+
+name|ex
+operator|.
+name|getMessage
+argument_list|()
 argument_list|,
 name|ex
 argument_list|)
