73db5edbcf8c6444881e70c1df75ae1e56502b0c
Robert Muir <rmuir@apache.org>
2012-03-21 12:56:31 +0000

    LUCENE-3895: ensure we get a random seed if another thread throws uncaught exception
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1303376 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
@@ -1808,7 +1808,9 @@ name|uncaughtExceptionsRule
 init|=
 operator|new
 name|UncaughtExceptionsRule
-argument_list|()
+argument_list|(
+literal|null
+argument_list|)
 decl_stmt|;
 comment|/**    * This controls how suite-level rules are nested. It is important that _all_ rules declared    * in {@link LuceneTestCase} are executed in proper order if they depend on each     * other.    */
 annotation|@
@@ -1860,7 +1862,9 @@ name|around
 argument_list|(
 operator|new
 name|UncaughtExceptionsRule
-argument_list|()
+argument_list|(
+name|this
+argument_list|)
 argument_list|)
 operator|.
 name|around
