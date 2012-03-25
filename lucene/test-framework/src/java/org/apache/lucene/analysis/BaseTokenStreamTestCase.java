e32b6b95b0272274d3ab631524462eecccf95006
Michael McCandless <mikemccand@apache.org>
2012-03-25 21:23:49 +0000

    on analysis consistency fail, print the text for easier debugging
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1305143 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/analysis/BaseTokenStreamTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/analysis/BaseTokenStreamTestCase.java
@@ -2923,6 +2923,8 @@ name|simple
 argument_list|)
 expr_stmt|;
 block|}
+try|try
+block|{
 name|checkAnalysisConsistency
 argument_list|(
 name|random
@@ -2934,6 +2936,36 @@ argument_list|,
 name|text
 argument_list|)
 expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|Throwable
+name|t
+parameter_list|)
+block|{
+name|System
+operator|.
+name|err
+operator|.
+name|println
+argument_list|(
+literal|"TEST FAIL: useCharFilter="
+operator|+
+name|useCharFilter
+operator|+
+literal|" text="
+operator|+
+name|text
+argument_list|)
+expr_stmt|;
+throw|throw
+operator|new
+name|RuntimeException
+argument_list|(
+name|t
+argument_list|)
+throw|;
+block|}
 block|}
 block|}
 DECL|method|checkAnalysisConsistency
