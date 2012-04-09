1a09af0ee5329607896806f93c4e21048af432d5
Robert Muir <rmuir@apache.org>
2012-04-09 14:31:25 +0000

    LUCENE-3969: don't allow negative subword params, Hyphenation relies upon this to filter out what appear to be bogus hyphenation points
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3969@1311257 13f79535-47bb-0310-9956-ffa450edef68

--- modules/analysis/common/src/java/org/apache/lucene/analysis/compound/HyphenationCompoundWordTokenFilter.java
+++ modules/analysis/common/src/java/org/apache/lucene/analysis/compound/HyphenationCompoundWordTokenFilter.java
@@ -535,6 +535,8 @@ operator|.
 name|minSubwordSize
 condition|)
 block|{
+comment|// nocommit/BOGUS/BROKEN/FUNKY/WACKO: somehow we have negative 'parts' according to the
+comment|// calculation above, and we rely upon minSubwordSize being>=0 to filter them out...
 continue|continue;
 block|}
 comment|// check the dictionary
