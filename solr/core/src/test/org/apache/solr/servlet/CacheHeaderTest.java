7a69e5c726b0ca6c793e0e35d163b357debc3679
Ryan McKinley <ryan@apache.org>
2012-04-20 22:39:06 +0000

    SOLR-3388: disable httpCaching for UpdateHandlers by default
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1328535 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/servlet/CacheHeaderTest.java
+++ solr/core/src/test/org/apache/solr/servlet/CacheHeaderTest.java
@@ -124,6 +124,21 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|solr
+operator|.
+name|common
+operator|.
+name|params
+operator|.
+name|CommonParams
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
 name|junit
 operator|.
 name|BeforeClass
@@ -213,12 +228,20 @@ name|getUpdateMethod
 argument_list|(
 literal|"GET"
 argument_list|,
-literal|"stream.file"
+name|CommonParams
+operator|.
+name|STREAM_FILE
 argument_list|,
 name|f
 operator|.
 name|getCanonicalPath
 argument_list|()
+argument_list|,
+name|CommonParams
+operator|.
+name|STREAM_CONTENTTYPE
+argument_list|,
+literal|"text/csv"
 argument_list|)
 decl_stmt|;
 name|HttpResponse
@@ -345,7 +368,11 @@ argument_list|)
 expr_stmt|;
 name|assertTrue
 argument_list|(
-literal|"We got no no-cache in the Cache-Control header"
+literal|"We got no no-cache in the Cache-Control header ["
+operator|+
+name|head
+operator|+
+literal|"]"
 argument_list|,
 name|head
 operator|.
@@ -360,7 +387,11 @@ argument_list|)
 expr_stmt|;
 name|assertTrue
 argument_list|(
-literal|"We got no no-store in the Cache-Control header"
+literal|"We got no no-store in the Cache-Control header ["
+operator|+
+name|head
+operator|+
+literal|"]"
 argument_list|,
 name|head
 operator|.
