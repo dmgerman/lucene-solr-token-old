fba68de6121aa2c653e44e6131cdced10de8f179
Ryan McKinley <ryan@apache.org>
2012-05-01 17:08:43 +0000

    SOLR-3426: better error message when log4j is installed
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1332752 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/CoreContainer.java
+++ solr/core/src/java/org/apache/solr/core/CoreContainer.java
@@ -2295,9 +2295,12 @@ operator|>
 literal|0
 condition|)
 block|{
-name|fname
-operator|=
-literal|"Log4j"
+name|log
+operator|.
+name|warn
+argument_list|(
+literal|"Log watching is not yet implemented for log4j"
+argument_list|)
 expr_stmt|;
 block|}
 elseif|else
@@ -2330,8 +2333,8 @@ name|log
 operator|.
 name|warn
 argument_list|(
-literal|"Unable to read SLF4J version"
-argument_list|,
+literal|"Unable to read SLF4J version.  LogWatcher will be disabled: "
+operator|+
 name|ex
 argument_list|)
 expr_stmt|;
