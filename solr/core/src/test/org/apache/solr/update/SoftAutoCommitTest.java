7e73c70eddabcbfb45da0dc76e4ace4a217d8b85
Uwe Schindler <uschindler@apache.org>
2012-05-20 12:06:22 +0000

    SOLR-3471: Disable tests that don't work correctly on Windows
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1340687 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/update/SoftAutoCommitTest.java
+++ solr/core/src/test/org/apache/solr/update/SoftAutoCommitTest.java
@@ -79,6 +79,19 @@ name|org
 operator|.
 name|apache
 operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|Constants
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
 name|solr
 operator|.
 name|common
@@ -204,6 +217,15 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|assumeFalse
+argument_list|(
+literal|"This test is not working on Windows (or maybe machines with only 2 CPUs)"
+argument_list|,
+name|Constants
+operator|.
+name|WINDOWS
+argument_list|)
+expr_stmt|;
 name|SolrCore
 name|core
 init|=
