0c5b3db37dce6604faa7cddf49c367b3f6f6d8b9
Robert Muir <rmuir@apache.org>
2012-05-24 12:25:33 +0000

    LUCENE-4055: add codecHeader
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4055@1342223 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/codecs/lucene40/Lucene40SegmentInfoFormat.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene40/Lucene40SegmentInfoFormat.java
@@ -104,7 +104,7 @@ begin_comment
 comment|// javadocs
 end_comment
 begin_comment
-comment|/**  * Lucene 4.0 Segment info format.  *<p>  * Files:  *<ul>  *<li><tt>.si</tt>: SegVersion, SegSize, IsCompoundFile, Diagnostics, Attributes, Files  *</ul>  *</p>  * Data types:  *<p>  *<ul>  *<li>SegSize --&gt; {@link DataOutput#writeInt Int32}</li>  *<li>SegVersion --&gt; {@link DataOutput#writeString String}</li>  *<li>Files --&gt; {@link DataOutput#writeStringSet Set&lt;String&gt;}</li>  *<li>Diagnostics, Attributes --&gt; {@link DataOutput#writeStringStringMap Map&lt;String,String&gt;}</li>  *<li>IsCompoundFile --&gt; {@link DataOutput#writeByte Int8}</li>  *</ul>  *</p>  * Field Descriptions:  *<p>  *<ul>  *<li>SegVersion is the code version that created the segment.</li>  *<li>SegSize is the number of documents contained in the segment index.</li>  *<li>IsCompoundFile records whether the segment is written as a compound file or  *       not. If this is -1, the segment is not a compound file. If it is 1, the segment  *       is a compound file.</li>  *<li>Checksum contains the CRC32 checksum of all bytes in the segments_N file up  *       until the checksum. This is used to verify integrity of the file on opening the  *       index.</li>  *<li>The Diagnostics Map is privately written by IndexWriter, as a debugging aid,  *       for each segment it creates. It includes metadata like the current Lucene  *       version, OS, Java version, why the segment was created (merge, flush,  *       addIndexes), etc.</li>  *<li>Attributes: a key-value map of codec-private attributes.</li>  *<li>Files is a list of files referred to by this segment.</li>  *</ul>  *</p>  *   * @see SegmentInfos  * @lucene.experimental  */
+comment|/**  * Lucene 4.0 Segment info format.  *<p>  * Files:  *<ul>  *<li><tt>.si</tt>: Header, SegVersion, SegSize, IsCompoundFile, Diagnostics, Attributes, Files  *</ul>  *</p>  * Data types:  *<p>  *<ul>  *<li>Header --&gt; {@link CodecUtil#writeHeader CodecHeader}</li>  *<li>SegSize --&gt; {@link DataOutput#writeInt Int32}</li>  *<li>SegVersion --&gt; {@link DataOutput#writeString String}</li>  *<li>Files --&gt; {@link DataOutput#writeStringSet Set&lt;String&gt;}</li>  *<li>Diagnostics, Attributes --&gt; {@link DataOutput#writeStringStringMap Map&lt;String,String&gt;}</li>  *<li>IsCompoundFile --&gt; {@link DataOutput#writeByte Int8}</li>  *</ul>  *</p>  * Field Descriptions:  *<p>  *<ul>  *<li>SegVersion is the code version that created the segment.</li>  *<li>SegSize is the number of documents contained in the segment index.</li>  *<li>IsCompoundFile records whether the segment is written as a compound file or  *       not. If this is -1, the segment is not a compound file. If it is 1, the segment  *       is a compound file.</li>  *<li>Checksum contains the CRC32 checksum of all bytes in the segments_N file up  *       until the checksum. This is used to verify integrity of the file on opening the  *       index.</li>  *<li>The Diagnostics Map is privately written by IndexWriter, as a debugging aid,  *       for each segment it creates. It includes metadata like the current Lucene  *       version, OS, Java version, why the segment was created (merge, flush,  *       addIndexes), etc.</li>  *<li>Attributes: a key-value map of codec-private attributes.</li>  *<li>Files is a list of files referred to by this segment.</li>  *</ul>  *</p>  *   * @see SegmentInfos  * @lucene.experimental  */
 end_comment
 begin_class
 DECL|class|Lucene40SegmentInfoFormat
@@ -166,6 +166,30 @@ name|String
 name|SI_EXTENSION
 init|=
 literal|"si"
+decl_stmt|;
+DECL|field|CODEC_NAME
+specifier|static
+specifier|final
+name|String
+name|CODEC_NAME
+init|=
+literal|"Lucene40SegmentInfo"
+decl_stmt|;
+DECL|field|VERSION_START
+specifier|static
+specifier|final
+name|int
+name|VERSION_START
+init|=
+literal|0
+decl_stmt|;
+DECL|field|VERSION_CURRENT
+specifier|static
+specifier|final
+name|int
+name|VERSION_CURRENT
+init|=
+name|VERSION_START
 decl_stmt|;
 block|}
 end_class
