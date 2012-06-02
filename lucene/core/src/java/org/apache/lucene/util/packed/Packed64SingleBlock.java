ed2daa789f027ac2db0282d8b19e9379cd1eb275
Michael McCandless <mikemccand@apache.org>
2012-06-02 18:24:28 +0000

    removed unused var
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1345542 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/packed/Packed64SingleBlock.java
+++ lucene/core/src/java/org/apache/lucene/util/packed/Packed64SingleBlock.java
@@ -13,6 +13,9 @@ operator|.
 name|packed
 package|;
 end_package
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements. See the NOTICE file distributed with this  * work for additional information regarding copyright ownership. The ASF  * licenses this file to You under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  * License for the specific language governing permissions and limitations under  * the License.  */
+end_comment
 begin_import
 import|import
 name|java
@@ -57,9 +60,6 @@ operator|.
 name|RamUsageEstimator
 import|;
 end_import
-begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements. See the NOTICE file distributed with this  * work for additional information regarding copyright ownership. The ASF  * licenses this file to You under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  * License for the specific language governing permissions and limitations under  * the License.  */
-end_comment
 begin_comment
 comment|/**  * This class is similar to {@link Packed64} except that it trades space for  * speed by ensuring that a single block needs to be read/written in order to  * read/write a value.  */
 end_comment
@@ -757,12 +757,6 @@ name|index
 operator|<
 name|valueCount
 assert|;
-specifier|final
-name|int
-name|origLen
-init|=
-name|len
-decl_stmt|;
 name|len
 operator|=
 name|Math
