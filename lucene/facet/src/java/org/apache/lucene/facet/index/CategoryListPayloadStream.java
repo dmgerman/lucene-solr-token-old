d5c4cbe36c0ddd32f84c18303c547c785d60b775
Andrzej Bialecki <ab@apache.org>
2012-06-08 20:06:24 +0000

    LUCENE-4122 Remove other non-fatal refs to Payload.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1348222 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/index/CategoryListPayloadStream.java
+++ lucene/facet/src/java/org/apache/lucene/facet/index/CategoryListPayloadStream.java
@@ -50,7 +50,7 @@ begin_comment
 comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/**  * Accumulates category IDs for a single document, for writing in byte array  * form, for example, to a Lucene Payload.  *   * @lucene.experimental  */
+comment|/**  * Accumulates category IDs for a single document, for writing in byte array  * form, for example, to a Lucene payload.  *   * @lucene.experimental  */
 end_comment
 begin_class
 DECL|class|CategoryListPayloadStream
@@ -74,7 +74,7 @@ specifier|private
 name|IntEncoder
 name|encoder
 decl_stmt|;
-comment|/** Creates a Payload stream using the specified encoder. */
+comment|/** Creates a payload stream using the specified encoder. */
 DECL|method|CategoryListPayloadStream
 specifier|public
 name|CategoryListPayloadStream
