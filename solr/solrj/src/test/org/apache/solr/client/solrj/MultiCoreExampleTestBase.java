df25b45af834aa504e56a9865ca72d8d055dd15f
Sami Siren <siren@apache.org>
2012-06-13 07:58:34 +0000

    SOLR-3540 set data dir for core0 so that tests don't clash
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1349671 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/MultiCoreExampleTestBase.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/MultiCoreExampleTestBase.java
@@ -316,7 +316,7 @@ operator|.
 name|getName
 argument_list|()
 operator|+
-literal|"-"
+literal|"-core1-"
 operator|+
 name|System
 operator|.
@@ -328,6 +328,20 @@ name|dataDir2
 operator|.
 name|mkdirs
 argument_list|()
+expr_stmt|;
+name|System
+operator|.
+name|setProperty
+argument_list|(
+literal|"solr.core0.data.dir"
+argument_list|,
+name|this
+operator|.
+name|dataDir
+operator|.
+name|getCanonicalPath
+argument_list|()
+argument_list|)
 expr_stmt|;
 name|System
 operator|.
@@ -420,7 +434,7 @@ name|println
 argument_list|(
 literal|"!!!! WARNING: best effort to remove "
 operator|+
-name|dataDir
+name|dataDir2
 operator|.
 name|getAbsolutePath
 argument_list|()
