fc8542c6c71f35314508ff6f8b112dad17e3eea1
Robert Muir <rmuir@apache.org>
2012-07-24 04:58:28 +0000

    LUCENE-4044: get analysis/common tests passing
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene2510@1364887 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/hunspell/TestHunspellStemFilterFactory.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/hunspell/TestHunspellStemFilterFactory.java
@@ -97,11 +97,13 @@ name|org
 operator|.
 name|apache
 operator|.
-name|solr
+name|lucene
 operator|.
-name|core
+name|analysis
 operator|.
-name|SolrResourceLoader
+name|util
+operator|.
+name|ResourceAsStreamResourceLoader
 import|;
 end_import
 begin_comment
@@ -153,7 +155,7 @@ name|put
 argument_list|(
 literal|"dictionary"
 argument_list|,
-literal|"hunspell-test.dic"
+literal|"test.dic"
 argument_list|)
 expr_stmt|;
 name|args
@@ -162,7 +164,7 @@ name|put
 argument_list|(
 literal|"affix"
 argument_list|,
-literal|"hunspell-test.aff"
+literal|"test.aff"
 argument_list|)
 expr_stmt|;
 name|factory
@@ -184,9 +186,10 @@ operator|.
 name|inform
 argument_list|(
 operator|new
-name|SolrResourceLoader
+name|ResourceAsStreamResourceLoader
 argument_list|(
-literal|"solr/collection1"
+name|getClass
+argument_list|()
 argument_list|)
 argument_list|)
 expr_stmt|;
