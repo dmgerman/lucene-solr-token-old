637e4f2d20f5cb56ed0d709229f227d747c09ffc
Robert Muir <rmuir@apache.org>
2012-07-25 23:23:02 +0000

    LUCENE-4250: fix trap in PayloadFunction explain API
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1365837 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/payloads/PayloadNearQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/payloads/PayloadNearQuery.java
@@ -1082,6 +1082,20 @@ name|getValue
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|String
+name|field
+init|=
+operator|(
+operator|(
+name|SpanQuery
+operator|)
+name|getQuery
+argument_list|()
+operator|)
+operator|.
+name|getField
+argument_list|()
+decl_stmt|;
 comment|// now the payloads part
 name|Explanation
 name|payloadExpl
@@ -1091,6 +1105,8 @@ operator|.
 name|explain
 argument_list|(
 name|doc
+argument_list|,
+name|field
 argument_list|,
 name|scorer
 operator|.
