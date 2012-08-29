407afe806afa6f7278139796beed0f349c8b7546
Robert Muir <rmuir@apache.org>
2012-08-29 14:56:28 +0000

    Add an example that builds a CharFilter chain in Analyzer
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1378593 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/analysis/CharFilter.java
+++ lucene/core/src/java/org/apache/lucene/analysis/CharFilter.java
@@ -33,7 +33,7 @@ name|Reader
 import|;
 end_import
 begin_comment
-comment|/**  * Subclasses of CharFilter can be chained to filter a Reader  * They can be used as {@link java.io.Reader} with additional offset  * correction. {@link Tokenizer}s will automatically use {@link #correctOffset}  * if a CharFilter subclass is used.  *<p>  * This class is abstract: at a minimum you must implement {@link #read(char[], int, int)},  * transforming the input in some way from {@link #input}, and {@link #correct(int)}  * to adjust the offsets to match the originals.  *<p>  * You can optionally provide more efficient implementations of additional methods   * like {@link #read()}, {@link #read(char[])}, {@link #read(java.nio.CharBuffer)},  * but this is not required.  */
+comment|/**  * Subclasses of CharFilter can be chained to filter a Reader  * They can be used as {@link java.io.Reader} with additional offset  * correction. {@link Tokenizer}s will automatically use {@link #correctOffset}  * if a CharFilter subclass is used.  *<p>  * This class is abstract: at a minimum you must implement {@link #read(char[], int, int)},  * transforming the input in some way from {@link #input}, and {@link #correct(int)}  * to adjust the offsets to match the originals.  *<p>  * You can optionally provide more efficient implementations of additional methods   * like {@link #read()}, {@link #read(char[])}, {@link #read(java.nio.CharBuffer)},  * but this is not required.  *<p>  * For examples and integration with {@link Analyzer}, see the   * {@link org.apache.lucene.analysis Analysis package documentation}.  */
 end_comment
 begin_comment
 comment|// the way java.io.FilterReader should work!
