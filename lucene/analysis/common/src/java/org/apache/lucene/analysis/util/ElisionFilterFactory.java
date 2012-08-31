37df8f9e39cfb0571f8a17d41079cb1d7fbfcbc0
Chris M. Hostetter <hossman@apache.org>
2012-08-31 03:44:05 +0000

    SOLR-3441: ElisionFilterFactory is now MultiTermAware
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1379275 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/util/ElisionFilterFactory.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/util/ElisionFilterFactory.java
@@ -65,6 +65,8 @@ extends|extends
 name|TokenFilterFactory
 implements|implements
 name|ResourceLoaderAware
+implements|,
+name|MultiTermAwareComponent
 block|{
 DECL|field|articles
 specifier|private
@@ -153,6 +155,18 @@ name|input
 argument_list|,
 name|articles
 argument_list|)
+return|;
+block|}
+annotation|@
+name|Override
+DECL|method|getMultiTermComponent
+specifier|public
+name|AbstractAnalysisFactory
+name|getMultiTermComponent
+parameter_list|()
+block|{
+return|return
+name|this
 return|;
 block|}
 block|}
