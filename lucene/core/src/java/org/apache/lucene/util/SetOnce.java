3d769c4470045aca8a902de7a01caeda56f2709c
Robert Muir <rmuir@apache.org>
2012-09-15 12:45:16 +0000

    use the correct exception subclass
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1385055 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/SetOnce.java
+++ lucene/core/src/java/org/apache/lucene/util/SetOnce.java
@@ -48,7 +48,7 @@ specifier|final
 class|class
 name|AlreadySetException
 extends|extends
-name|RuntimeException
+name|IllegalStateException
 block|{
 DECL|method|AlreadySetException
 specifier|public
