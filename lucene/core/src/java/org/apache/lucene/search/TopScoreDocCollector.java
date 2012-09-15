945b3e4f03a295299e7fdfd039bd25337b4e7b19
Robert Muir <rmuir@apache.org>
2012-09-15 12:41:12 +0000

    javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1385051 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/TopScoreDocCollector.java
+++ lucene/core/src/java/org/apache/lucene/search/TopScoreDocCollector.java
@@ -37,7 +37,7 @@ name|AtomicReaderContext
 import|;
 end_import
 begin_comment
-comment|/**  * A {@link Collector} implementation that collects the top-scoring hits,  * returning them as a {@link TopDocs}. This is used by {@link IndexSearcher} to  * implement {@link TopDocs}-based search. Hits are sorted by score descending  * and then (when the scores are tied) docID ascending. When you create an  * instance of this collector you should know in advance whether documents are  * going to be collected in doc Id order or not.  *  *<p><b>NOTE</b>: The values {@link Float#NaN} and  * {Float#NEGATIVE_INFINITY} are not valid scores.  This  * collector will not properly collect hits with such  * scores.  */
+comment|/**  * A {@link Collector} implementation that collects the top-scoring hits,  * returning them as a {@link TopDocs}. This is used by {@link IndexSearcher} to  * implement {@link TopDocs}-based search. Hits are sorted by score descending  * and then (when the scores are tied) docID ascending. When you create an  * instance of this collector you should know in advance whether documents are  * going to be collected in doc Id order or not.  *  *<p><b>NOTE</b>: The values {@link Float#NaN} and  * {@link Float#NEGATIVE_INFINITY} are not valid scores.  This  * collector will not properly collect hits with such  * scores.  */
 end_comment
 begin_class
 DECL|class|TopScoreDocCollector
