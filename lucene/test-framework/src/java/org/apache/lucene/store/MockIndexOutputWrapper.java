728733d1ff41f86db355fb73f8fc01d7a59c9802
Michael McCandless <mikemccand@apache.org>
2012-10-04 22:55:11 +0000

    bring back third bum: he seems to find bugs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1394317 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/store/MockIndexOutputWrapper.java
+++ lucene/test-framework/src/java/org/apache/lucene/store/MockIndexOutputWrapper.java
@@ -65,12 +65,6 @@ name|first
 init|=
 literal|true
 decl_stmt|;
-DECL|field|rateLimiter
-specifier|private
-specifier|final
-name|RateLimiter
-name|rateLimiter
-decl_stmt|;
 DECL|field|name
 specifier|final
 name|String
@@ -100,9 +94,6 @@ name|delegate
 parameter_list|,
 name|String
 name|name
-parameter_list|,
-name|RateLimiter
-name|rateLimiter
 parameter_list|)
 block|{
 name|this
@@ -122,12 +113,6 @@ operator|.
 name|delegate
 operator|=
 name|delegate
-expr_stmt|;
-name|this
-operator|.
-name|rateLimiter
-operator|=
-name|rateLimiter
 expr_stmt|;
 block|}
 annotation|@
@@ -298,6 +283,8 @@ literal|0
 decl_stmt|;
 if|if
 condition|(
+name|dir
+operator|.
 name|rateLimiter
 operator|!=
 literal|null
@@ -307,6 +294,8 @@ operator|>=
 literal|1000
 condition|)
 block|{
+name|dir
+operator|.
 name|rateLimiter
 operator|.
 name|pause
