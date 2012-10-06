ca59f9336bc8b1dc22d69b2160b65f985e1f77a2
Dawid Weiss <dweiss@apache.org>
2012-10-06 18:56:15 +0000

    These filters are now inside the runner itself.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1395149 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/util/QuickPatchThreadsFilter.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/QuickPatchThreadsFilter.java
@@ -26,7 +26,7 @@ begin_comment
 comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/**  * Last minute patches.  * TODO: remove when integrated in system filters in rr.  */
+comment|/**  * Last minute patches.  */
 end_comment
 begin_class
 DECL|class|QuickPatchThreadsFilter
@@ -47,60 +47,6 @@ name|Thread
 name|t
 parameter_list|)
 block|{
-comment|// MacOS system thread.
-if|if
-condition|(
-name|t
-operator|.
-name|getName
-argument_list|()
-operator|.
-name|equals
-argument_list|(
-literal|"AWT-AppKit"
-argument_list|)
-condition|)
-block|{
-return|return
-literal|true
-return|;
-block|}
-comment|// J9 memory pool thread.
-if|if
-condition|(
-name|t
-operator|.
-name|getName
-argument_list|()
-operator|.
-name|equals
-argument_list|(
-literal|"MemoryPoolMXBean notification dispatcher"
-argument_list|)
-condition|)
-block|{
-return|return
-literal|true
-return|;
-block|}
-comment|// forked process reaper on Unixish systems
-if|if
-condition|(
-name|t
-operator|.
-name|getName
-argument_list|()
-operator|.
-name|equals
-argument_list|(
-literal|"process reaper"
-argument_list|)
-condition|)
-block|{
-return|return
-literal|true
-return|;
-block|}
 return|return
 literal|false
 return|;
