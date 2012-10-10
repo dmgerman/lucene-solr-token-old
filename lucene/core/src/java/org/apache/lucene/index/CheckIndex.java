e22383640407930e73147fbefcbbb7a651e11bed
Robert Muir <rmuir@apache.org>
2012-10-10 03:10:12 +0000

    Make the checkindex output for docvalues consistent and line up so the output is pretty
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1396454 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/index/CheckIndex.java
+++ lucene/core/src/java/org/apache/lucene/index/CheckIndex.java
@@ -6748,7 +6748,7 @@ name|infoStream
 operator|.
 name|print
 argument_list|(
-literal|"    test: DocValues........"
+literal|"    test: docvalues..........."
 argument_list|)
 expr_stmt|;
 block|}
@@ -6846,11 +6846,13 @@ name|status
 operator|.
 name|docCount
 operator|+
-literal|" total doc Count; Num DocValues Fields "
+literal|" total doc count; "
 operator|+
 name|status
 operator|.
 name|totalValueFields
+operator|+
+literal|" docvalues fields]"
 argument_list|)
 expr_stmt|;
 block|}
