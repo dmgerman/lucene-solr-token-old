475e2dd14830fc490c5b1443238fd749d1dfe4b1
Mark Robert Miller <markrmiller@apache.org>
2012-10-27 15:41:22 +0000

    there should be a wait on this retry
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1402808 13f79535-47bb-0310-9956-ffa450edef68

--- solr/test-framework/src/java/org/apache/solr/cloud/AbstractFullDistribZkTestBase.java
+++ solr/test-framework/src/java/org/apache/solr/cloud/AbstractFullDistribZkTestBase.java
@@ -7693,6 +7693,13 @@ literal|" jetty"
 argument_list|)
 expr_stmt|;
 block|}
+name|Thread
+operator|.
+name|sleep
+argument_list|(
+literal|1000
+argument_list|)
+expr_stmt|;
 name|clusterState
 operator|=
 name|zkStateReader
