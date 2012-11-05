42050d6108e7b4b44efea3ed14d36e628ebfb80c
Michael McCandless <mikemccand@apache.org>
2012-11-05 21:17:11 +0000

    LUCENE-4534: handle 0 byte values in lookup keys
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1405960 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/suggest/src/java/org/apache/lucene/search/suggest/fst/WFSTCompletionLookup.java
+++ lucene/suggest/src/java/org/apache/lucene/search/suggest/fst/WFSTCompletionLookup.java
@@ -472,11 +472,6 @@ operator|new
 name|WFSTTermFreqIteratorWrapper
 argument_list|(
 name|iterator
-argument_list|,
-name|BytesRef
-operator|.
-name|getUTF8SortedAsUnicodeComparator
-argument_list|()
 argument_list|)
 decl_stmt|;
 name|IntsRef
@@ -1357,12 +1352,6 @@ name|WFSTTermFreqIteratorWrapper
 parameter_list|(
 name|TermFreqIterator
 name|source
-parameter_list|,
-name|Comparator
-argument_list|<
-name|BytesRef
-argument_list|>
-name|comparator
 parameter_list|)
 throws|throws
 name|IOException
@@ -1370,10 +1359,6 @@ block|{
 name|super
 argument_list|(
 name|source
-argument_list|,
-name|comparator
-argument_list|,
-literal|true
 argument_list|)
 expr_stmt|;
 block|}
@@ -1409,7 +1394,7 @@ name|spare
 operator|.
 name|length
 operator|+
-literal|5
+literal|4
 operator|>=
 name|buffer
 operator|.
@@ -1428,7 +1413,7 @@ name|spare
 operator|.
 name|length
 operator|+
-literal|5
+literal|4
 argument_list|)
 expr_stmt|;
 block|}
@@ -1456,17 +1441,6 @@ operator|.
 name|length
 argument_list|)
 expr_stmt|;
-name|output
-operator|.
-name|writeByte
-argument_list|(
-operator|(
-name|byte
-operator|)
-literal|0
-argument_list|)
-expr_stmt|;
-comment|// separator: not used, just for sort order
 name|output
 operator|.
 name|writeInt
@@ -1506,6 +1480,14 @@ name|ByteArrayDataInput
 name|tmpInput
 parameter_list|)
 block|{
+name|scratch
+operator|.
+name|length
+operator|-=
+literal|4
+expr_stmt|;
+comment|// int
+comment|// skip suggestion:
 name|tmpInput
 operator|.
 name|reset
@@ -1513,27 +1495,18 @@ argument_list|(
 name|scratch
 operator|.
 name|bytes
-argument_list|)
-expr_stmt|;
-name|tmpInput
+argument_list|,
+name|scratch
 operator|.
-name|skipBytes
-argument_list|(
+name|offset
+operator|+
 name|scratch
 operator|.
 name|length
-operator|-
+argument_list|,
 literal|4
 argument_list|)
 expr_stmt|;
-comment|// suggestion + separator
-name|scratch
-operator|.
-name|length
-operator|-=
-literal|5
-expr_stmt|;
-comment|// sep + long
 return|return
 name|tmpInput
 operator|.
