81a608f03bc5909425438e9e6d98b527cbbc40d6
Michael McCandless <mikemccand@apache.org>
2012-11-09 18:17:16 +0000

    start at ST producer
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1407573 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/codecs/SimpleDocValuesFormat.java
+++ lucene/core/src/java/org/apache/lucene/codecs/SimpleDocValuesFormat.java
@@ -33,6 +33,19 @@ name|lucene
 operator|.
 name|index
 operator|.
+name|SegmentReadState
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|index
+operator|.
 name|SegmentWriteState
 import|;
 end_import
@@ -62,7 +75,18 @@ throws|throws
 name|IOException
 function_decl|;
 comment|// nocommit do this:
-comment|//public abstract SimpleDVProducer fieldsProducer(SegmentReadState state) throws IOException;
+DECL|method|fieldsProducer
+specifier|public
+specifier|abstract
+name|PerDocProducer
+name|fieldsProducer
+parameter_list|(
+name|SegmentReadState
+name|state
+parameter_list|)
+throws|throws
+name|IOException
+function_decl|;
 block|}
 end_class
 end_unit
