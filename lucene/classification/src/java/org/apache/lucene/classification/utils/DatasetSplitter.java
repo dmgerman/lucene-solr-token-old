a3e6deef5e230ebd7742a7ad6317784341fdb367
Tommaso Teofili <tommaso@apache.org>
2012-11-29 08:28:08 +0000

    [LUCENE-4345] - fixed javadoc for DatasetSplitter
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1415079 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/classification/src/java/org/apache/lucene/classification/utils/DatasetSplitter.java
+++ lucene/classification/src/java/org/apache/lucene/classification/utils/DatasetSplitter.java
@@ -263,6 +263,7 @@ operator|=
 name|testRatio
 expr_stmt|;
 block|}
+comment|/**    * Split a given index into 3 indexes for training, test and cross validation tasks respectively    * @param originalIndex an {@link AtomicReader} on the source index    * @param trainingIndex a {@link Directory} used to write the training index    * @param testIndex a {@link Directory} used to write the test index    * @param crossValidationIndex a {@link Directory} used to write the cross validation index    * @param analyzer {@link Analyzer} used to create the new docs    * @param fieldNames names of fields that need to be put in the new indexes or<code>null</code> if all should be used    * @throws IOException if any writing operation fails on any of the indexes    */
 DECL|method|split
 specifier|public
 name|void
