476e21759d23b1ad74a4af811e216f9866b9689d
Yonik Seeley <yonik@apache.org>
2012-12-09 16:54:01 +0000

    SOLR-2592: fix implicit router to return found slice, test querying via shards
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1419019 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/java/org/apache/solr/common/cloud/DocRouter.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/DocRouter.java
@@ -869,6 +869,7 @@ return|return
 name|ranges
 return|;
 block|}
+comment|/** Returns the Slice that the document should reside on, or null if there is not enough information */
 DECL|method|getTargetSlice
 specifier|public
 specifier|abstract
--- solr/solrj/src/java/org/apache/solr/common/cloud/ImplicitDocRouter.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ImplicitDocRouter.java
@@ -214,6 +214,9 @@ name|collection
 argument_list|)
 throw|;
 block|}
+return|return
+name|slice
+return|;
 block|}
 return|return
 literal|null
