69c24c8207813a9e0c3e7c14a270224b7947bfc5
Mark Robert Miller <markrmiller@apache.org>
2012-12-12 00:38:14 +0000

    SOLR-4161: add a little more protection
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1420500 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
+++ solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
@@ -370,6 +370,19 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
+if|if
+condition|(
+name|closed
+condition|)
+block|{
+throw|throw
+operator|new
+name|RuntimeException
+argument_list|(
+literal|"SolrCoreState already closed"
+argument_list|)
+throw|;
+block|}
 synchronized|synchronized
 init|(
 name|writerPauseLock
@@ -626,7 +639,7 @@ throw|throw
 operator|new
 name|RuntimeException
 argument_list|(
-literal|"Already closed"
+literal|"SolrCoreState already closed"
 argument_list|)
 throw|;
 block|}
