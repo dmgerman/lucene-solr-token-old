ea614b82e46d453ec403561df298f1e2b46e8ff4
Mark Robert Miller <markrmiller@apache.org>
2012-12-12 20:34:37 +0000

    try a 300ms wait on startup to be sure our core(s) have loaded
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1420954 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/client/solrj/embedded/JettySolrRunner.java
+++ solr/core/src/java/org/apache/solr/client/solrj/embedded/JettySolrRunner.java
@@ -1225,6 +1225,14 @@ argument_list|)
 throw|;
 block|}
 block|}
+name|this
+operator|.
+name|wait
+argument_list|(
+literal|300
+argument_list|)
+expr_stmt|;
+comment|// always 100ms - can take a moment for our cores to come up
 block|}
 name|System
 operator|.
