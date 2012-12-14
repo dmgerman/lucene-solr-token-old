fe516a768fcaa5119f683a8a9aa38a9dacce2f94
Mark Robert Miller <markrmiller@apache.org>
2012-12-14 15:03:32 +0000

    SOLR-4187: NPE on Directory release
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1421914 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/SolrCore.java
+++ solr/core/src/java/org/apache/solr/core/SolrCore.java
@@ -1833,6 +1833,13 @@ expr_stmt|;
 block|}
 finally|finally
 block|{
+if|if
+condition|(
+name|dir
+operator|!=
+literal|null
+condition|)
+block|{
 try|try
 block|{
 name|getDirectoryFactory
@@ -1861,6 +1868,7 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 block|}
 if|if
