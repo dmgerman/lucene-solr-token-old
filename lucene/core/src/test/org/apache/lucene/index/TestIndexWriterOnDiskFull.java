723c65c0c895f0d11ce0b639476ce68fe99059ae
Robert Muir <rmuir@apache.org>
2013-01-21 16:05:04 +0000

    improve dv testing
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1436437 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestIndexWriterOnDiskFull.java
+++ lucene/core/src/test/org/apache/lucene/index/TestIndexWriterOnDiskFull.java
@@ -251,9 +251,6 @@ end_import
 begin_comment
 comment|/**  * Tests for IndexWriter when the disk runs out of space  */
 end_comment
-begin_comment
-comment|//nocommit: make sure disk full etc tests here test DV2.0
-end_comment
 begin_class
 DECL|class|TestIndexWriterOnDiskFull
 specifier|public
