09e00c431eab9ed152c26d2abf1843360c4dcddb
Shai Erera <shaie@apache.org>
2013-02-08 18:28:56 +0000

    rename Dummy to FacetPackage
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1444180 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/FacetPackage.java
+++ lucene/facet/src/java/org/apache/lucene/facet/FacetPackage.java
@@ -15,18 +15,18 @@ begin_comment
 comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/** ignore me: I workaround bugs in oracle's javadoc */
+comment|/**  * Required so that userguide files are copied as part of javadocs generation.  * Otherwise, if the root facet package contains no classes, doc-files aren't  * copied.  */
 end_comment
 begin_class
-DECL|class|Dummy
+DECL|class|FacetPackage
 specifier|public
+specifier|final
 class|class
-name|Dummy
+name|FacetPackage
 block|{
-comment|// TODO: better way to workaround this?
-DECL|method|Dummy
+DECL|method|FacetPackage
 specifier|private
-name|Dummy
+name|FacetPackage
 parameter_list|()
 block|{}
 block|}
