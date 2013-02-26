d6458d3a14a9fdfa7f5adea3dc152d0fb38a64cc
Robert Muir <rmuir@apache.org>
2013-02-26 20:56:29 +0000

    fix broken regex from r1225920 allowing most of unicode as a letter
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1450410 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/analysis/MockTokenizer.java
+++ lucene/test-framework/src/java/org/apache/lucene/analysis/MockTokenizer.java
@@ -189,7 +189,7 @@ argument_list|(
 operator|new
 name|RegExp
 argument_list|(
-literal|"[A-Za-zÂªÂµÂºÃ-ÃÃ-Ã¶Ã¸-ï¼º]+"
+literal|"[A-Za-zÂªÂµÂºÃ-ÃÃ-Ã¶Ã¸-Ë]+"
 argument_list|)
 operator|.
 name|toAutomaton
