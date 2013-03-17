279245875ff8eba04cc041b91c223ecc98ab7054
Mark Robert Miller <markrmiller@apache.org>
2013-03-17 23:18:02 +0000

    SOLR-4601: A Collection that is only partially created and then deleted will leave pre allocated shard information in ZooKeeper.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1457585 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/OverseerCollectionProcessor.java
+++ solr/core/src/java/org/apache/solr/cloud/OverseerCollectionProcessor.java
@@ -2414,7 +2414,11 @@ literal|" on collection "
 operator|+
 name|collectionName
 operator|+
-literal|" is higher than or equal to the number of Solr instances currently live ("
+literal|" is higher than or equal to the number of Solr instances currently live or part of your "
+operator|+
+name|CREATE_NODE_SET
+operator|+
+literal|"("
 operator|+
 name|nodeList
 operator|.
@@ -2858,7 +2862,7 @@ block|}
 block|}
 DECL|method|collectionCmd
 specifier|private
-name|boolean
+name|void
 name|collectionCmd
 parameter_list|(
 name|ClusterState
@@ -3181,11 +3185,6 @@ expr_stmt|;
 block|}
 block|}
 block|}
-name|int
-name|failed
-init|=
-literal|0
-decl_stmt|;
 name|ShardResponse
 name|srsp
 decl_stmt|;
@@ -3221,21 +3220,6 @@ operator|!=
 literal|null
 condition|)
 do|;
-comment|// if all calls succeeded, return true
-if|if
-condition|(
-name|failed
-operator|>
-literal|0
-condition|)
-block|{
-return|return
-literal|false
-return|;
-block|}
-return|return
-literal|true
-return|;
 block|}
 DECL|method|processResponse
 specifier|private
