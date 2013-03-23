361c03e866b675a80de5cb9a89e91369d9e60f41
Mark Robert Miller <markrmiller@apache.org>
2013-03-23 02:40:12 +0000

    remove cruft
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1460076 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
+++ solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
@@ -68,19 +68,6 @@ name|org
 operator|.
 name|apache
 operator|.
-name|lucene
-operator|.
-name|store
-operator|.
-name|AlreadyClosedException
-import|;
-end_import
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
 name|solr
 operator|.
 name|cloud
@@ -635,9 +622,13 @@ condition|)
 block|{
 throw|throw
 operator|new
-name|AlreadyClosedException
+name|SolrException
 argument_list|(
-literal|"SolrCoreState already closed"
+name|ErrorCode
+operator|.
+name|SERVICE_UNAVAILABLE
+argument_list|,
+literal|"Already closed"
 argument_list|)
 throw|;
 block|}
