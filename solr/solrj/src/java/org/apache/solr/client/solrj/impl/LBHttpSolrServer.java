6a31ad24feb9ef6cf3a54d27195b51711c923c9b
Robert Muir <rmuir@apache.org>
2013-05-14 21:29:52 +0000

    SOLR-4448: Allow the solr internal load balancer to be more easily pluggable
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1482600 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/LBHttpSolrServer.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/LBHttpSolrServer.java
@@ -245,7 +245,7 @@ argument_list|()
 decl_stmt|;
 comment|// access to aliveServers should be synchronized on itself
 DECL|field|zombieServers
-specifier|private
+specifier|protected
 specifier|final
 name|Map
 argument_list|<
@@ -338,7 +338,7 @@ argument_list|)
 expr_stmt|;
 block|}
 DECL|class|ServerWrapper
-specifier|private
+specifier|protected
 specifier|static
 class|class
 name|ServerWrapper
@@ -1358,7 +1358,7 @@ throw|;
 block|}
 block|}
 DECL|method|addZombie
-specifier|private
+specifier|protected
 name|Exception
 name|addZombie
 parameter_list|(
