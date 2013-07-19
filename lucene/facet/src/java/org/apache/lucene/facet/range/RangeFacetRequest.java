c048497dd72cbf393439958ff85b06a22bd17ae7
Shai Erera <shaie@apache.org>
2013-07-19 21:58:26 +0000

    LUCENE-4894: remove facet userguide
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1505041 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/range/RangeFacetRequest.java
+++ lucene/facet/src/java/org/apache/lucene/facet/range/RangeFacetRequest.java
@@ -123,6 +123,11 @@ name|Range
 index|[]
 name|ranges
 decl_stmt|;
+annotation|@
+name|SuppressWarnings
+argument_list|(
+literal|"unchecked"
+argument_list|)
 DECL|method|RangeFacetRequest
 specifier|public
 name|RangeFacetRequest
