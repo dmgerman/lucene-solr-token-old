34f965e79c32e5f3cea7571e5e22ae3cec1a4cf0
Dawid Weiss <dweiss@apache.org>
2013-08-09 14:32:48 +0000

    SOLR-4708: one more follow-up to a class that didn't extend from the base Solr test class.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1512322 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/embedded/JettyWebappTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/embedded/JettyWebappTest.java
@@ -79,6 +79,17 @@ name|apache
 operator|.
 name|solr
 operator|.
+name|SolrTestCaseJ4
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
 name|util
 operator|.
 name|ExternalPaths
@@ -206,7 +217,7 @@ specifier|public
 class|class
 name|JettyWebappTest
 extends|extends
-name|LuceneTestCase
+name|SolrTestCaseJ4
 block|{
 DECL|field|port
 name|int
