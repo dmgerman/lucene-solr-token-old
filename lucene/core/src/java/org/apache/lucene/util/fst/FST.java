ff88738dc2009711ed6782b420a10c670e5428d7
Michael McCandless <mikemccand@apache.org>
2013-08-15 22:42:54 +0000

    LUCENE-5152: make assert less costly
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1514520 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/fst/FST.java
+++ lucene/core/src/java/org/apache/lucene/util/fst/FST.java
@@ -5325,10 +5325,6 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-assert|assert
-name|assertRootArcs
-argument_list|()
-assert|;
 if|if
 condition|(
 name|labelToMatch
@@ -5427,6 +5423,12 @@ operator|.
 name|length
 condition|)
 block|{
+comment|// LUCENE-5152: detect tricky cases where caller
+comment|// modified previously returned cached root-arcs:
+assert|assert
+name|assertRootArcs
+argument_list|()
+assert|;
 specifier|final
 name|Arc
 argument_list|<
