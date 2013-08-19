1fe26269d2066add9456f5ed1f4c3afe1e135cbe
Robert Muir <rmuir@apache.org>
2013-08-19 17:51:49 +0000

    move to test-framework (only needed for simulating old codecs which have no concept of missing)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene5178@1515532 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/codecs/MissingOrdRemapper.java
+++ lucene/test-framework/src/java/org/apache/lucene/codecs/MissingOrdRemapper.java
@@ -37,13 +37,7 @@ begin_comment
 comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/**   * a utility class to write missing values for SORTED_SET as if they were the empty string  * (to simulate pre-Lucene4.5 dv behavior for testing old codecs)  */
-end_comment
-begin_comment
-comment|// nocommit: move this to test-framework with all the impersonators of
-end_comment
-begin_comment
-comment|// these old codecs once new memory/disk codecs are written that support missing
+comment|/**   * a utility class to write missing values for SORTED as if they were the empty string  * (to simulate pre-Lucene4.5 dv behavior for testing old codecs)  */
 end_comment
 begin_class
 DECL|class|MissingOrdRemapper
