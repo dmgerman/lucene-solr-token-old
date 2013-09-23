dfb7a3499b6664398b1c41cc0a21b7e2244c995a
Shalin Shekhar Mangar <shalin@apache.org>
2013-09-23 15:14:10 +0000

    SOLR-5259: Fix typo in error message when _version_ field is missing
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1525620 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/VersionInfo.java
+++ solr/core/src/java/org/apache/solr/update/VersionInfo.java
@@ -247,7 +247,7 @@ name|errPrefix
 init|=
 name|VERSION_FIELD
 operator|+
-literal|"field must exist in schema, using indexed=\"true\" stored=\"true\" and multiValued=\"false\""
+literal|" field must exist in schema, using indexed=\"true\" stored=\"true\" and multiValued=\"false\""
 decl_stmt|;
 name|SchemaField
 name|sf
