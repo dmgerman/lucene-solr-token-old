26a0d99e79542a9836e53338cfef992b31523a43
Shai Erera <shaie@apache.org>
2013-09-24 08:23:37 +0000

    LUCENE-5237: assert parameters validity as well as minor improvements
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1525813 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/util/StemmerUtil.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/util/StemmerUtil.java
@@ -17,7 +17,7 @@ begin_comment
 comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 begin_comment
-comment|/** Some commonly-used stemming functions */
+comment|/**  * Some commonly-used stemming functions  *   * @lucene.internal  */
 end_comment
 begin_class
 DECL|class|StemmerUtil
@@ -276,12 +276,21 @@ name|int
 name|len
 parameter_list|)
 block|{
+assert|assert
+name|pos
+operator|<
+name|len
+assert|;
 if|if
 condition|(
 name|pos
 operator|<
 name|len
+operator|-
+literal|1
 condition|)
+block|{
+comment|// don't arraycopy if asked to delete last character
 name|System
 operator|.
 name|arraycopy
@@ -303,6 +312,7 @@ operator|-
 literal|1
 argument_list|)
 expr_stmt|;
+block|}
 return|return
 name|len
 operator|-
@@ -330,34 +340,49 @@ name|int
 name|nChars
 parameter_list|)
 block|{
-comment|// TODO: speed up, this is silly
-for|for
-control|(
-name|int
-name|i
-init|=
-literal|0
-init|;
-name|i
-operator|<
+assert|assert
+name|pos
+operator|+
 name|nChars
-condition|;
-name|i
-operator|++
-control|)
+operator|<=
 name|len
-operator|=
-name|delete
+assert|;
+if|if
+condition|(
+name|pos
+operator|+
+name|nChars
+operator|<
+name|len
+condition|)
+block|{
+comment|// don't arraycopy if asked to delete the last characters
+name|System
+operator|.
+name|arraycopy
 argument_list|(
+name|s
+argument_list|,
+name|pos
+operator|+
+name|nChars
+argument_list|,
 name|s
 argument_list|,
 name|pos
 argument_list|,
 name|len
+operator|-
+name|pos
+operator|-
+name|nChars
 argument_list|)
 expr_stmt|;
+block|}
 return|return
 name|len
+operator|-
+name|nChars
 return|;
 block|}
 block|}
