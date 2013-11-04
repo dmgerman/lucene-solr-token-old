74e807c8124e1dc4b63c07195cf81238d555f891
Michael McCandless <mikemccand@apache.org>
2013-11-04 11:16:00 +0000

    change assume to SuppressCodecs for more test iterations over time
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1538556 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/demo/src/test/org/apache/lucene/demo/facet/TestSimpleSortedSetFacetsExample.java
+++ lucene/demo/src/test/org/apache/lucene/demo/facet/TestSimpleSortedSetFacetsExample.java
@@ -93,6 +93,21 @@ operator|.
 name|util
 operator|.
 name|LuceneTestCase
+operator|.
+name|SuppressCodecs
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
 import|;
 end_import
 begin_import
@@ -107,7 +122,19 @@ end_import
 begin_comment
 comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
+begin_comment
+comment|// We require sorted set DVs:
+end_comment
 begin_class
+annotation|@
+name|SuppressCodecs
+argument_list|(
+block|{
+literal|"Lucene40"
+block|,
+literal|"Lucene41"
+block|}
+argument_list|)
 DECL|class|TestSimpleSortedSetFacetsExample
 specifier|public
 class|class
@@ -372,14 +399,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-name|assumeTrue
-argument_list|(
-literal|"Test requires SortedSetDV support"
-argument_list|,
-name|defaultCodecSupportsSortedSet
-argument_list|()
-argument_list|)
-expr_stmt|;
 name|List
 argument_list|<
 name|FacetResult
@@ -421,14 +440,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-name|assumeTrue
-argument_list|(
-literal|"Test requires SortedSetDV support"
-argument_list|,
-name|defaultCodecSupportsSortedSet
-argument_list|()
-argument_list|)
-expr_stmt|;
 name|List
 argument_list|<
 name|FacetResult
