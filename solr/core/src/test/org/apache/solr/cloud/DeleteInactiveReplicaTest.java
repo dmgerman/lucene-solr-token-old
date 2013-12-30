5505dfce7269b8e5dcb440993f49af20558502d3
Mark Robert Miller <markrmiller@apache.org>
2013-12-30 04:28:35 +0000

    SOLR-5580: Remove more code that is not legal for determining if a core is new or not.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1554138 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/DeleteInactiveReplicaTest.java
+++ solr/core/src/test/org/apache/solr/cloud/DeleteInactiveReplicaTest.java
@@ -225,7 +225,21 @@ operator|.
 name|Before
 import|;
 end_import
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Ignore
+import|;
+end_import
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Not currently valid see SOLR-5580"
+argument_list|)
 DECL|class|DeleteInactiveReplicaTest
 specifier|public
 class|class
