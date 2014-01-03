3241fbff592fba5a25405eec601b504465469844
Mark Robert Miller <markrmiller@apache.org>
2014-01-03 19:06:22 +0000

    SOLR-5577: fix possible race on canceling timer on close
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1555204 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/java/org/apache/solr/common/cloud/ConnectionManager.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ConnectionManager.java
@@ -798,14 +798,7 @@ name|likelyExpired
 operator|=
 literal|true
 expr_stmt|;
-if|if
-condition|(
-name|this
-operator|.
-name|disconnectedTimer
-operator|!=
-literal|null
-condition|)
+try|try
 block|{
 name|this
 operator|.
@@ -814,6 +807,17 @@ operator|.
 name|cancel
 argument_list|()
 expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|NullPointerException
+name|e
+parameter_list|)
+block|{
+comment|// fine
+block|}
+finally|finally
+block|{
 name|this
 operator|.
 name|disconnectedTimer
