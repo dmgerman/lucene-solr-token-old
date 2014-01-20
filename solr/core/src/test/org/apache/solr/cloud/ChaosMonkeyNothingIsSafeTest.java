8315fb7960cbcdc99d0750c49348da7986b67003
Mark Robert Miller <markrmiller@apache.org>
2014-01-20 16:14:27 +0000

    tests: improve assertion
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1559764 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
@@ -1073,11 +1073,9 @@ name|FullThrottleStopableIndexingThread
 operator|)
 condition|)
 block|{
-name|assertEquals
+name|assertFalse
 argument_list|(
-literal|"There were expected update fails"
-argument_list|,
-literal|0
+literal|"There were too many update fails - we expect it can happen, but shouldn't easily"
 argument_list|,
 operator|(
 operator|(
@@ -1088,6 +1086,8 @@ operator|)
 operator|.
 name|getFails
 argument_list|()
+operator|>
+literal|1
 argument_list|)
 expr_stmt|;
 block|}
