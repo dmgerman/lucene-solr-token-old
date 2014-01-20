edbe3598b66fe10e0262f94a76a4039262ebe408
Mark Robert Miller <markrmiller@apache.org>
2014-01-20 16:31:12 +0000

    SOLR-5488: print exception stack trace rather than eating it
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1559770 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/analytics/AbstractAnalyticsStatsTest.java
+++ solr/core/src/test/org/apache/solr/analytics/AbstractAnalyticsStatsTest.java
@@ -729,6 +729,11 @@ name|Exception
 name|e
 parameter_list|)
 block|{
+name|e
+operator|.
+name|printStackTrace
+argument_list|()
+expr_stmt|;
 name|fail
 argument_list|(
 literal|"Caught exception in getStatResult, xPath = "
