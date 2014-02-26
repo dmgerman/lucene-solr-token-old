05410c2c33717cf38e980597c1ccf413d10c2348
Robert Muir <rmuir@apache.org>
2014-02-26 06:13:26 +0000

    add missing javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1571928 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/taxonomy/CachedOrdinalsReader.java
+++ lucene/facet/src/java/org/apache/lucene/facet/taxonomy/CachedOrdinalsReader.java
@@ -344,7 +344,7 @@ block|}
 block|}
 return|;
 block|}
-comment|/** Holds the cached ordinals in two paralel {@code int[]} arrays. */
+comment|/** Holds the cached ordinals in two parallel {@code int[]} arrays. */
 DECL|class|CachedOrds
 specifier|public
 specifier|static
@@ -603,6 +603,7 @@ name|ords
 expr_stmt|;
 block|}
 block|}
+comment|/** Returns number of bytes used by this cache entry */
 DECL|method|ramBytesUsed
 specifier|public
 name|long
