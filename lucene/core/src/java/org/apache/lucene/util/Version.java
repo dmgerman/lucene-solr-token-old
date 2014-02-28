e3efef82656290757b419a05b84ee44fc9c5630a
Robert Muir <rmuir@apache.org>
2014-02-28 20:00:40 +0000

    LUCENE-5482: add missing constant
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1573059 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -99,6 +99,13 @@ name|Deprecated
 DECL|enum constant|LUCENE_47
 name|LUCENE_47
 block|,
+comment|/**    * Match settings and bugs in Lucene's 4.8 release.    * @deprecated (5.0) Use latest    */
+DECL|enum constant|Deprecated
+annotation|@
+name|Deprecated
+DECL|enum constant|LUCENE_48
+name|LUCENE_48
+block|,
 comment|/** Match settings and bugs in Lucene's 5.0 release.    *<p>    *  Use this to get the latest&amp; greatest settings, bug    *  fixes, etc, for Lucene.    */
 DECL|enum constant|LUCENE_50
 name|LUCENE_50
