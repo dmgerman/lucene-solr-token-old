dc591c06a07fc344f80adf01ebbeb2c5f465d85b
Shai Erera <shaie@apache.org>
2014-03-20 11:33:38 +0000

    LUCENE-5476: use empty diamond
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1579596 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/RandomSamplingFacetsCollector.java
+++ lucene/facet/src/java/org/apache/lucene/facet/RandomSamplingFacetsCollector.java
@@ -480,9 +480,7 @@ name|sampledDocsList
 init|=
 operator|new
 name|ArrayList
-argument_list|<
-name|MatchingDocs
-argument_list|>
+argument_list|<>
 argument_list|(
 name|matchingDocsList
 operator|.
