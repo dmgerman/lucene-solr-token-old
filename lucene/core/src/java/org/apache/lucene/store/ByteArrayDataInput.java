ba52d82e52b34dba6623216d06c8dd7424e7a0af
Adrien Grand <jpountz@apache.org>
2014-04-10 07:12:38 +0000

    LUCENE-5583: Add DataInput.skipBytes, ChecksumIndexInput can now seek forward.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1586231 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/store/ByteArrayDataInput.java
+++ lucene/core/src/java/org/apache/lucene/store/ByteArrayDataInput.java
@@ -224,12 +224,14 @@ operator|==
 name|limit
 return|;
 block|}
+annotation|@
+name|Override
 DECL|method|skipBytes
 specifier|public
 name|void
 name|skipBytes
 parameter_list|(
-name|int
+name|long
 name|count
 parameter_list|)
 block|{
