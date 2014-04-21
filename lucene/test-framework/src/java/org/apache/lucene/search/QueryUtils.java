6a950e334087d59345a5b5f8b2ec8e06c7799d62
Michael McCandless <mikemccand@apache.org>
2014-04-21 16:40:52 +0000

    if you run -Dtests.verbose, QueryUtils now advertises when it's opening an IndexWriter to make an empty index
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1588921 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/search/QueryUtils.java
+++ lucene/test-framework/src/java/org/apache/lucene/search/QueryUtils.java
@@ -1112,6 +1112,23 @@ name|RAMDirectory
 argument_list|()
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|LuceneTestCase
+operator|.
+name|VERBOSE
+condition|)
+block|{
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"NOTE: QueryUtils: now create empty index"
+argument_list|)
+expr_stmt|;
+block|}
 name|IndexWriter
 name|w
 init|=
@@ -1170,6 +1187,23 @@ operator|.
 name|shutdown
 argument_list|()
 expr_stmt|;
+if|if
+condition|(
+name|LuceneTestCase
+operator|.
+name|VERBOSE
+condition|)
+block|{
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"NOTE: QueryUtils: done create empty index"
+argument_list|)
+expr_stmt|;
+block|}
 name|DirectoryReader
 name|reader
 init|=
