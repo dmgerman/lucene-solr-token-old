1c505f46708f983a3487d0d30c6416e497986748
Ryan Ernst <rjernst@apache.org>
2014-04-22 19:45:58 +0000

    SOLR-5999: Add checkIntegrityAtMerge support to solrconfig.xml
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1589272 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/SolrIndexConfig.java
+++ solr/core/src/java/org/apache/solr/update/SolrIndexConfig.java
@@ -400,6 +400,12 @@ name|LOCK_TYPE_NONE
 init|=
 literal|"none"
 decl_stmt|;
+DECL|field|checkIntegrityAtMerge
+specifier|public
+specifier|final
+name|boolean
+name|checkIntegrityAtMerge
+decl_stmt|;
 comment|/**    * Internal constructor for setting defaults based on Lucene Version    */
 annotation|@
 name|SuppressWarnings
@@ -480,6 +486,10 @@ expr_stmt|;
 name|mergedSegmentWarmerInfo
 operator|=
 literal|null
+expr_stmt|;
+name|checkIntegrityAtMerge
+operator|=
+literal|false
 expr_stmt|;
 block|}
 comment|/**    * Constructs a SolrIndexConfig which parses the Lucene related config params in solrconfig.xml    * @param solrConfig the overall SolrConfig object    * @param prefix the XPath prefix for which section to parse (mandatory)    * @param def a SolrIndexConfig instance to pick default values from (optional)    */
@@ -953,6 +963,21 @@ literal|"Supplying a mergedSegmentWarmer will do nothing since nrtMode is false"
 argument_list|)
 throw|;
 block|}
+name|checkIntegrityAtMerge
+operator|=
+name|solrConfig
+operator|.
+name|getBool
+argument_list|(
+name|prefix
+operator|+
+literal|"/checkIntegrityAtMerge"
+argument_list|,
+name|def
+operator|.
+name|checkIntegrityAtMerge
+argument_list|)
+expr_stmt|;
 block|}
 comment|/*    * Assert that assertCondition is true.    * If not, prints reason as log warning.    * If failCondition is true, then throw exception instead of warning     */
 DECL|method|assertWarnOrFail
@@ -1238,6 +1263,13 @@ name|warmer
 argument_list|)
 expr_stmt|;
 block|}
+name|iwc
+operator|.
+name|setCheckIntegrityAtMerge
+argument_list|(
+name|checkIntegrityAtMerge
+argument_list|)
+expr_stmt|;
 return|return
 name|iwc
 return|;
