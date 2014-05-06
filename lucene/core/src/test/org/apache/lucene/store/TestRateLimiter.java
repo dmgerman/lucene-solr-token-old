cf38f1386c13134eb0be88cb5b14fb09b00a5e19
Michael McCandless <mikemccand@apache.org>
2014-05-06 14:26:57 +0000

    fix test bug causing false failure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1592756 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/store/TestRateLimiter.java
+++ lucene/core/src/test/org/apache/lucene/store/TestRateLimiter.java
@@ -510,6 +510,8 @@ name|actualMBPerSec
 operator|/
 name|targetMBPerSec
 decl_stmt|;
+comment|// Only enforce that it wasn't too fast; if machine is bogged down (can't schedule threads / sleep properly) then it may falsely be too slow:
+comment|//assertTrue("targetMBPerSec=" + targetMBPerSec + " actualMBPerSec=" + actualMBPerSec, ratio>= 0.9&& ratio<= 1.1);
 name|assertTrue
 argument_list|(
 literal|"targetMBPerSec="
@@ -520,10 +522,6 @@ literal|" actualMBPerSec="
 operator|+
 name|actualMBPerSec
 argument_list|,
-name|ratio
-operator|>=
-literal|0.9
-operator|&&
 name|ratio
 operator|<=
 literal|1.1
