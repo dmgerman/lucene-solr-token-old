d525dbe46892a78dbae25863e250309eaf3a3046
Michael McCandless <mikemccand@apache.org>
2014-05-09 10:22:32 +0000

    include state number in Automaton.toDot output
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1593507 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/automaton/Automaton.java
+++ lucene/core/src/java/org/apache/lucene/util/automaton/Automaton.java
@@ -2208,7 +2208,13 @@ name|b
 operator|.
 name|append
 argument_list|(
-literal|" [shape=doublecircle,label=\"\"];\n"
+literal|" [shape=doublecircle,label=\""
+operator|+
+name|s
+operator|.
+name|number
+operator|+
+literal|"\"];\n"
 argument_list|)
 expr_stmt|;
 else|else
@@ -2216,7 +2222,13 @@ name|b
 operator|.
 name|append
 argument_list|(
-literal|" [shape=circle,label=\"\"];\n"
+literal|" [shape=circle,label=\" "
+operator|+
+name|s
+operator|.
+name|number
+operator|+
+literal|"\"];\n"
 argument_list|)
 expr_stmt|;
 if|if
