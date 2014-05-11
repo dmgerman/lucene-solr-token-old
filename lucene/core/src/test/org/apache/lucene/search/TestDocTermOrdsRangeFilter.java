8b54aef7aa0b10c4dfab02602db973f954b45f2c
Robert Muir <rmuir@apache.org>
2014-05-11 20:17:12 +0000

    LUCENE-5666: move out more uninverting
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene5666@1593850 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/search/TestDocTermOrdsRangeFilter.java
+++ lucene/core/src/test/org/apache/lucene/search/TestDocTermOrdsRangeFilter.java
@@ -203,6 +203,21 @@ name|lucene
 operator|.
 name|util
 operator|.
+name|LuceneTestCase
+operator|.
+name|SuppressCodecs
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
 name|TestUtil
 import|;
 end_import
@@ -223,6 +238,18 @@ begin_comment
 comment|/**  * Tests the DocTermOrdsRangeFilter  */
 end_comment
 begin_class
+annotation|@
+name|SuppressCodecs
+argument_list|(
+block|{
+literal|"Lucene40"
+block|,
+literal|"Lucene41"
+block|,
+literal|"Lucene42"
+block|}
+argument_list|)
+comment|// needs SORTED_SET
 DECL|class|TestDocTermOrdsRangeFilter
 specifier|public
 class|class
@@ -269,14 +296,6 @@ name|super
 operator|.
 name|setUp
 argument_list|()
-expr_stmt|;
-name|assumeTrue
-argument_list|(
-literal|"requires codec support for SORTED_SET"
-argument_list|,
-name|defaultCodecSupportsSortedSet
-argument_list|()
-argument_list|)
 expr_stmt|;
 name|dir
 operator|=
