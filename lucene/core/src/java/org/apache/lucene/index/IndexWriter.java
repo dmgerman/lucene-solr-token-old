04583e5c200a25f4767002583eb546b05dc152a0
Michael McCandless <mikemccand@apache.org>
2014-05-16 15:41:35 +0000

    only throw IOExc if merge really hit an exception
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1595239 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
@@ -4983,6 +4983,21 @@ operator|=
 literal|true
 expr_stmt|;
 block|}
+name|Throwable
+name|t
+init|=
+name|merge
+operator|.
+name|getException
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|t
+operator|!=
+literal|null
+condition|)
+block|{
 throw|throw
 operator|new
 name|IOException
@@ -5002,6 +5017,7 @@ name|getException
 argument_list|()
 argument_list|)
 throw|;
+block|}
 block|}
 comment|// If any of our merges are still running, wait:
 if|if
