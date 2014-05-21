ca5e5e02e3824f57a850f07aaf7d006bec9b5455
Timothy Potter <thelabdude@apache.org>
2014-05-21 18:05:53 +0000

    SOLR-5495: Print cluster state in assertion failure messages if a leader cannot be found to determine root cause of HttpPartitionTest failures in Jenkins.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1596636 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/HttpPartitionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/HttpPartitionTest.java
@@ -370,7 +370,7 @@ specifier|final
 name|long
 name|sleepMsBeforeHealPartition
 init|=
-literal|1000L
+literal|2000L
 decl_stmt|;
 DECL|field|maxWaitSecsToSeeAllActive
 specifier|private
@@ -1025,12 +1025,7 @@ argument_list|()
 operator|+
 literal|"; clusterState: "
 operator|+
-name|cloudClient
-operator|.
-name|getZkStateReader
-argument_list|()
-operator|.
-name|getClusterState
+name|printClusterStateInfo
 argument_list|()
 argument_list|,
 name|notLeaders
@@ -1217,12 +1212,7 @@ argument_list|()
 operator|+
 literal|"; clusterState: "
 operator|+
-name|cloudClient
-operator|.
-name|getZkStateReader
-argument_list|()
-operator|.
-name|getClusterState
+name|printClusterStateInfo
 argument_list|()
 argument_list|,
 name|notLeaders
@@ -1370,6 +1360,11 @@ argument_list|(
 literal|"Could not find leader for shard1 of "
 operator|+
 name|testCollectionName
+operator|+
+literal|"; clusterState: "
+operator|+
+name|printClusterStateInfo
+argument_list|()
 argument_list|,
 name|leader
 argument_list|)
@@ -1498,9 +1493,10 @@ name|Thread
 operator|.
 name|sleep
 argument_list|(
-name|sleepMsBeforeHealPartition
+literal|10000
 argument_list|)
 expr_stmt|;
+comment|// give chance for new leader to be elected.
 name|Replica
 name|newLeader
 init|=
@@ -1520,7 +1516,10 @@ argument_list|)
 decl_stmt|;
 name|assertNotNull
 argument_list|(
-literal|"No new leader was elected after 60 seconds"
+literal|"No new leader was elected after 60 seconds; clusterState: "
+operator|+
+name|printClusterStateInfo
+argument_list|()
 argument_list|,
 name|newLeader
 argument_list|)
@@ -1533,12 +1532,7 @@ name|shouldNotBeNewLeaderNode
 operator|+
 literal|" to NOT be the new leader b/c it was out-of-sync with the old leader! ClusterState: "
 operator|+
-name|cloudClient
-operator|.
-name|getZkStateReader
-argument_list|()
-operator|.
-name|getClusterState
+name|printClusterStateInfo
 argument_list|()
 argument_list|,
 operator|!
@@ -1600,6 +1594,11 @@ operator|+
 literal|"; "
 operator|+
 name|activeReps
+operator|+
+literal|"; clusterState: "
+operator|+
+name|printClusterStateInfo
+argument_list|()
 argument_list|,
 name|activeReps
 operator|.
@@ -1625,6 +1624,39 @@ argument_list|,
 literal|6
 argument_list|)
 expr_stmt|;
+block|}
+DECL|method|printClusterStateInfo
+specifier|protected
+name|String
+name|printClusterStateInfo
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|cloudClient
+operator|.
+name|getZkStateReader
+argument_list|()
+operator|.
+name|updateClusterState
+argument_list|(
+literal|true
+argument_list|)
+expr_stmt|;
+return|return
+name|String
+operator|.
+name|valueOf
+argument_list|(
+name|cloudClient
+operator|.
+name|getZkStateReader
+argument_list|()
+operator|.
+name|getClusterState
+argument_list|()
+argument_list|)
+return|;
 block|}
 DECL|method|getActiveOrRecoveringReplicas
 specifier|protected
@@ -2481,7 +2513,8 @@ name|maxWaitMs
 operator|+
 literal|" ms! ClusterState: "
 operator|+
-name|cs
+name|printClusterStateInfo
+argument_list|()
 argument_list|)
 expr_stmt|;
 if|if
@@ -2495,7 +2528,8 @@ name|fail
 argument_list|(
 literal|"Didn't isolate any replicas that are not the leader! ClusterState: "
 operator|+
-name|cs
+name|printClusterStateInfo
+argument_list|()
 argument_list|)
 expr_stmt|;
 name|long
