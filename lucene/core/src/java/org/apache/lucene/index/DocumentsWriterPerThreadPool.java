95bd6d1176057728e15eacfcdb3beb9d90bd8bd5
Michael McCandless <mikemccand@apache.org>
2014-08-11 09:04:40 +0000

    throw ACE not NPE
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1617228 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/index/DocumentsWriterPerThreadPool.java
+++ lucene/core/src/java/org/apache/lucene/index/DocumentsWriterPerThreadPool.java
@@ -35,6 +35,19 @@ name|apache
 operator|.
 name|lucene
 operator|.
+name|store
+operator|.
+name|AlreadyClosedException
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
 name|util
 operator|.
 name|ThreadInterruptedException
@@ -434,9 +447,13 @@ assert|assert
 name|assertUnreleasedThreadStatesInactive
 argument_list|()
 assert|;
-return|return
-literal|null
-return|;
+throw|throw
+operator|new
+name|AlreadyClosedException
+argument_list|(
+literal|"this IndexWriter is closed"
+argument_list|)
+throw|;
 block|}
 finally|finally
 block|{
