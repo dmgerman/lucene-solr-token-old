6f8bdb9d941dcf191a17229dac7064f71d979f38
James Dyer <jdyer@apache.org>
2014-08-11 14:13:56 +0000

    SOLR-6362: fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1617289 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/dataimporthandler/src/test/org/apache/solr/handler/dataimport/TestSqlEntityProcessorDelta.java
+++ solr/contrib/dataimporthandler/src/test/org/apache/solr/handler/dataimport/TestSqlEntityProcessorDelta.java
@@ -568,7 +568,7 @@ argument_list|)
 operator|+
 literal|"']"
 argument_list|,
-literal|"substring(//doc/str[@name='COUNTRY_NAME_s'], 1, 8)='MODIFIED'"
+literal|"substring((//doc/str[@name='COUNTRY_NAME_s'])[1], 1, 8)='MODIFIED'"
 argument_list|)
 expr_stmt|;
 block|}
