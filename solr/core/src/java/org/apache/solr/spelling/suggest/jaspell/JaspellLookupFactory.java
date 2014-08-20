2f5b9f6fc133ca798d22b694f81daadf90cb4768
Chris M. Hostetter <hossman@apache.org>
2014-08-20 17:31:08 +0000

    SOLR-6178: backout deprecation until we have a diff default
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1619172 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/spelling/suggest/jaspell/JaspellLookupFactory.java
+++ solr/core/src/java/org/apache/solr/spelling/suggest/jaspell/JaspellLookupFactory.java
@@ -112,11 +112,9 @@ name|LoggerFactory
 import|;
 end_import
 begin_comment
-comment|/**  * Factory for {@link JaspellLookup}  * @deprecated Migrate to one of the newer suggesters which are much more RAM efficient.  */
+comment|/**  * Factory for {@link JaspellLookup}  *<b>Note:</b> This Suggester is not very RAM efficient.  */
 end_comment
 begin_class
-annotation|@
-name|Deprecated
 DECL|class|JaspellLookupFactory
 specifier|public
 class|class
