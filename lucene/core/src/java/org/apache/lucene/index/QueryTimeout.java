37c172a8f67f25262a341a842290daa778ff139c
Anshum Gupta <anshum@apache.org>
2014-09-25 19:42:07 +0000

    SOLR-5986: Add an ExitableDirectoryReader in Lucene and use that to support exiting of long running queries in Solr.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1627622 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lucene/core/src/java/org/apache/lucene/index/QueryTimeout.java
@@ -0,0 +1,36 @@
+begin_unit
+begin_package
+DECL|package|org.apache.lucene.index
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|index
+package|;
+end_package
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+begin_comment
+comment|/**  * Base for query timeout implementations, which will provide a {@code shouldExit()} method,  * used with {@link ExitableDirectoryReader}.  */
+end_comment
+begin_interface
+DECL|interface|QueryTimeout
+specifier|public
+interface|interface
+name|QueryTimeout
+block|{
+comment|/**    * Called from {@link ExitableDirectoryReader.ExitableTermsEnum#next()}     * to determine whether to stop processing a query.    */
+DECL|method|shouldExit
+specifier|public
+specifier|abstract
+name|boolean
+name|shouldExit
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+end_unit
