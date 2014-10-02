64d59f59de8483709cd5c060bfefdf94fa2f11b8
Shalin Shekhar Mangar <shalin@apache.org>
2014-10-02 10:34:46 +0000

    SOLR-6564: Fix failing SolrInfoMBeanTest
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1628927 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/search/DelayingSearchComponent.java
+++ solr/core/src/test/org/apache/solr/search/DelayingSearchComponent.java
@@ -22,6 +22,19 @@ name|apache
 operator|.
 name|solr
 operator|.
+name|core
+operator|.
+name|SolrInfoMBean
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
 name|handler
 operator|.
 name|component
@@ -130,7 +143,7 @@ name|getDescription
 parameter_list|()
 block|{
 return|return
-literal|null
+literal|"SearchComponent used to add delay to each request"
 return|;
 block|}
 block|}
