27a89ff2f512ec73fcdcef0d43b44a0c47f6f259
Adrien Grand <jpountz@apache.org>
2014-10-16 14:01:53 +0000

    LUCENE-6009: Remove redundant == NO_MORE_DOCS checks.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1632314 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/RoaringDocIdSet.java
+++ lucene/core/src/java/org/apache/lucene/util/RoaringDocIdSet.java
@@ -1180,17 +1180,6 @@ parameter_list|()
 throws|throws
 name|IOException
 block|{
-if|if
-condition|(
-name|doc
-operator|==
-name|NO_MORE_DOCS
-condition|)
-block|{
-return|return
-name|NO_MORE_DOCS
-return|;
-block|}
 specifier|final
 name|int
 name|subNext
