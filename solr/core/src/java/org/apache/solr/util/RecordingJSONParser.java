710cb894469d9faa8640dd0db6568c243e44b6e5
Noble Paul <noble@apache.org>
2014-10-21 15:12:55 +0000

    SOLR-6633 changed package
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1633394 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/util/RecordingJSONParser.java
+++ solr/core/src/java/org/apache/solr/util/RecordingJSONParser.java
@@ -1,6 +1,6 @@
 begin_unit
 begin_package
-DECL|package|org.apache.solr.util.xslt
+DECL|package|org.apache.solr.util
 package|package
 name|org
 operator|.
@@ -9,8 +9,6 @@ operator|.
 name|solr
 operator|.
 name|util
-operator|.
-name|xslt
 package|;
 end_package
 begin_comment
