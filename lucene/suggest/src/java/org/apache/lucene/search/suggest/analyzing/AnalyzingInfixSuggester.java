ab1a465d35d1dfd0fd02a644f633b60ac060e199
Shai Erera <shaie@apache.org>
2014-12-16 21:21:19 +0000

    Fix typo
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1646083 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/suggest/src/java/org/apache/lucene/search/suggest/analyzing/AnalyzingInfixSuggester.java
+++ lucene/suggest/src/java/org/apache/lucene/search/suggest/analyzing/AnalyzingInfixSuggester.java
@@ -1622,7 +1622,7 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
-comment|/** Updates a previous suggestion, matching the exact same    *  text as before.  Use this to change the weight or    *  payload of an already added suggstion.  If you know    *  this text is not already present you can use {@link    *  #add} instead.  After adding or updating a batch of    *  new suggestions, you must call {@link #refresh} in the    *  end in order to see the suggestions in {@link #lookup} */
+comment|/** Updates a previous suggestion, matching the exact same    *  text as before.  Use this to change the weight or    *  payload of an already added suggestion.  If you know    *  this text is not already present you can use {@link    *  #add} instead.  After adding or updating a batch of    *  new suggestions, you must call {@link #refresh} in the    *  end in order to see the suggestions in {@link #lookup} */
 DECL|method|update
 specifier|public
 name|void
