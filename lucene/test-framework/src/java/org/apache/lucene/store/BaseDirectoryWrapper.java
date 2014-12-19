4d9795db5ca16e99e49c907100863f4d0b052d7b
Robert Muir <rmuir@apache.org>
2014-12-19 22:31:01 +0000

    LUCENE-6124: fix double-close bug in BaseDirectoryWrapper
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1646897 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/store/BaseDirectoryWrapper.java
+++ lucene/test-framework/src/java/org/apache/lucene/store/BaseDirectoryWrapper.java
@@ -115,6 +115,11 @@ parameter_list|()
 throws|throws
 name|IOException
 block|{
+if|if
+condition|(
+name|isOpen
+condition|)
+block|{
 name|isOpen
 operator|=
 literal|false
@@ -140,6 +145,7 @@ argument_list|,
 name|crossCheckTermVectorsOnClose
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|super
 operator|.
