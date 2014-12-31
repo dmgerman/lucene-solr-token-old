652978b94d990bfc90447c3f8044e02dc7632f9a
Uwe Schindler <uschindler@apache.org>
2014-12-31 15:52:01 +0000

    LUCENE-6152: Fix double close bug in OutputStreamIndexOutput
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1648724 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/store/OutputStreamIndexOutput.java
+++ lucene/core/src/java/org/apache/lucene/store/OutputStreamIndexOutput.java
@@ -97,6 +97,13 @@ name|bytesWritten
 init|=
 literal|0L
 decl_stmt|;
+DECL|field|flushedOnClose
+specifier|private
+name|boolean
+name|flushedOnClose
+init|=
+literal|false
+decl_stmt|;
 comment|/**    * Creates a new {@link OutputStreamIndexOutput} with the given buffer size.     * @param bufferSize the buffer size in bytes used to buffer writes internally.    * @throws IllegalArgumentException if the given buffer size is less or equal to<tt>0</tt>    */
 DECL|method|OutputStreamIndexOutput
 specifier|public
@@ -219,13 +226,27 @@ init|)
 block|{
 comment|// We want to make sure that os.flush() was running before close:
 comment|// BufferedOutputStream may ignore IOExceptions while flushing on close().
-comment|// TODO: this is no longer an issue in Java 8:
-comment|// http://hg.openjdk.java.net/jdk8/tl/jdk/rev/759aa847dcaf
+comment|// We keep this also in Java 8, although it claims to be fixed there,
+comment|// because there are more bugs around this! See:
+comment|// # https://bugs.openjdk.java.net/browse/JDK-7015589
+comment|// # https://bugs.openjdk.java.net/browse/JDK-8054565
+if|if
+condition|(
+operator|!
+name|flushedOnClose
+condition|)
+block|{
+name|flushedOnClose
+operator|=
+literal|true
+expr_stmt|;
+comment|// set this BEFORE calling flush!
 name|o
 operator|.
 name|flush
 argument_list|()
 expr_stmt|;
+block|}
 block|}
 block|}
 annotation|@
