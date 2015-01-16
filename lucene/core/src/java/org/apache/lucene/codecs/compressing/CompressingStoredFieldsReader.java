b1d0c9ba7b3c60f19c4b9b10c6090b3639a5985d
Robert Muir <rmuir@apache.org>
2015-01-16 05:31:54 +0000

    LUCENE-6183: be prepared for future packedints version changes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1652342 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/codecs/compressing/CompressingStoredFieldsReader.java
+++ lucene/core/src/java/org/apache/lucene/codecs/compressing/CompressingStoredFieldsReader.java
@@ -3772,6 +3772,15 @@ return|return
 name|numDirtyChunks
 return|;
 block|}
+DECL|method|getPackedIntsVersion
+name|int
+name|getPackedIntsVersion
+parameter_list|()
+block|{
+return|return
+name|packedIntsVersion
+return|;
+block|}
 annotation|@
 name|Override
 DECL|method|ramBytesUsed
--- lucene/core/src/java/org/apache/lucene/codecs/compressing/CompressingStoredFieldsWriter.java
+++ lucene/core/src/java/org/apache/lucene/codecs/compressing/CompressingStoredFieldsWriter.java
@@ -2874,6 +2874,15 @@ argument_list|()
 operator|==
 name|chunkSize
 operator|&&
+name|matchingFieldsReader
+operator|.
+name|getPackedIntsVersion
+argument_list|()
+operator|==
+name|PackedInts
+operator|.
+name|VERSION_CURRENT
+operator|&&
 name|liveDocs
 operator|==
 literal|null
