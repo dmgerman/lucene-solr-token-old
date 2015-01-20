f7247d8118e41a1f36030beeb8077db23d76ea84
Erik Hatcher <ehatcher@apache.org>
2015-01-20 20:52:59 +0000

    Fix wrong StringUtils import (merged from r1653365)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1653368 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/velocity/src/java/org/apache/solr/response/VelocityResponseWriter.java
+++ solr/contrib/velocity/src/java/org/apache/solr/response/VelocityResponseWriter.java
@@ -121,9 +121,9 @@ name|org
 operator|.
 name|apache
 operator|.
-name|hadoop
+name|commons
 operator|.
-name|util
+name|lang
 operator|.
 name|StringUtils
 import|;
@@ -1687,9 +1687,9 @@ name|StringUtils
 operator|.
 name|join
 argument_list|(
-literal|","
-argument_list|,
 name|loaders
+argument_list|,
+literal|','
 argument_list|)
 argument_list|)
 expr_stmt|;
