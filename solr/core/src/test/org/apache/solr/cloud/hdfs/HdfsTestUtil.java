5fd00707ce0ac43defa8908406db0f2024f2b27b
Mark Robert Miller <markrmiller@apache.org>
2015-02-06 21:38:57 +0000

    SOLR-6920, SOLR-6640: A replicated index can end up corrupted when small files end up with the same file name and size.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1657969 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsTestUtil.java
+++ solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsTestUtil.java
@@ -821,6 +821,13 @@ literal|"/"
 argument_list|,
 literal|"_"
 argument_list|)
+operator|.
+name|replaceAll
+argument_list|(
+literal|" "
+argument_list|,
+literal|"_"
+argument_list|)
 decl_stmt|;
 return|return
 name|dir
