c4845dafe3d8676826533856d8c5a83b83647093
Adrien Grand <jpountz@apache.org>
2015-02-09 18:47:17 +0000

    LUCENE-6224: cut over more package.htmls
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1658525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ solr/contrib/analytics/src/java/org/apache/solr/analytics/statistics/package-info.java
@@ -0,0 +1,22 @@
+begin_unit
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+begin_comment
+comment|/**   * Statistics collectors reduce a list of Objects to a single value. Most implementations reduce a list to a statistic on that list.  */
+end_comment
+begin_package
+DECL|package|org.apache.solr.analytics.statistics
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
+name|analytics
+operator|.
+name|statistics
+package|;
+end_package
+end_unit
