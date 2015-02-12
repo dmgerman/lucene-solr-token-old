efd3372e721c9353d85b20f3bf06abeaf1d8a8bb
Shalin Shekhar Mangar <shalin@apache.org>
2015-02-12 10:30:57 +0000

    SOLR-6214: Fix mistake in failure message
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1659203 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/handler/TestReplicationHandlerBackup.java
+++ solr/core/src/test/org/apache/solr/handler/TestReplicationHandlerBackup.java
@@ -1504,7 +1504,7 @@ literal|"Backup should have been cleaned up because "
 operator|+
 name|backupKeepParamName
 operator|+
-literal|" was set to 2."
+literal|" was set to 1."
 argument_list|)
 expr_stmt|;
 block|}
