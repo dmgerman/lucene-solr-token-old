e602710fab1295877f0411b89112b8e1a837b6f7
Adrien Grand <jpountz@apache.org>
2015-02-13 16:45:06 +0000

    LUCENE-6198: Two-phase execution for phrase queries and conjunctions.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1659599 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/Scorer.java
+++ lucene/core/src/java/org/apache/lucene/search/Scorer.java
@@ -175,6 +175,17 @@ name|relationship
 expr_stmt|;
 block|}
 block|}
+comment|/**    * Optional method: Return a {@link TwoPhaseDocIdSetIterator} view of this    * {@link Scorer}. A return value of {@code null} indicates that    * two-phase iteration is not supported.    *    * Note that the returned {@link TwoPhaseDocIdSetIterator}'s    * {@link TwoPhaseDocIdSetIterator#approximation() approximation} must    * advance synchronously with this iterator: advancing the approximation must    * advance this iterator and vice-versa.    *    * Implementing this method is typically useful on {@link Scorer}s    * that have a high per-document overhead in order to confirm matches.    *    * The default implementation returns {@code null}.    */
+DECL|method|asTwoPhaseIterator
+specifier|public
+name|TwoPhaseDocIdSetIterator
+name|asTwoPhaseIterator
+parameter_list|()
+block|{
+return|return
+literal|null
+return|;
+block|}
 block|}
 end_class
 end_unit
