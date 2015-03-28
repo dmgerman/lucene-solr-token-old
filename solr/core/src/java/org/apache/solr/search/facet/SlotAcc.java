399c45863ea36d70e2b31b54c9f38f08a7353374
Yonik Seeley <yonik@apache.org>
2015-03-28 00:02:40 +0000

    SOLR-7214: optimize multi-valued counts-only case
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1669712 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/facet/SlotAcc.java
+++ solr/core/src/java/org/apache/solr/search/facet/SlotAcc.java
@@ -1469,6 +1469,17 @@ name|slot
 index|]
 return|;
 block|}
+comment|// internal and expert
+DECL|method|getCountArray
+name|int
+index|[]
+name|getCountArray
+parameter_list|()
+block|{
+return|return
+name|result
+return|;
+block|}
 annotation|@
 name|Override
 DECL|method|reset
