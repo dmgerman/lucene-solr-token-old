8467594baee997a4abd96b9a5c5739166d73e3e0
Gregory Chanan <gchanan@apache.org>
2015-03-30 19:07:41 +0000

    SOLR-7272: Use OverseerCollectionProcessor.ROUTER and DocCollection.DOC_ROUTER consistently
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1670178 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/CustomCollectionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CustomCollectionTest.java
@@ -533,7 +533,7 @@ name|cloud
 operator|.
 name|OverseerCollectionProcessor
 operator|.
-name|ROUTER
+name|SHARDS_PROP
 import|;
 end_import
 begin_import
@@ -544,11 +544,13 @@ name|apache
 operator|.
 name|solr
 operator|.
+name|common
+operator|.
 name|cloud
 operator|.
-name|OverseerCollectionProcessor
+name|DocCollection
 operator|.
-name|SHARDS_PROP
+name|DOC_ROUTER
 import|;
 end_import
 begin_import
@@ -1306,7 +1308,7 @@ name|coll
 operator|.
 name|get
 argument_list|(
-name|ROUTER
+name|DOC_ROUTER
 argument_list|)
 operator|)
 operator|.
