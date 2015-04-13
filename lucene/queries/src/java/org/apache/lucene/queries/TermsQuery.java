4faa0513fe4d8a5b192442e3f7cd6d12cc031292
Adrien Grand <jpountz@apache.org>
2015-04-13 06:40:38 +0000

    LUCENE-6415: Make TermsQuery.extractTerms a no-op instead of throwing an UOE.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1673118 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/queries/src/java/org/apache/lucene/queries/TermsQuery.java
+++ lucene/queries/src/java/org/apache/lucene/queries/TermsQuery.java
@@ -79,6 +79,15 @@ import|;
 end_import
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Set
+import|;
+end_import
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -377,7 +386,7 @@ name|ToStringUtils
 import|;
 end_import
 begin_comment
-comment|/**  * Specialization for a disjunction over many terms that behaves like a  * {@link ConstantScoreQuery} over a {@link BooleanQuery} containing only  * {@link org.apache.lucene.search.BooleanClause.Occur#SHOULD} clauses.  * This query creates a bit set and sets bits that matches any of the wrapped  * terms. While this might help performance when there are many terms, it would  * be slower than a {@link BooleanQuery} when there are few terms to match.  */
+comment|/**  * Specialization for a disjunction over many terms that behaves like a  * {@link ConstantScoreQuery} over a {@link BooleanQuery} containing only  * {@link org.apache.lucene.search.BooleanClause.Occur#SHOULD} clauses.  *<p>For instance in the following example, both @{code q1} and {@code q2}  * would yield the same scores:  *<pre class="prettyprint">  * Query q1 = new TermsQuery(new Term("field", "foo"), new Term("field", "bar"));  *   * BooleanQuery bq = new BooleanQuery();  * bq.add(new TermQuery(new Term("field", "foo")), Occur.SHOULD);  * bq.add(new TermQuery(new Term("field", "bar")), Occur.SHOULD);  * Query q2 = new ConstantScoreQuery(bq);  *</pre>  *<p>This query creates a bit set and sets bits that match any of the  * wrapped terms. While this might help performance when there are many terms,  * it would be slower than a {@link BooleanQuery} when there are few terms to  * match.  *<p>NOTE: This query produces scores that are equal to its boost  */
 end_comment
 begin_class
 DECL|class|TermsQuery
@@ -1374,6 +1383,25 @@ operator|.
 name|emptyList
 argument_list|()
 return|;
+block|}
+annotation|@
+name|Override
+DECL|method|extractTerms
+specifier|public
+name|void
+name|extractTerms
+parameter_list|(
+name|Set
+argument_list|<
+name|Term
+argument_list|>
+name|terms
+parameter_list|)
+block|{
+comment|// no-op
+comment|// This query is for abuse cases when the number of terms is too high to
+comment|// run efficiently as a BooleanQuery. So likewise we hide its terms in
+comment|// order to protect highlighters
 block|}
 DECL|class|TermsAndField
 specifier|private
