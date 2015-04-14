bc48f6ac385f0f5c1681a1a051aa33120ccb09b6
Shalin Shekhar Mangar <shalin@apache.org>
2015-04-14 05:12:52 +0000

    SOLR-7372: Enable maxRamMB to be configured via the Config APIs on filterCache and queryResultCache
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1673358 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/ConfigOverlay.java
+++ solr/core/src/java/org/apache/solr/core/ConfigOverlay.java
@@ -1260,6 +1260,8 @@ literal|"      initialSize:20,"
 operator|+
 literal|"      autowarmCount:20,"
 operator|+
+literal|"      maxRamMB:20,"
+operator|+
 literal|"      regenerator:0},"
 operator|+
 literal|"    queryResultCache:{"
@@ -1271,6 +1273,8 @@ operator|+
 literal|"      initialSize:20,"
 operator|+
 literal|"      autowarmCount:20,"
+operator|+
+literal|"      maxRamMB:20,"
 operator|+
 literal|"      regenerator:0},"
 operator|+
