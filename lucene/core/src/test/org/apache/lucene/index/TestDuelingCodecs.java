3b14e94bd5070a64268f438b162bdf9045822a4d
Robert Muir <rmuir@apache.org>
2015-04-19 22:47:00 +0000

    speedups for the slowest tests
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1674704 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestDuelingCodecs.java
+++ lucene/core/src/test/org/apache/lucene/index/TestDuelingCodecs.java
@@ -167,6 +167,21 @@ name|lucene
 operator|.
 name|util
 operator|.
+name|LuceneTestCase
+operator|.
+name|Slow
+import|;
+end_import
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
 name|TestUtil
 import|;
 end_import
@@ -192,6 +207,8 @@ begin_comment
 comment|/**  * Compares one codec against another  */
 end_comment
 begin_class
+annotation|@
+name|Slow
 DECL|class|TestDuelingCodecs
 specifier|public
 class|class
