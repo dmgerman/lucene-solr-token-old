179a20663c1d2f611b580cc4f72d8cdc57c97a31
Michael McCandless <mikemccand@apache.org>
2015-04-20 18:41:22 +0000

    LUCENE-6444: add missing 4.10.4 unsupported back-compat indices
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1674961 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -1719,7 +1719,11 @@ block|,
 literal|"4.10.3-cfs"
 block|,
 literal|"4.10.3-nocfs"
-block|,   }
+block|,
+literal|"4.10.4-cfs"
+block|,
+literal|"4.10.4-nocfs"
+block|}
 decl_stmt|;
 DECL|field|oldSingleSegmentNames
 specifier|final
