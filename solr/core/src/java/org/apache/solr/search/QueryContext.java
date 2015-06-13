0f3e8e970ae7789d35802db17e6d3ec23d662eda
Yonik Seeley <yonik@apache.org>
2015-06-13 22:32:48 +0000

    SOLR-7676: nested docs facet domain switching
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1685340 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/QueryContext.java
+++ solr/core/src/java/org/apache/solr/search/QueryContext.java
@@ -94,7 +94,7 @@ name|SolrRequestInfo
 import|;
 end_import
 begin_comment
-comment|/**  * Bridge between old style context and a real class  * @lucene.experimental  */
+comment|/*  * Bridge between old style context and a real class.  * This is currently slightly more heavy weight than necessary because of the need to inherit from IdentityHashMap rather than  * instantiate it on demand (and the need to put "searcher" in the map)  * @lucene.experimental  */
 end_comment
 begin_class
 DECL|class|QueryContext
@@ -189,7 +189,7 @@ argument_list|,
 name|searcher
 argument_list|)
 expr_stmt|;
-comment|// see ValueSource.newContext()
+comment|// see ValueSource.newContext()  // TODO: move check to "get"?
 block|}
 DECL|method|searcher
 specifier|public
