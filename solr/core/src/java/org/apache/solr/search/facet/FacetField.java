dc73a70da8b59354c69574416a3aee04f40ccb5d
Yonik Seeley <yonik@apache.org>
2015-07-12 22:34:53 +0000

    SOLR-7781: change FacetField BytesRef shallow clone to deep clone
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1690541 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/facet/FacetField.java
+++ solr/core/src/java/org/apache/solr/search/facet/FacetField.java
@@ -2680,6 +2680,7 @@ name|getFieldCacheCounts
 argument_list|()
 expr_stmt|;
 block|}
+comment|/** this BytesRef may be shared across calls and should be deep-cloned if necessary */
 DECL|method|lookupOrd
 specifier|abstract
 specifier|protected
@@ -3579,10 +3580,12 @@ operator|.
 name|getName
 argument_list|()
 argument_list|,
-name|br
+name|BytesRef
 operator|.
-name|clone
-argument_list|()
+name|deepCopyOf
+argument_list|(
+name|br
+argument_list|)
 argument_list|)
 argument_list|)
 else|:
