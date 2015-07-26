e623460c52b48dcfff21a9e173460e87a9f93f99
Shalin Shekhar Mangar <shalin@apache.org>
2015-07-26 05:21:48 +0000

    Fix typos
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1692688 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/HttpPartitionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/HttpPartitionTest.java
@@ -654,7 +654,7 @@ name|log
 operator|.
 name|info
 argument_list|(
-literal|"HttpParitionTest succeeded ... shutting down now!"
+literal|"HttpPartitionTest succeeded ... shutting down now!"
 argument_list|)
 expr_stmt|;
 block|}
