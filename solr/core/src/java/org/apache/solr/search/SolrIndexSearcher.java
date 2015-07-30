dbfdbf7b06ea97ae81c4ffe000773980185b5b04
Noble Paul <noble@apache.org>
2015-07-30 14:28:20 +0000

    SOLR-6625: Enable registering interceptors for the calls made using HttpClient and make the   request object available at the interceptor context
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1693432 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
+++ solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
@@ -11343,6 +11343,11 @@ argument_list|()
 decl_stmt|;
 name|SolrRequestInfo
 operator|.
+name|clearRequestInfo
+argument_list|()
+expr_stmt|;
+name|SolrRequestInfo
+operator|.
 name|setRequestInfo
 argument_list|(
 operator|new
