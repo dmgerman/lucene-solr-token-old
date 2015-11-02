adaf860d9bc9ec4f7a5ce52f3fcdf27a98f0ce50
Yonik Seeley <yonik@apache.org>
2015-11-02 18:41:02 +0000

    SOLR-8203: use shutdownNow on updateExecutor
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1712111 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/UpdateShardHandler.java
+++ solr/core/src/java/org/apache/solr/update/UpdateShardHandler.java
@@ -553,7 +553,7 @@ try|try
 block|{
 name|ExecutorUtil
 operator|.
-name|shutdownAndAwaitTermination
+name|shutdownWithInterruptAndAwaitTermination
 argument_list|(
 name|updateExecutor
 argument_list|)
