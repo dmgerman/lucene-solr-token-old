d2616151a709227307ab7c848cc1ef622443a6d6
Mark Robert Miller <markrmiller@apache.org>
2016-01-12 17:17:47 +0000

    SOLR-7064: ConcurrentUpdateSolrClient should only close it's httpclient if it created it.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1724288 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/ConcurrentUpdateSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/ConcurrentUpdateSolrClient.java
@@ -641,6 +641,11 @@ specifier|final
 name|boolean
 name|streamDeletes
 decl_stmt|;
+DECL|field|internalHttpClient
+specifier|private
+name|boolean
+name|internalHttpClient
+decl_stmt|;
 comment|/**    * Uses an internally managed HttpClient instance.    *     * @param solrServerUrl    *          The Solr server URL    * @param queueSize    *          The buffer size before the documents are sent to the server    * @param threadCount    *          The number of background threads used to empty the queue    */
 DECL|method|ConcurrentUpdateSolrClient
 specifier|public
@@ -668,6 +673,10 @@ name|threadCount
 argument_list|)
 expr_stmt|;
 name|shutdownExecutor
+operator|=
+literal|true
+expr_stmt|;
+name|internalHttpClient
 operator|=
 literal|true
 expr_stmt|;
@@ -2441,6 +2450,10 @@ name|void
 name|close
 parameter_list|()
 block|{
+if|if
+condition|(
+name|internalHttpClient
+condition|)
 name|IOUtils
 operator|.
 name|closeQuietly
@@ -2576,6 +2589,10 @@ name|void
 name|shutdownNow
 parameter_list|()
 block|{
+if|if
+condition|(
+name|internalHttpClient
+condition|)
 name|IOUtils
 operator|.
 name|closeQuietly
