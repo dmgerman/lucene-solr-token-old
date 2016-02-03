d042c11d0e71b3ce9396534554c2ae6e094abcbb
Mike McCandless <mikemccand@apache.org>
2016-02-03 15:47:50 -0500

    remove nocommit

--- lucene/test-framework/src/java/org/apache/lucene/util/TestRuleTemporaryFilesCleanup.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/TestRuleTemporaryFilesCleanup.java
@@ -941,7 +941,6 @@ literal|null
 argument_list|)
 expr_stmt|;
 block|}
-comment|// nocommit true:
 if|if
 condition|(
 name|allowed
@@ -953,9 +952,6 @@ operator|.
 name|class
 argument_list|)
 operator|&&
-operator|(
-literal|true
-operator|||
 name|random
 operator|.
 name|nextInt
@@ -964,7 +960,6 @@ literal|10
 argument_list|)
 operator|==
 literal|1
-operator|)
 condition|)
 block|{
 comment|// 10% of the time we swap in virus checking (acts-like-windows) FS:
