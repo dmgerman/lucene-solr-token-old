2fc570202574425103015264cf060b982be19afb
markrmiller <markrmiller@apache.org>
2016-02-17 13:37:08 -0500

    SOLR-8416: The collections create API should return after all replicas are active.

--- solr/core/src/java/org/apache/solr/handler/admin/CoreAdminOperation.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CoreAdminOperation.java
@@ -1311,6 +1311,12 @@ name|CallInfo
 name|callInfo
 parameter_list|)
 block|{
+assert|assert
+name|TestInjection
+operator|.
+name|injectRandomDelayInCoreCreation
+argument_list|()
+assert|;
 name|SolrParams
 name|params
 init|=
