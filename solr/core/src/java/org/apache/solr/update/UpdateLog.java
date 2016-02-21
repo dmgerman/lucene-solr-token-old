1f7376ab5c5cb85c32e3ffc71f1584e187ace511
markrmiller <markrmiller@apache.org>
2016-02-20 20:35:21 -0500

    SOLR-8575: Add missing assert.

--- solr/core/src/java/org/apache/solr/update/UpdateLog.java
+++ solr/core/src/java/org/apache/solr/update/UpdateLog.java
@@ -6898,11 +6898,12 @@ argument_list|)
 expr_stmt|;
 comment|// something wrong with the request?
 block|}
+assert|assert
 name|TestInjection
 operator|.
 name|injectUpdateLogReplayRandomPause
 argument_list|()
-expr_stmt|;
+assert|;
 block|}
 name|CommitUpdateCommand
 name|cmd
