ccc314b9e9419eddbb16c30adc75e1d8d0705428
Mike McCandless <mikemccand@apache.org>
2016-02-24 17:45:56 -0500

    add BinaryPoint.newSetQuery

--- lucene/core/src/java/org/apache/lucene/search/PointInSetQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/PointInSetQuery.java
@@ -729,14 +729,6 @@ name|bytesPerDim
 argument_list|)
 throw|;
 block|}
-name|int
-name|bytesPerDim
-init|=
-name|fieldInfo
-operator|.
-name|getPointNumBytes
-argument_list|()
-decl_stmt|;
 name|DocIdSetBuilder
 name|result
 init|=
