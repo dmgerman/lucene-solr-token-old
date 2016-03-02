4dc473dcca928dcf1f4c2776a1a3259d6821ed6a
Mike McCandless <mikemccand@apache.org>
2016-03-02 06:30:43 -0500

    add todos, fix whitespace

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -1491,6 +1491,7 @@ name|close
 argument_list|()
 expr_stmt|;
 block|}
+comment|// TODO: on 6.0.0 release, gen the indices and add here:
 DECL|field|oldNames
 specifier|final
 specifier|static
@@ -1752,6 +1753,7 @@ block|,
 literal|"5.5.0-nocfs"
 block|}
 decl_stmt|;
+comment|// TODO: on 6.0.0 release, gen the single segment indices and add here:
 DECL|field|oldSingleSegmentNames
 specifier|final
 specifier|static
