cf9665b615faa3e7e95b9fd0d8d16758677e1639
Christine Poerschke <cpoerschke@apache.org>
2016-04-25 09:39:29 +0100

    LUCENE-7247: TestCoreParser.dumpResults verbose and test-fail logging tweaks

--- lucene/queryparser/src/test/org/apache/lucene/queryparser/xml/TestCoreParser.java
+++ lucene/queryparser/src/test/org/apache/lucene/queryparser/xml/TestCoreParser.java
@@ -1200,13 +1200,23 @@ literal|"TEST: qType="
 operator|+
 name|qType
 operator|+
-literal|" query="
-operator|+
-name|q
-operator|+
 literal|" numDocs="
 operator|+
 name|numDocs
+operator|+
+literal|" "
+operator|+
+name|q
+operator|.
+name|getClass
+argument_list|()
+operator|.
+name|getCanonicalName
+argument_list|()
+operator|+
+literal|" query="
+operator|+
+name|q
 argument_list|)
 expr_stmt|;
 block|}
@@ -1229,22 +1239,22 @@ argument_list|,
 name|numDocs
 argument_list|)
 decl_stmt|;
-name|assertTrue
-argument_list|(
-name|qType
-operator|+
-literal|" should produce results "
-argument_list|,
+specifier|final
+name|boolean
+name|producedResults
+init|=
+operator|(
 name|hits
 operator|.
 name|totalHits
 operator|>
 literal|0
-argument_list|)
-expr_stmt|;
+operator|)
+decl_stmt|;
 if|if
 condition|(
-name|VERBOSE
+operator|!
+name|producedResults
 condition|)
 block|{
 name|System
@@ -1253,13 +1263,35 @@ name|out
 operator|.
 name|println
 argument_list|(
-literal|"========="
+literal|"TEST: qType="
 operator|+
 name|qType
 operator|+
-literal|"============"
+literal|" numDocs="
+operator|+
+name|numDocs
+operator|+
+literal|" "
+operator|+
+name|q
+operator|.
+name|getClass
+argument_list|()
+operator|.
+name|getCanonicalName
+argument_list|()
+operator|+
+literal|" query="
+operator|+
+name|q
 argument_list|)
 expr_stmt|;
+block|}
+if|if
+condition|(
+name|VERBOSE
+condition|)
+block|{
 name|ScoreDoc
 index|[]
 name|scoreDocs
@@ -1341,6 +1373,15 @@ name|println
 argument_list|()
 expr_stmt|;
 block|}
+name|assertTrue
+argument_list|(
+name|qType
+operator|+
+literal|" produced no results"
+argument_list|,
+name|producedResults
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 end_class
