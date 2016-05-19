f1b6f7ff9153868c5fe2860ea01736c0a6bca7be
Karl Wright <DaddyWri@gmail.com>
2016-05-18 20:34:49 -0400

    LUCENE-7290: Decrease MINIMUM_RESOLUTION, since it's too big for at least one test case.

--- lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/Vector.java
+++ lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/Vector.java
@@ -33,7 +33,7 @@ specifier|final
 name|double
 name|MINIMUM_RESOLUTION
 init|=
-literal|1.0e-12
+literal|1.0e-13
 decl_stmt|;
 comment|/**    * For squared quantities, the bound is squared too.    */
 DECL|field|MINIMUM_RESOLUTION_SQUARED
